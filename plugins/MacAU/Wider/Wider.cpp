ee090696621521aaa9475b5a3b3015b3b7511800
airwindows <jinx6568@sover.net>
2018-05-06 20:11:50 -0400

    Wider

---
+++ plugins/MacAU/Wider/Wider.cpp
@@ -0,0 +1,373 @@
+/*
+ *	File:		Wider.cpp
+ *	
+ *	Version:	1.0
+ * 
+ *	Created:	5/21/07
+ *	
+ *	Copyright:  Copyright © 2007 Airwindows, All Rights Reserved
+ * 
+ *	Disclaimer:	IMPORTANT:  This Apple software is supplied to you by Apple Computer, Inc. ("Apple") in 
+ *				consideration of your agreement to the following terms, and your use, installation, modification 
+ *				or redistribution of this Apple software constitutes acceptance of these terms.  If you do 
+ *				not agree with these terms, please do not use, install, modify or redistribute this Apple 
+ *				software.
+ *
+ *				In consideration of your agreement to abide by the following terms, and subject to these terms, 
+ *				Apple grants you a personal, non-exclusive license, under Apple's copyrights in this 
+ *				original Apple software (the "Apple Software"), to use, reproduce, modify and redistribute the 
+ *				Apple Software, with or without modifications, in source and/or binary forms; provided that if you 
+ *				redistribute the Apple Software in its entirety and without modifications, you must retain this 
+ *				notice and the following text and disclaimers in all such redistributions of the Apple Software. 
+ *				Neither the name, trademarks, service marks or logos of Apple Computer, Inc. may be used to 
+ *				endorse or promote products derived from the Apple Software without specific prior written 
+ *				permission from Apple.  Except as expressly stated in this notice, no other rights or 
+ *				licenses, express or implied, are granted by Apple herein, including but not limited to any 
+ *				patent rights that may be infringed by your derivative works or by other works in which the 
+ *				Apple Software may be incorporated.
+ *
+ *				The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO WARRANTIES, EXPRESS OR 
+ *				IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY 
+ *				AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND OPERATION ALONE 
+ *				OR IN COMBINATION WITH YOUR PRODUCTS.
+ *
+ *				IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL 
+ *				DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
+ *				OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, 
+ *				REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER 
+ *				UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR OTHERWISE, EVEN 
+ *				IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+/*=============================================================================
+ Wider.h
+ 
+ =============================================================================*/
+#include "Wider.h"
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+COMPONENT_ENTRY(Wider)
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Wider::Wider
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Wider::Wider(AudioUnit component)
+: AUEffectBase(component)
+{
+	CreateElements();
+	Globals()->UseIndexedParameters(kNumberOfParameters);
+	SetParameter(kParam_One, kDefaultValue_ParamOne );
+	SetParameter(kParam_Two, kDefaultValue_ParamTwo );
+	SetParameter(kParam_Three, kDefaultValue_ParamThree );
+	
+#if AU_DEBUG_DISPATCHER
+	mDebugDispatcher = new AUDebugDispatcher (this);
+#endif
+	
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Wider::GetParameterValueStrings
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult		Wider::GetParameterValueStrings(AudioUnitScope		inScope,
+												   AudioUnitParameterID	inParameterID,
+												   CFArrayRef *		outStrings)
+{
+	
+    return kAudioUnitErr_InvalidProperty;
+}
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Wider::GetParameterInfo
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult		Wider::GetParameterInfo(AudioUnitScope		inScope,
+										   AudioUnitParameterID	inParameterID,
+										   AudioUnitParameterInfo	&outParameterInfo )
+{
+	ComponentResult result = noErr;
+	
+	outParameterInfo.flags = 	kAudioUnitParameterFlag_IsWritable
+	|		kAudioUnitParameterFlag_IsReadable;
+    
+    if (inScope == kAudioUnitScope_Global) {
+        switch(inParameterID)
+        {
+			case kParam_One:
+                AUBase::FillInParameterName (outParameterInfo, kParameterOneName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = -1.0;
+                outParameterInfo.maxValue = 1.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamOne;
+                break;
+            case kParam_Two:
+                AUBase::FillInParameterName (outParameterInfo, kParameterTwoName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = -1.0;
+                outParameterInfo.maxValue = 1.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamTwo;
+                break;
+            case kParam_Three:
+                AUBase::FillInParameterName (outParameterInfo, kParameterThreeName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = 0.0;
+                outParameterInfo.maxValue = 1.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamThree;
+                break;
+			default:
+                result = kAudioUnitErr_InvalidParameter;
+                break;
+		}
+	} else {
+        result = kAudioUnitErr_InvalidParameter;
+    }
+    
+	
+	
+	return result;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Wider::GetPropertyInfo
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult		Wider::GetPropertyInfo (AudioUnitPropertyID	inID,
+										   AudioUnitScope		inScope,
+										   AudioUnitElement	inElement,
+										   UInt32 &		outDataSize,
+										   Boolean &		outWritable)
+{
+	return AUEffectBase::GetPropertyInfo (inID, inScope, inElement, outDataSize, outWritable);
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// state that plugin supports only stereo-in/stereo-out processing
+UInt32 Wider::SupportedNumChannels(const AUChannelInfo ** outInfo)
+{
+	if (outInfo != NULL)
+	{
+		static AUChannelInfo info;
+		info.inChannels = 2;
+		info.outChannels = 2;
+		*outInfo = &info;
+	}
+	
+	return 1;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Wider::GetProperty
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult		Wider::GetProperty(	AudioUnitPropertyID inID,
+									  AudioUnitScope 		inScope,
+									  AudioUnitElement 	inElement,
+									  void *			outData )
+{
+	return AUEffectBase::GetProperty (inID, inScope, inElement, outData);
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Wider::Initialize
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult Wider::Initialize()
+{
+    ComponentResult result = AUEffectBase::Initialize();
+    if (result == noErr)
+        Reset(kAudioUnitScope_Global, 0);
+    return result;
+}
+
+#pragma mark ____WiderEffectKernel
+
+//-----------------------------------------------------------------------------------------
+// this is called the reset the DSP state (clear buffers, reset counters, etc.)
+ComponentResult		Wider::Reset(AudioUnitScope inScope, AudioUnitElement inElement)
+{
+	for(int fcount = 0; fcount < 4098; fcount++) {p[fcount] = 0.0;}
+	count = 0;
+	flip = false;
+	fpNShapeAL = 0.0;
+	fpNShapeBL = 0.0;
+	fpNShapeAR = 0.0;
+	fpNShapeBR = 0.0;
+	return noErr;
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Wider::ProcessBufferLists
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+OSStatus		Wider::ProcessBufferLists(AudioUnitRenderActionFlags & ioActionFlags,
+										 const AudioBufferList & inBuffer,
+										 AudioBufferList & outBuffer,
+										 UInt32 			inFramesToProcess)
+{
+	
+	Float32 * inputL = (Float32*)(inBuffer.mBuffers[0].mData);
+	Float32 * inputR = (Float32*)(inBuffer.mBuffers[1].mData);
+	Float32 * outputL = (Float32*)(outBuffer.mBuffers[0].mData);
+	Float32 * outputR = (Float32*)(outBuffer.mBuffers[1].mData);
+	UInt32 nSampleFrames = inFramesToProcess;
+	Float64 overallscale = 1.0;
+	overallscale /= 44100.0;
+	overallscale *= GetSampleRate();
+	Float32 fpTemp;
+	Float64 fpOld = 0.618033988749894848204586; //golden ratio!
+	Float64 fpNew = 1.0 - fpOld;
+	long double inputSampleL;
+	long double inputSampleR;
+	Float64 drySampleL;
+	Float64 drySampleR;
+	long double mid;
+	long double side;
+	Float64 out;
+	Float64 densityside = GetParameter( kParam_One );
+	Float64 densitymid = GetParameter( kParam_Two );
+	Float64 wet = GetParameter( kParam_Three );
+	Float64 dry = 1.0 - wet;
+	wet *= 0.5; //we make mid-side by adding/subtracting both channels into each channel
+	//and that's why we gotta divide it by 2: otherwise everything's doubled. So, premultiply it to save an extra 'math'
+	Float64 offset = (densityside-densitymid)/2;
+	if (offset > 0) offset = sin(offset);
+	if (offset < 0) offset = -sin(-offset);
+	offset = -(pow(offset,4) * 20 * overallscale);
+	int near = (int)floor(fabs(offset));
+	Float64 farLevel = fabs(offset) - near;
+	int far = near + 1;
+	Float64 nearLevel = 1.0 - farLevel;
+	Float64 bridgerectifier;
+	//interpolating the sample
+	
+	
+	while (nSampleFrames-- > 0) {
+		inputSampleL = *inputL;
+		inputSampleR = *inputR;
+		if (inputSampleL<1.2e-38 && -inputSampleL<1.2e-38) {
+			static int noisesource = 0;
+			//this declares a variable before anything else is compiled. It won't keep assigning
+			//it to 0 for every sample, it's as if the declaration doesn't exist in this context,
+			//but it lets me add this denormalization fix in a single place rather than updating
+			//it in three different locations. The variable isn't thread-safe but this is only
+			//a random seed and we can share it with whatever.
+			noisesource = noisesource % 1700021; noisesource++;
+			int residue = noisesource * noisesource;
+			residue = residue % 170003; residue *= residue;
+			residue = residue % 17011; residue *= residue;
+			residue = residue % 1709; residue *= residue;
+			residue = residue % 173; residue *= residue;
+			residue = residue % 17;
+			double applyresidue = residue;
+			applyresidue *= 0.00000001;
+			applyresidue *= 0.00000001;
+			inputSampleL = applyresidue;
+		}
+		if (inputSampleR<1.2e-38 && -inputSampleR<1.2e-38) {
+			static int noisesource = 0;
+			noisesource = noisesource % 1700021; noisesource++;
+			int residue = noisesource * noisesource;
+			residue = residue % 170003; residue *= residue;
+			residue = residue % 17011; residue *= residue;
+			residue = residue % 1709; residue *= residue;
+			residue = residue % 173; residue *= residue;
+			residue = residue % 17;
+			double applyresidue = residue;
+			applyresidue *= 0.00000001;
+			applyresidue *= 0.00000001;
+			inputSampleR = applyresidue;
+			//this denormalization routine produces a white noise at -300 dB which the noise
+			//shaping will interact with to produce a bipolar output, but the noise is actually
+			//all positive. That should stop any variables from going denormal, and the routine
+			//only kicks in if digital black is input. As a final touch, if you save to 24-bit
+			//the silence will return to being digital black again.
+		}
+		drySampleL = inputSampleL;
+		drySampleR = inputSampleR;
+		//assign working variables		
+		mid = inputSampleL + inputSampleR;
+		side = inputSampleL - inputSampleR;
+		//assign mid and side. Now, High Impact code
+		
+		if (densityside != 0.0)
+		{
+			out = fabs(densityside);
+			bridgerectifier = fabs(side)*1.57079633;
+			if (bridgerectifier > 1.57079633) bridgerectifier = 1.57079633;
+			//max value for sine function
+			if (densityside > 0) bridgerectifier = sin(bridgerectifier);
+			else bridgerectifier = 1-cos(bridgerectifier);
+			//produce either boosted or starved version
+			if (side > 0) side = (side*(1-out))+(bridgerectifier*out);
+			else side = (side*(1-out))-(bridgerectifier*out);
+			//blend according to density control
+		}
+		
+		if (densitymid != 0.0)
+		{
+			out = fabs(densitymid);
+			bridgerectifier = fabs(mid)*1.57079633;
+			if (bridgerectifier > 1.57079633) bridgerectifier = 1.57079633;
+			//max value for sine function
+			if (densitymid > 0) bridgerectifier = sin(bridgerectifier);
+			else bridgerectifier = 1-cos(bridgerectifier);
+			//produce either boosted or starved version
+			if (mid > 0) mid = (mid*(1-out))+(bridgerectifier*out);
+			else mid = (mid*(1-out))-(bridgerectifier*out);
+			//blend according to density control
+		}
+		
+		if (count < 1 || count > 2048) {count = 2048;}
+		if (offset > 0)
+		{
+			p[count+2048] = p[count] = mid;
+			mid = p[count+near]*nearLevel;
+			mid += p[count+far]*farLevel;
+		}
+		
+		if (offset < 0)
+		{
+			p[count+2048] = p[count] = side;
+			side = p[count+near]*nearLevel;
+			side += p[count+far]*farLevel;
+		}
+		count -= 1;
+		
+		inputSampleL = (drySampleL * dry) + ((mid+side) * wet);
+		inputSampleR = (drySampleR * dry) + ((mid-side) * wet);
+		
+		//noise shaping to 32-bit floating point
+		if (flip) {
+			fpTemp = inputSampleL;
+			fpNShapeAL = (fpNShapeAL*fpOld)+((inputSampleL-fpTemp)*fpNew);
+			inputSampleL += fpNShapeAL;
+			
+			fpTemp = inputSampleR;
+			fpNShapeAR = (fpNShapeAR*fpOld)+((inputSampleR-fpTemp)*fpNew);
+			inputSampleR += fpNShapeAR;
+		}
+		else {
+			fpTemp = inputSampleL;
+			fpNShapeBL = (fpNShapeBL*fpOld)+((inputSampleL-fpTemp)*fpNew);
+			inputSampleL += fpNShapeBL;
+			
+			fpTemp = inputSampleR;
+			fpNShapeBR = (fpNShapeBR*fpOld)+((inputSampleR-fpTemp)*fpNew);
+			inputSampleR += fpNShapeBR;
+		}
+		flip = !flip;
+		//end noise shaping on 32 bit output
+
+		*outputL = inputSampleL;
+		*outputR = inputSampleR;
+		
+		inputL += 1;
+		inputR += 1;
+		outputL += 1;
+		outputR += 1;
+	}
+	return noErr;
+}
+
