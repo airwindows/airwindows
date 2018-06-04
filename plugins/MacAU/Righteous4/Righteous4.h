3a3c2dde62b7c28950898c469c376ad32ac63f39
Chris Johnson <jinx6568@sover.net>
2018-06-03 21:19:24 -0400

    Righteous4 && Channel4

---
+++ plugins/MacAU/Righteous4/Righteous4.h
@@ -0,0 +1,184 @@
+/*
+*	File:		Righteous4.h
+*	
+*	Version:	1.0
+* 
+*	Created:	4/8/18
+*	
+*	Copyright:  Copyright © 2018 Airwindows, All Rights Reserved
+* 
+*	Disclaimer:	IMPORTANT:  This Apple software is supplied to you by Apple Computer, Inc. ("Apple") in 
+*				consideration of your agreement to the following terms, and your use, installation, modification 
+*				or redistribution of this Apple software constitutes acceptance of these terms.  If you do 
+*				not agree with these terms, please do not use, install, modify or redistribute this Apple 
+*				software.
+*
+*				In consideration of your agreement to abide by the following terms, and subject to these terms, 
+*				Apple grants you a personal, non-exclusive license, under Apple's copyrights in this 
+*				original Apple software (the "Apple Software"), to use, reproduce, modify and redistribute the 
+*				Apple Software, with or without modifications, in source and/or binary forms; provided that if you 
+*				redistribute the Apple Software in its entirety and without modifications, you must retain this 
+*				notice and the following text and disclaimers in all such redistributions of the Apple Software. 
+*				Neither the name, trademarks, service marks or logos of Apple Computer, Inc. may be used to 
+*				endorse or promote products derived from the Apple Software without specific prior written 
+*				permission from Apple.  Except as expressly stated in this notice, no other rights or 
+*				licenses, express or implied, are granted by Apple herein, including but not limited to any 
+*				patent rights that may be infringed by your derivative works or by other works in which the 
+*				Apple Software may be incorporated.
+*
+*				The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO WARRANTIES, EXPRESS OR 
+*				IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY 
+*				AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND OPERATION ALONE 
+*				OR IN COMBINATION WITH YOUR PRODUCTS.
+*
+*				IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL 
+*				DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
+*				OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, 
+*				REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER 
+*				UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR OTHERWISE, EVEN 
+*				IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+#include "AUEffectBase.h"
+#include "Righteous4Version.h"
+
+#if AU_DEBUG_DISPATCHER
+	#include "AUDebugDispatcher.h"
+#endif
+
+
+#ifndef __Righteous4_h__
+#define __Righteous4_h__
+
+
+#pragma mark ____Righteous4 Parameters
+
+// parameters
+static const float kDefaultValue_ParamOne = -13.0;
+static const float kDefaultValue_ParamTwo = 2;
+
+static const int k16bit = 1;
+static const int k24bit = 2;
+static const int k32bit = 3;
+
+static CFStringRef kMenuItem_16bit = CFSTR ("16 bit");
+static CFStringRef kMenuItem_24bit = CFSTR ("24 bit");
+static CFStringRef kMenuItem_32bit = CFSTR ("32 bit");
+
+static CFStringRef kParameterOneName = CFSTR("Loudness Target");
+static CFStringRef kParameterTwoName = CFSTR("Output Bit Depth");
+//Alter the name if desired, but using the plugin name is a start
+
+enum {
+	kParam_One =0,
+	kParam_Two =1,
+	//Add your parameters here...
+	kNumberOfParameters=2
+};
+
+#pragma mark ____Righteous4
+class Righteous4 : public AUEffectBase
+{
+public:
+	Righteous4(AudioUnit component);
+#if AU_DEBUG_DISPATCHER
+	virtual ~Righteous4 () { delete mDebugDispatcher; }
+#endif
+	
+	virtual AUKernelBase *		NewKernel() { return new Righteous4Kernel(this); }
+	
+	virtual	ComponentResult		GetParameterValueStrings(AudioUnitScope			inScope,
+														 AudioUnitParameterID		inParameterID,
+														 CFArrayRef *			outStrings);
+    
+	virtual	ComponentResult		GetParameterInfo(AudioUnitScope			inScope,
+												 AudioUnitParameterID	inParameterID,
+												 AudioUnitParameterInfo	&outParameterInfo);
+    
+	virtual ComponentResult		GetPropertyInfo(AudioUnitPropertyID		inID,
+												AudioUnitScope			inScope,
+												AudioUnitElement		inElement,
+												UInt32 &			outDataSize,
+												Boolean	&			outWritable );
+	
+	virtual ComponentResult		GetProperty(AudioUnitPropertyID inID,
+											AudioUnitScope 		inScope,
+											AudioUnitElement 		inElement,
+											void *			outData);
+	
+	virtual ComponentResult    Initialize();
+	virtual bool				SupportsTail () { return true; }
+    virtual Float64				GetTailTime() {return (1.0/GetSampleRate())*0.0;} //in SECONDS! gsr * a number = in samples
+    virtual Float64				GetLatency() {return (1.0/GetSampleRate())*0.0;}	// in SECONDS! gsr * a number = in samples
+	
+	/*! @method Version */
+	virtual ComponentResult		Version() { return kRighteous4Version; }
+	
+    
+	
+protected:
+		class Righteous4Kernel : public AUKernelBase		// most of the real work happens here
+	{
+public:
+		Righteous4Kernel(AUEffectBase *inAudioUnit )
+		: AUKernelBase(inAudioUnit)
+	{
+	}
+		
+		// *Required* overides for the process method for this effect
+		// processes one channel of interleaved samples
+        virtual void 		Process(	const Float32 	*inSourceP,
+										Float32		 	*inDestP,
+										UInt32 			inFramesToProcess,
+										UInt32			inNumChannels,
+										bool			&ioSilence);
+		
+        virtual void		Reset();
+		
+		private: 
+		
+		Float64 midSampleA;
+		Float64 midSampleB;
+		Float64 midSampleC;
+		Float64 midSampleD;
+		Float64 midSampleE;
+		Float64 midSampleF;
+		Float64 midSampleG;
+		Float64 midSampleH;
+		Float64 midSampleI;
+		Float64 midSampleJ;
+		Float64 midSampleK;
+		Float64 midSampleL;
+		Float64 midSampleM;
+		Float64 midSampleN;
+		Float64 midSampleO;
+		Float64 midSampleP;
+		Float64 midSampleQ;
+		Float64 midSampleR;
+		Float64 midSampleS;
+		Float64 midSampleT;
+		Float64 midSampleU;
+		Float64 midSampleV;
+		Float64 midSampleW;
+		Float64 midSampleX;
+		Float64 midSampleY;
+		Float64 midSampleZ;
+		
+		Float64 byn[13];
+		long double noiseShaping;
+		
+		Float64 lastSample;
+		Float64 IIRsample;
+		
+		Float64 gwPrev;
+		Float64 gwA;
+		Float64 gwB;
+		
+		long double fpNShape;		
+	};
+};
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#endif
