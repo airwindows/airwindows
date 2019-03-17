3e70ca9a520599f87e0fd48491273c6d6f870a67
Chris Johnson <jinx6568@sover.net>
2019-03-17 17:59:24 -0400

    Compresaturator

---
+++ plugins/MacAU/Compresaturator/Compresaturator.cpp
@@ -0,0 +1,329 @@
+/*
+*	File:		Compresaturator.cpp
+*	
+*	Version:	1.0
+* 
+*	Created:	11/30/18
+*	
+*	Copyright:  Copyright © 2018 Airwindows, All Rights Reserved
+* 
+*	Disclaimer:	IMPORTANT:  This Apple software is supplied to you by Apple Computer, Inc. ("Apple") in 
+*				consideration of your agreement to the following terms, and your use, installation, modification 
+*				or redistribution of this Apple software constitutes acceptance of these terms.  If you do 
+*				not agree with these terms, please do not use, install, modify or redistribute this Apple 
+*				software.
+*
+*				In consideration of your agreement to abide by the following terms, and subject to these terms, 
+*				Apple grants you a personal, non-exclusive license, under Apple's copyrights in this 
+*				original Apple software (the "Apple Software"), to use, reproduce, modify and redistribute the 
+*				Apple Software, with or without modifications, in source and/or binary forms; provided that if you 
+*				redistribute the Apple Software in its entirety and without modifications, you must retain this 
+*				notice and the following text and disclaimers in all such redistributions of the Apple Software. 
+*				Neither the name, trademarks, service marks or logos of Apple Computer, Inc. may be used to 
+*				endorse or promote products derived from the Apple Software without specific prior written 
+*				permission from Apple.  Except as expressly stated in this notice, no other rights or 
+*				licenses, express or implied, are granted by Apple herein, including but not limited to any 
+*				patent rights that may be infringed by your derivative works or by other works in which the 
+*				Apple Software may be incorporated.
+*
+*				The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO WARRANTIES, EXPRESS OR 
+*				IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY 
+*				AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND OPERATION ALONE 
+*				OR IN COMBINATION WITH YOUR PRODUCTS.
+*
+*				IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL 
+*				DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
+*				OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, 
+*				REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER 
+*				UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR OTHERWISE, EVEN 
+*				IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+/*=============================================================================
+	Compresaturator.cpp
+	
+=============================================================================*/
+#include "Compresaturator.h"
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+COMPONENT_ENTRY(Compresaturator)
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Compresaturator::Compresaturator
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Compresaturator::Compresaturator(AudioUnit component)
+	: AUEffectBase(component)
+{
+	CreateElements();
+	Globals()->UseIndexedParameters(kNumberOfParameters);
+	SetParameter(kParam_One, kDefaultValue_ParamOne );
+	SetParameter(kParam_Two, kDefaultValue_ParamTwo );
+	SetParameter(kParam_Three, kDefaultValue_ParamThree );
+	SetParameter(kParam_Four, kDefaultValue_ParamFour );
+	SetParameter(kParam_Five, kDefaultValue_ParamFive );
+         
+#if AU_DEBUG_DISPATCHER
+	mDebugDispatcher = new AUDebugDispatcher (this);
+#endif
+	
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Compresaturator::GetParameterValueStrings
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Compresaturator::GetParameterValueStrings(AudioUnitScope		inScope,
+                                                                AudioUnitParameterID	inParameterID,
+                                                                CFArrayRef *		outStrings)
+{
+        
+    return kAudioUnitErr_InvalidProperty;
+}
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Compresaturator::GetParameterInfo
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Compresaturator::GetParameterInfo(AudioUnitScope		inScope,
+                                                        AudioUnitParameterID	inParameterID,
+                                                        AudioUnitParameterInfo	&outParameterInfo )
+{
+	ComponentResult result = noErr;
+
+	outParameterInfo.flags = 	kAudioUnitParameterFlag_IsWritable
+						|		kAudioUnitParameterFlag_IsReadable;
+    
+    if (inScope == kAudioUnitScope_Global) {
+        switch(inParameterID)
+        {
+			case kParam_One:
+                AUBase::FillInParameterName (outParameterInfo, kParameterOneName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Decibels;
+                outParameterInfo.minValue = -12.0;
+                outParameterInfo.maxValue = 12.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamOne;
+                break;
+            case kParam_Two:
+                AUBase::FillInParameterName (outParameterInfo, kParameterTwoName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Percent;
+				outParameterInfo.minValue = 0.0;
+                outParameterInfo.maxValue = 100.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamTwo;
+                break;
+            case kParam_Three:
+                AUBase::FillInParameterName (outParameterInfo, kParameterThreeName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_CustomUnit;
+				outParameterInfo.flags |= kAudioUnitParameterFlag_DisplayLogarithmic;
+				outParameterInfo.unitName = kParameterThreeUnit;
+				outParameterInfo.minValue = 50.0;
+                outParameterInfo.maxValue = 5000.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamThree;
+                break;
+			case kParam_Four:
+                AUBase::FillInParameterName (outParameterInfo, kParameterFourName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = 0.0;
+                outParameterInfo.maxValue = 1.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamFour;
+                break;
+			case kParam_Five:
+                AUBase::FillInParameterName (outParameterInfo, kParameterFiveName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = 0.0;
+                outParameterInfo.maxValue = 1.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamFive;
+                break;
+			default:
+                result = kAudioUnitErr_InvalidParameter;
+                break;
+            }
+	} else {
+        result = kAudioUnitErr_InvalidParameter;
+    }
+    
+
+
+	return result;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Compresaturator::GetPropertyInfo
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Compresaturator::GetPropertyInfo (AudioUnitPropertyID	inID,
+                                                        AudioUnitScope		inScope,
+                                                        AudioUnitElement	inElement,
+                                                        UInt32 &		outDataSize,
+                                                        Boolean &		outWritable)
+{
+	return AUEffectBase::GetPropertyInfo (inID, inScope, inElement, outDataSize, outWritable);
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Compresaturator::GetProperty
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Compresaturator::GetProperty(	AudioUnitPropertyID inID,
+                                                        AudioUnitScope 		inScope,
+                                                        AudioUnitElement 	inElement,
+                                                        void *			outData )
+{
+	return AUEffectBase::GetProperty (inID, inScope, inElement, outData);
+}
+
+//	Compresaturator::Initialize
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult Compresaturator::Initialize()
+{
+    ComponentResult result = AUEffectBase::Initialize();
+    if (result == noErr)
+        Reset(kAudioUnitScope_Global, 0);
+    return result;
+}
+
+#pragma mark ____CompresaturatorEffectKernel
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Compresaturator::CompresaturatorKernel::Reset()
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void		Compresaturator::CompresaturatorKernel::Reset()
+{
+	for(int count = 0; count < 10990; count++) {d[count] = 0.0;}
+	dCount = 0;
+	lastWidth = 500;
+	padFactor = 0;
+	fpd = 17;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Compresaturator::CompresaturatorKernel::Process
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void		Compresaturator::CompresaturatorKernel::Process(	const Float32 	*inSourceP,
+                                                    Float32		 	*inDestP,
+                                                    UInt32 			inFramesToProcess,
+                                                    UInt32			inNumChannels, 
+                                                    bool			&ioSilence )
+{
+	UInt32 nSampleFrames = inFramesToProcess;
+	const Float32 *sourceP = inSourceP;
+	Float32 *destP = inDestP;
+
+	Float64 inputgain = pow(10.0,GetParameter( kParam_One )/20.0);
+	Float64 satComp = GetParameter( kParam_Two ) / 50;
+	int widestRange = GetParameter( kParam_Three );
+	satComp += (((Float64)widestRange/3000.0)*satComp);
+	//set the max wideness of comp zone, minimum range boosted (too much?)
+	Float64 output = GetParameter( kParam_Four );
+	Float64 wet = GetParameter( kParam_Five );
+	
+	while (nSampleFrames-- > 0) {
+		long double inputSample = *sourceP;
+		long double temp = inputSample;
+
+		static int noisesource = 0;
+		int residue;
+		double applyresidue;
+		noisesource = noisesource % 1700021; noisesource++;
+		residue = noisesource * noisesource;
+		residue = residue % 170003; residue *= residue;
+		residue = residue % 17011; residue *= residue;
+		residue = residue % 1709; residue *= residue;
+		residue = residue % 173; residue *= residue;
+		residue = residue % 17;
+		applyresidue = residue;
+		applyresidue *= 0.00000001;
+		applyresidue *= 0.00000001;
+		inputSample += applyresidue;
+		if (inputSample<1.2e-38 && -inputSample<1.2e-38) {
+			inputSample -= applyresidue;
+		}
+		//for live air, we always apply the dither noise. Then, if our result is 
+		//effectively digital black, we'll subtract it again. We want a 'air' hiss
+		long double drySample = inputSample;
+		
+		if (dCount < 1 || dCount > 5000) {dCount = 5000;}
+
+		Float64 variSpeed = 1.0 + ((padFactor/lastWidth)*satComp);
+		if (variSpeed < 1.0) variSpeed = 1.0;
+		Float64 totalgain = inputgain / variSpeed;
+		if (totalgain != 1.0) {
+			inputSample *= totalgain;
+			if (totalgain < 1.0) {
+				temp *= totalgain;
+				//no boosting beyond unity please
+			}
+		}
+		
+		long double bridgerectifier = fabs(inputSample);
+		Float64 overspill = 0;
+		int targetWidth = widestRange;
+		//we now have defaults and an absolute input value to work with
+		if (bridgerectifier < 0.01) padFactor *= 0.9999;
+		//in silences we bring back padFactor if it got out of hand		
+		if (bridgerectifier > 1.57079633) {
+			bridgerectifier = 1.57079633;
+			targetWidth = 8;
+		}
+		//if our output's gone beyond saturating to distorting, we begin chasing the
+		//buffer size smaller. Anytime we don't have that, we expand (smoothest sound, only adding to an increasingly subdivided buffer)
+		
+		bridgerectifier = sin(bridgerectifier);
+		if (inputSample > 0) {
+			inputSample = bridgerectifier;
+			overspill = temp - bridgerectifier;
+		}
+		
+		if (inputSample < 0) {
+			inputSample = -bridgerectifier;
+			overspill = (-temp) - bridgerectifier;
+		} 
+		//drive section
+		
+		d[dCount + 5000] = d[dCount] = overspill * satComp;
+		//we now have a big buffer to draw from, which is always positive amount of overspill
+		dCount--;
+		
+		padFactor += d[dCount];
+		Float64 randy = (rand()/(double)RAND_MAX);
+		if ((targetWidth*randy) > lastWidth) {
+			//we are expanding the buffer so we don't remove this trailing sample
+			lastWidth += 1;
+		} else {
+			padFactor -= d[dCount+lastWidth];
+			//zero change, or target is smaller and we are shrinking
+			if (targetWidth < lastWidth) {
+				lastWidth -= 1;
+				if (lastWidth < 2) lastWidth = 2;
+				//sanity check as randy can give us target zero
+				padFactor -= d[dCount+lastWidth];
+			}
+		}
+		//variable attack/release speed more rapid as comp intensity increases
+		//implemented in a way where we're repeatedly not altering the buffer as it expands, which makes the comp artifacts smoother
+		if (padFactor < 0) padFactor = 0;
+		
+
+		if (output < 1.0) {
+			inputSample *= output;
+		}
+		
+		if (wet < 1.0) {
+			inputSample = (inputSample * wet) + (drySample * (1.0-wet));
+		}
+		
+		//begin 32 bit floating point dither
+		int expon; frexpf((float)inputSample, &expon);
+		fpd ^= fpd << 13; fpd ^= fpd >> 17; fpd ^= fpd << 5;
+		inputSample += static_cast<int32_t>(fpd) * 5.960464655174751e-36L * pow(2,expon+62);
+		//end 32 bit floating point dither
+		
+		*destP = inputSample;
+		
+		sourceP += inNumChannels; destP += inNumChannels;
+	}
+}
+
