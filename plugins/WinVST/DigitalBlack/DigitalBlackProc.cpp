614745bf38ad4f07cdc8dd3408015ef46e510d59
Chris Johnson <jinx6568@sover.net>
2020-03-01 18:33:44 -0500

    DigitalBlack

---
+++ plugins/WinVST/DigitalBlack/DigitalBlackProc.cpp
@@ -0,0 +1,210 @@
+/* ========================================
+ *  DigitalBlack - DigitalBlack.h
+ *  Copyright (c) 2016 airwindows, All rights reserved
+ * ======================================== */
+
+#ifndef __DigitalBlack_H
+#include "DigitalBlack.h"
+#endif
+
+void DigitalBlack::processReplacing(float **inputs, float **outputs, VstInt32 sampleFrames) 
+{
+    float* in1  =  inputs[0];
+    float* in2  =  inputs[1];
+    float* out1 = outputs[0];
+    float* out2 = outputs[1];
+
+	double overallscale = 1.0;
+	overallscale /= 44100.0;
+	overallscale *= getSampleRate();
+
+	double threshold = (pow(A,4)/3)+0.00018;
+	double release = 0.0064 / overallscale;
+	double wet = B;
+    
+    while (--sampleFrames >= 0)
+    {
+		long double inputSampleL = *in1;
+		long double inputSampleR = *in2;
+		if (fabs(inputSampleL)<1.18e-37) inputSampleL = fpd * 1.18e-37;
+		if (fabs(inputSampleR)<1.18e-37) inputSampleR = fpd * 1.18e-37;
+		long double drySampleL = inputSampleL;
+		long double drySampleR = inputSampleR;
+		
+
+		if (inputSampleL > 0) {
+			if (WasNegativeL == true) ZeroCrossL = 0;
+			WasNegativeL = false;
+		} else {
+			ZeroCrossL += 1;
+			WasNegativeL = true;
+		}
+		if (inputSampleR > 0) {
+			if (WasNegativeR == true) ZeroCrossR = 0;
+			WasNegativeR = false;
+		} else {
+			ZeroCrossR += 1;
+			WasNegativeR = true;
+		}
+		
+		if (ZeroCrossL > 6000) ZeroCrossL = 6000;
+		if (ZeroCrossR > 6000) ZeroCrossR = 6000;
+		
+		if (fabs(inputSampleL) > threshold) {
+			if (gaterollerL < ZeroCrossL) gaterollerL = ZeroCrossL;
+		} else {
+			gaterollerL -= release;
+		}
+		if (fabs(inputSampleR) > threshold) {
+			if (gaterollerR < ZeroCrossR) gaterollerR = ZeroCrossR;
+		} else {
+			gaterollerR -= release;
+		}
+		
+		if (gaterollerL < 0.0) gaterollerL = 0.0;
+		if (gaterollerR < 0.0) gaterollerR = 0.0;
+		
+		double gate = 1.0;
+		if (gaterollerL < 1.0) gate = gaterollerL;
+		
+		long double bridgerectifier = 1-cos(fabs(inputSampleL));
+		
+		if (inputSampleL > 0) inputSampleL = (inputSampleL*gate)+(bridgerectifier*(1-gate));
+		else inputSampleL = (inputSampleL*gate)-(bridgerectifier*(1-gate));
+		
+		if ((gate == 0.0) && (fabs(inputSampleL*3) < threshold)) inputSampleL = 0.0;
+		
+		gate = 1.0;
+		if (gaterollerR < 1.0) gate = gaterollerR;
+		
+		bridgerectifier = 1-cos(fabs(inputSampleR));
+		
+		if (inputSampleR > 0) inputSampleR = (inputSampleR*gate)+(bridgerectifier*(1-gate));
+		else inputSampleR = (inputSampleR*gate)-(bridgerectifier*(1-gate));
+		
+		if ((gate == 0.0) && (fabs(inputSampleR*3) < threshold)) inputSampleR = 0.0;
+				
+		if (wet !=1.0) {
+			inputSampleL = (inputSampleL * wet) + (drySampleL * (1.0-wet));
+			inputSampleR = (inputSampleR * wet) + (drySampleR * (1.0-wet));
+		}
+		
+		//begin 32 bit stereo floating point dither
+		int expon; frexpf((float)inputSampleL, &expon);
+		fpd ^= fpd << 13; fpd ^= fpd >> 17; fpd ^= fpd << 5;
+		inputSampleL += ((double(fpd)-uint32_t(0x7fffffff)) * 5.5e-36l * pow(2,expon+62));
+		frexpf((float)inputSampleR, &expon);
+		fpd ^= fpd << 13; fpd ^= fpd >> 17; fpd ^= fpd << 5;
+		inputSampleR += ((double(fpd)-uint32_t(0x7fffffff)) * 5.5e-36l * pow(2,expon+62));
+		//end 32 bit stereo floating point dither
+		
+		*out1 = inputSampleL;
+		*out2 = inputSampleR;
+
+		*in1++;
+		*in2++;
+		*out1++;
+		*out2++;
+    }
+}
+
+void DigitalBlack::processDoubleReplacing(double **inputs, double **outputs, VstInt32 sampleFrames) 
+{
+    double* in1  =  inputs[0];
+    double* in2  =  inputs[1];
+    double* out1 = outputs[0];
+    double* out2 = outputs[1];
+
+	double overallscale = 1.0;
+	overallscale /= 44100.0;
+	overallscale *= getSampleRate();
+	
+	double threshold = (pow(A,4)/3)+0.00018;
+	double release = 0.0064 / overallscale;
+	double wet = B;
+    
+    while (--sampleFrames >= 0)
+    {
+		long double inputSampleL = *in1;
+		long double inputSampleR = *in2;
+		if (fabs(inputSampleL)<1.18e-43) inputSampleL = fpd * 1.18e-43;
+		if (fabs(inputSampleR)<1.18e-43) inputSampleR = fpd * 1.18e-43;
+		long double drySampleL = inputSampleL;
+		long double drySampleR = inputSampleR;
+		
+		
+		if (inputSampleL > 0) {
+			if (WasNegativeL == true) ZeroCrossL = 0;
+			WasNegativeL = false;
+		} else {
+			ZeroCrossL += 1;
+			WasNegativeL = true;
+		}
+		if (inputSampleR > 0) {
+			if (WasNegativeR == true) ZeroCrossR = 0;
+			WasNegativeR = false;
+		} else {
+			ZeroCrossR += 1;
+			WasNegativeR = true;
+		}
+		
+		if (ZeroCrossL > 6000) ZeroCrossL = 6000;
+		if (ZeroCrossR > 6000) ZeroCrossR = 6000;
+		
+		if (fabs(inputSampleL) > threshold) {
+			if (gaterollerL < ZeroCrossL) gaterollerL = ZeroCrossL;
+		} else {
+			gaterollerL -= release;
+		}
+		if (fabs(inputSampleR) > threshold) {
+			if (gaterollerR < ZeroCrossR) gaterollerR = ZeroCrossR;
+		} else {
+			gaterollerR -= release;
+		}
+		
+		if (gaterollerL < 0.0) gaterollerL = 0.0;
+		if (gaterollerR < 0.0) gaterollerR = 0.0;
+		
+		double gate = 1.0;
+		if (gaterollerL < 1.0) gate = gaterollerL;
+		
+		long double bridgerectifier = 1-cos(fabs(inputSampleL));
+		
+		if (inputSampleL > 0) inputSampleL = (inputSampleL*gate)+(bridgerectifier*(1-gate));
+		else inputSampleL = (inputSampleL*gate)-(bridgerectifier*(1-gate));
+		
+		if ((gate == 0.0) && (fabs(inputSampleL*3) < threshold)) inputSampleL = 0.0;
+		
+		gate = 1.0;
+		if (gaterollerR < 1.0) gate = gaterollerR;
+		
+		bridgerectifier = 1-cos(fabs(inputSampleR));
+		
+		if (inputSampleR > 0) inputSampleR = (inputSampleR*gate)+(bridgerectifier*(1-gate));
+		else inputSampleR = (inputSampleR*gate)-(bridgerectifier*(1-gate));
+		
+		if ((gate == 0.0) && (fabs(inputSampleR*3) < threshold)) inputSampleR = 0.0;
+		
+		if (wet !=1.0) {
+			inputSampleL = (inputSampleL * wet) + (drySampleL * (1.0-wet));
+			inputSampleR = (inputSampleR * wet) + (drySampleR * (1.0-wet));
+		}
+		
+		//begin 64 bit stereo floating point dither
+		int expon; frexp((double)inputSampleL, &expon);
+		fpd ^= fpd << 13; fpd ^= fpd >> 17; fpd ^= fpd << 5;
+		inputSampleL += ((double(fpd)-uint32_t(0x7fffffff)) * 1.1e-44l * pow(2,expon+62));
+		frexp((double)inputSampleR, &expon);
+		fpd ^= fpd << 13; fpd ^= fpd >> 17; fpd ^= fpd << 5;
+		inputSampleR += ((double(fpd)-uint32_t(0x7fffffff)) * 1.1e-44l * pow(2,expon+62));
+		//end 64 bit stereo floating point dither
+		
+		*out1 = inputSampleL;
+		*out2 = inputSampleR;
+
+		*in1++;
+		*in2++;
+		*out1++;
+		*out2++;
+    }
+}
