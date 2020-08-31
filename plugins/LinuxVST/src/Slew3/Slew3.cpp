84a2c3c249205c03f43bab9ba9ec100c764a8d6c
Chris Johnson <jinx6568@sover.net>
2020-08-30 21:26:52 -0400

    Slew3

---
+++ plugins/LinuxVST/src/Slew3/Slew3.cpp
@@ -0,0 +1,120 @@
+/* ========================================
+ *  Slew3 - Slew3.h
+ *  Copyright (c) 2016 airwindows, All rights reserved
+ * ======================================== */
+
+#ifndef __Slew3_H
+#include "Slew3.h"
+#endif
+
+AudioEffect* createEffectInstance(audioMasterCallback audioMaster) {return new Slew3(audioMaster);}
+
+Slew3::Slew3(audioMasterCallback audioMaster) :
+    AudioEffectX(audioMaster, kNumPrograms, kNumParameters)
+{
+	A = 0.0;
+	lastSampleAL = lastSampleBL = lastSampleCL = 0.0;
+	lastSampleAR = lastSampleBR = lastSampleCR = 0.0;
+	fpd = 17;
+	//this is reset: values being initialized only once. Startup values, whatever they are.
+	
+    _canDo.insert("plugAsChannelInsert"); // plug-in can be used as a channel insert effect.
+    _canDo.insert("plugAsSend"); // plug-in can be used as a send effect.
+    _canDo.insert("x2in2out"); 
+    setNumInputs(kNumInputs);
+    setNumOutputs(kNumOutputs);
+    setUniqueID(kUniqueId);
+    canProcessReplacing();     // supports output replacing
+    canDoubleReplacing();      // supports double precision processing
+	programsAreChunks(true);
+    vst_strncpy (_programName, "Default", kVstMaxProgNameLen); // default program name
+}
+
+Slew3::~Slew3() {}
+VstInt32 Slew3::getVendorVersion () {return 1000;}
+void Slew3::setProgramName(char *name) {vst_strncpy (_programName, name, kVstMaxProgNameLen);}
+void Slew3::getProgramName(char *name) {vst_strncpy (name, _programName, kVstMaxProgNameLen);}
+//airwindows likes to ignore this stuff. Make your own programs, and make a different plugin rather than
+//trying to do versioning and preventing people from using older versions. Maybe they like the old one!
+
+static float pinParameter(float data)
+{
+	if (data < 0.0f) return 0.0f;
+	if (data > 1.0f) return 1.0f;
+	return data;
+}
+
+VstInt32 Slew3::getChunk (void** data, bool isPreset)
+{
+	float *chunkData = (float *)calloc(kNumParameters, sizeof(float));
+	chunkData[0] = A;
+	/* Note: The way this is set up, it will break if you manage to save settings on an Intel
+	 machine and load them on a PPC Mac. However, it's fine if you stick to the machine you 
+	 started with. */
+	
+	*data = chunkData;
+	return kNumParameters * sizeof(float);
+}
+
+VstInt32 Slew3::setChunk (void* data, VstInt32 byteSize, bool isPreset)
+{	
+	float *chunkData = (float *)data;
+	A = pinParameter(chunkData[0]);
+	/* We're ignoring byteSize as we found it to be a filthy liar */
+	
+	/* calculate any other fields you need here - you could copy in 
+	 code from setParameter() here. */
+	return 0;
+}
+
+void Slew3::setParameter(VstInt32 index, float value) {
+    switch (index) {
+        case kParamA: A = value; break;
+        default: throw; // unknown parameter, shouldn't happen!
+    }
+}
+
+float Slew3::getParameter(VstInt32 index) {
+    switch (index) {
+        case kParamA: return A; break;
+        default: break; // unknown parameter, shouldn't happen!
+    } return 0.0; //we only need to update the relevant name, this is simple to manage
+}
+
+void Slew3::getParameterName(VstInt32 index, char *text) {
+    switch (index) {
+        case kParamA: vst_strncpy (text, "Clampng", kVstMaxParamStrLen); break;
+        default: break; // unknown parameter, shouldn't happen!
+    } //this is our labels for displaying in the VST host
+}
+
+void Slew3::getParameterDisplay(VstInt32 index, char *text) {
+    switch (index) {
+        case kParamA: float2string (A, text, kVstMaxParamStrLen); break;
+        default: break; // unknown parameter, shouldn't happen!
+	} //this displays the values and handles 'popups' where it's discrete choices
+}
+
+void Slew3::getParameterLabel(VstInt32 index, char *text) {
+    switch (index) {
+        case kParamA: vst_strncpy (text, "", kVstMaxParamStrLen); break;
+		default: break; // unknown parameter, shouldn't happen!
+    }
+}
+
+VstInt32 Slew3::canDo(char *text) 
+{ return (_canDo.find(text) == _canDo.end()) ? -1: 1; } // 1 = yes, -1 = no, 0 = don't know
+
+bool Slew3::getEffectName(char* name) {
+    vst_strncpy(name, "Slew3", kVstMaxProductStrLen); return true;
+}
+
+VstPlugCategory Slew3::getPlugCategory() {return kPlugCategEffect;}
+
+bool Slew3::getProductString(char* text) {
+  	vst_strncpy (text, "airwindows Slew3", kVstMaxProductStrLen); return true;
+}
+
+bool Slew3::getVendorString(char* text) {
+  	vst_strncpy (text, "airwindows", kVstMaxVendorStrLen); return true;
+}
---
+++ plugins/LinuxVST/src/Slew3/Slew3Proc.cpp
@@ -0,0 +1,154 @@
+/* ========================================
+ *  Slew3 - Slew3.h
+ *  Copyright (c) 2016 airwindows, All rights reserved
+ * ======================================== */
+
+#ifndef __Slew3_H
+#include "Slew3.h"
+#endif
+
+void Slew3::processReplacing(float **inputs, float **outputs, VstInt32 sampleFrames) 
+{
+    float* in1  =  inputs[0];
+    float* in2  =  inputs[1];
+    float* out1 = outputs[0];
+    float* out2 = outputs[1];
+
+	double overallscale = 1.0;
+	overallscale /= 44100.0;
+	overallscale *= getSampleRate();
+
+	double threshold = pow((1-A),4)/overallscale;
+    
+    while (--sampleFrames >= 0)
+    {
+		long double inputSampleL = *in1;
+		long double inputSampleR = *in2;
+		if (fabs(inputSampleL)<1.18e-37) inputSampleL = fpd * 1.18e-37;
+		if (fabs(inputSampleR)<1.18e-37) inputSampleR = fpd * 1.18e-37;
+		
+		//begin L
+		double clamp = (lastSampleBL - lastSampleCL) * 0.381966011250105;
+		clamp -= (lastSampleAL - lastSampleBL) * 0.6180339887498948482045;
+		clamp += inputSampleL - lastSampleAL; //regular slew clamping added
+		
+		lastSampleCL = lastSampleBL;
+		lastSampleBL = lastSampleAL;
+		lastSampleAL = inputSampleL; //now our output relates off lastSampleB
+		
+		if (clamp > threshold)
+			inputSampleL = lastSampleBL + threshold;
+		if (-clamp > threshold)
+			inputSampleL = lastSampleBL - threshold;
+
+		lastSampleAL = (lastSampleAL*0.381966011250105)+(inputSampleL*0.6180339887498948482045); //split the difference between raw and smoothed for buffer
+		//end L
+		
+		//begin R
+		clamp = (lastSampleBR - lastSampleCR) * 0.381966011250105;
+		clamp -= (lastSampleAR - lastSampleBR) * 0.6180339887498948482045;
+		clamp += inputSampleR - lastSampleAR; //regular slew clamping added
+		
+		lastSampleCR = lastSampleBR;
+		lastSampleBR = lastSampleAR;
+		lastSampleAR = inputSampleR; //now our output relates off lastSampleB
+		
+		if (clamp > threshold)
+			inputSampleR = lastSampleBR + threshold;
+		if (-clamp > threshold)
+			inputSampleR = lastSampleBR - threshold;
+		
+		lastSampleAR = (lastSampleAR*0.381966011250105)+(inputSampleR*0.6180339887498948482045); //split the difference between raw and smoothed for buffer
+		//end R
+		
+		//begin 32 bit stereo floating point dither
+		int expon; frexpf((float)inputSampleL, &expon);
+		fpd ^= fpd << 13; fpd ^= fpd >> 17; fpd ^= fpd << 5;
+		inputSampleL += ((double(fpd)-uint32_t(0x7fffffff)) * 5.5e-36l * pow(2,expon+62));
+		frexpf((float)inputSampleR, &expon);
+		fpd ^= fpd << 13; fpd ^= fpd >> 17; fpd ^= fpd << 5;
+		inputSampleR += ((double(fpd)-uint32_t(0x7fffffff)) * 5.5e-36l * pow(2,expon+62));
+		//end 32 bit stereo floating point dither
+		
+		*out1 = inputSampleL;
+		*out2 = inputSampleR;
+
+		*in1++;
+		*in2++;
+		*out1++;
+		*out2++;
+    }
+}
+
+void Slew3::processDoubleReplacing(double **inputs, double **outputs, VstInt32 sampleFrames) 
+{
+    double* in1  =  inputs[0];
+    double* in2  =  inputs[1];
+    double* out1 = outputs[0];
+    double* out2 = outputs[1];
+
+	double overallscale = 1.0;
+	overallscale /= 44100.0;
+	overallscale *= getSampleRate();
+	
+	double threshold = pow((1-A),4)/overallscale;
+    
+    while (--sampleFrames >= 0)
+    {
+		long double inputSampleL = *in1;
+		long double inputSampleR = *in2;
+		if (fabs(inputSampleL)<1.18e-43) inputSampleL = fpd * 1.18e-43;
+		if (fabs(inputSampleR)<1.18e-43) inputSampleR = fpd * 1.18e-43;
+		
+		//begin L
+		double clamp = (lastSampleBL - lastSampleCL) * 0.381966011250105;
+		clamp -= (lastSampleAL - lastSampleBL) * 0.6180339887498948482045;
+		clamp += inputSampleL - lastSampleAL; //regular slew clamping added
+		
+		lastSampleCL = lastSampleBL;
+		lastSampleBL = lastSampleAL;
+		lastSampleAL = inputSampleL; //now our output relates off lastSampleB
+		
+		if (clamp > threshold)
+			inputSampleL = lastSampleBL + threshold;
+		if (-clamp > threshold)
+			inputSampleL = lastSampleBL - threshold;
+		
+		lastSampleAL = (lastSampleAL*0.381966011250105)+(inputSampleL*0.6180339887498948482045); //split the difference between raw and smoothed for buffer
+		//end L
+		
+		//begin R
+		clamp = (lastSampleBR - lastSampleCR) * 0.381966011250105;
+		clamp -= (lastSampleAR - lastSampleBR) * 0.6180339887498948482045;
+		clamp += inputSampleR - lastSampleAR; //regular slew clamping added
+		
+		lastSampleCR = lastSampleBR;
+		lastSampleBR = lastSampleAR;
+		lastSampleAR = inputSampleR; //now our output relates off lastSampleB
+		
+		if (clamp > threshold)
+			inputSampleR = lastSampleBR + threshold;
+		if (-clamp > threshold)
+			inputSampleR = lastSampleBR - threshold;
+		
+		lastSampleAR = (lastSampleAR*0.381966011250105)+(inputSampleR*0.6180339887498948482045); //split the difference between raw and smoothed for buffer
+		//end R
+		
+		//begin 64 bit stereo floating point dither
+		int expon; frexp((double)inputSampleL, &expon);
+		fpd ^= fpd << 13; fpd ^= fpd >> 17; fpd ^= fpd << 5;
+		inputSampleL += ((double(fpd)-uint32_t(0x7fffffff)) * 1.1e-44l * pow(2,expon+62));
+		frexp((double)inputSampleR, &expon);
+		fpd ^= fpd << 13; fpd ^= fpd >> 17; fpd ^= fpd << 5;
+		inputSampleR += ((double(fpd)-uint32_t(0x7fffffff)) * 1.1e-44l * pow(2,expon+62));
+		//end 64 bit stereo floating point dither
+		
+		*out1 = inputSampleL;
+		*out2 = inputSampleR;
+
+		*in1++;
+		*in2++;
+		*out1++;
+		*out2++;
+    }
+}
