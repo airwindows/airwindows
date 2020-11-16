8e70eedbaeb7998a276331eb5eeb1015b1bc8807
Chris Johnson <jinx6568@sover.net>
2020-11-15 19:09:05 -0500

    Isolator

---
+++ plugins/MacAU/Isolator/Isolator.cpp
@@ -0,0 +1,278 @@
+/*
+*	File:		Isolator.cpp
+*	
+*	Version:	1.0
+* 
+*	Created:	11/2/20
+*	
+*	Copyright:  Copyright © 2020 Airwindows, All Rights Reserved
+* 
+*	Disclaimer:	IMPORTANT:  This Apple software is supplied to you by Apple Computer, Inc. ("Apple") in 
+*				consideration of your agreement to the following terms, and your use, installation, modification 
+*				or redistribution of this Apple software constitutes acceptance of these terms.  If you do 
+*				not agree with these terms, please do not use, install, modify or redistribute this Apple 
+*				software.
+*
+*				In consideration of your agreement to abide by the following terms, and subject to these terms, 
+*				Apple grants you a personal, non-exclusive license, under Apple's copyrights in this 
+*				original Apple software (the "Apple Software"), to use, reproduce, modify and redistribute the 
+*				Apple Software, with or without modifications, in source and/or binary forms; provided that if you 
+*				redistribute the Apple Software in its entirety and without modifications, you must retain this 
+*				notice and the following text and disclaimers in all such redistributions of the Apple Software. 
+*				Neither the name, trademarks, service marks or logos of Apple Computer, Inc. may be used to 
+*				endorse or promote products derived from the Apple Software without specific prior written 
+*				permission from Apple.  Except as expressly stated in this notice, no other rights or 
+*				licenses, express or implied, are granted by Apple herein, including but not limited to any 
+*				patent rights that may be infringed by your derivative works or by other works in which the 
+*				Apple Software may be incorporated.
+*
+*				The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO WARRANTIES, EXPRESS OR 
+*				IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY 
+*				AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND OPERATION ALONE 
+*				OR IN COMBINATION WITH YOUR PRODUCTS.
+*
+*				IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL 
+*				DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
+*				OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, 
+*				REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER 
+*				UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR OTHERWISE, EVEN 
+*				IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+/*=============================================================================
+	Isolator.cpp
+	
+=============================================================================*/
+#include "Isolator.h"
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+COMPONENT_ENTRY(Isolator)
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Isolator::Isolator
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Isolator::Isolator(AudioUnit component)
+	: AUEffectBase(component)
+{
+	CreateElements();
+	Globals()->UseIndexedParameters(kNumberOfParameters);
+	SetParameter(kParam_One, kDefaultValue_ParamOne );
+	SetParameter(kParam_Two, kDefaultValue_ParamTwo );
+	SetParameter(kParam_Three, kDefaultValue_ParamThree );
+         
+#if AU_DEBUG_DISPATCHER
+	mDebugDispatcher = new AUDebugDispatcher (this);
+#endif
+	
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Isolator::GetParameterValueStrings
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Isolator::GetParameterValueStrings(AudioUnitScope		inScope,
+                                                                AudioUnitParameterID	inParameterID,
+                                                                CFArrayRef *		outStrings)
+{
+        
+    return kAudioUnitErr_InvalidProperty;
+}
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Isolator::GetParameterInfo
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Isolator::GetParameterInfo(AudioUnitScope		inScope,
+                                                        AudioUnitParameterID	inParameterID,
+                                                        AudioUnitParameterInfo	&outParameterInfo )
+{
+	ComponentResult result = noErr;
+
+	outParameterInfo.flags = 	kAudioUnitParameterFlag_IsWritable
+						|		kAudioUnitParameterFlag_IsReadable;
+    
+    if (inScope == kAudioUnitScope_Global) {
+        switch(inParameterID)
+        {
+			case kParam_One:
+                AUBase::FillInParameterName (outParameterInfo, kParameterOneName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = 0.0;
+                outParameterInfo.maxValue = 1.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamOne;
+                break;
+			case kParam_Two:
+                AUBase::FillInParameterName (outParameterInfo, kParameterTwoName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = 0.0;
+                outParameterInfo.maxValue = 1.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamTwo;
+                break;
+			case kParam_Three:
+                AUBase::FillInParameterName (outParameterInfo, kParameterThreeName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = 0.0;
+                outParameterInfo.maxValue = 1.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamThree;
+                break;
+			default:
+                result = kAudioUnitErr_InvalidParameter;
+                break;
+            }
+	} else {
+        result = kAudioUnitErr_InvalidParameter;
+    }
+    
+
+
+	return result;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Isolator::GetPropertyInfo
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Isolator::GetPropertyInfo (AudioUnitPropertyID	inID,
+                                                        AudioUnitScope		inScope,
+                                                        AudioUnitElement	inElement,
+                                                        UInt32 &		outDataSize,
+                                                        Boolean &		outWritable)
+{
+	return AUEffectBase::GetPropertyInfo (inID, inScope, inElement, outDataSize, outWritable);
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Isolator::GetProperty
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Isolator::GetProperty(	AudioUnitPropertyID inID,
+                                                        AudioUnitScope 		inScope,
+                                                        AudioUnitElement 	inElement,
+                                                        void *			outData )
+{
+	return AUEffectBase::GetProperty (inID, inScope, inElement, outData);
+}
+
+//	Isolator::Initialize
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult Isolator::Initialize()
+{
+    ComponentResult result = AUEffectBase::Initialize();
+    if (result == noErr)
+        Reset(kAudioUnitScope_Global, 0);
+    return result;
+}
+
+#pragma mark ____IsolatorEffectKernel
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Isolator::IsolatorKernel::Reset()
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void		Isolator::IsolatorKernel::Reset()
+{
+	for (int x = 0; x < 11; x++) {biquadA[x] = 0.0;biquadB[x] = 0.0;biquadC[x] = 0.0;}
+	fpd = 17;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Isolator::IsolatorKernel::Process
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void		Isolator::IsolatorKernel::Process(	const Float32 	*inSourceP,
+                                                    Float32		 	*inDestP,
+                                                    UInt32 			inFramesToProcess,
+                                                    UInt32			inNumChannels, 
+                                                    bool			&ioSilence )
+{
+	UInt32 nSampleFrames = inFramesToProcess;
+	const Float32 *sourceP = inSourceP;
+	Float32 *destP = inDestP;
+	long double overallscale = 1.0;
+	overallscale /= 44100.0;
+	overallscale *= GetSampleRate();
+	
+	bool bypass = (GetParameter( kParam_One ) == 1.0);
+	Float64 high = GetParameter( kParam_Two );
+	Float64 low = GetParameter( kParam_Three ); //this gives us shelving, and
+	//the ability to use the isolator as a highpass
+	if (high > 0.0) bypass = false;
+	if (low < 1.0) bypass = false;
+	
+	biquadA[0] = pow(GetParameter( kParam_One ),(2.0*sqrt(overallscale)))*0.4999;
+	if (biquadA[0] < 0.001) biquadA[0] = 0.001;
+	biquadC[0] = biquadB[0] = biquadA[0];
+	
+    biquadA[1] = 0.5;
+	biquadB[1] = 0.618033988749894848204586;
+	biquadC[1] = 1.618033988749894848204586;
+		
+	double K = tan(M_PI * biquadA[0]); //lowpass
+	double norm = 1.0 / (1.0 + K / biquadA[1] + K * K);
+	biquadA[2] = K * K * norm;
+	biquadA[3] = 2.0 * biquadA[2];
+	biquadA[4] = biquadA[2];
+	biquadA[5] = 2.0 * (K * K - 1.0) * norm;
+	biquadA[6] = (1.0 - K / biquadA[1] + K * K) * norm;
+	
+	K = tan(M_PI * biquadA[0]);
+	norm = 1.0 / (1.0 + K / biquadB[1] + K * K);
+	biquadB[2] = K * K * norm;
+	biquadB[3] = 2.0 * biquadB[2];
+	biquadB[4] = biquadB[2];
+	biquadB[5] = 2.0 * (K * K - 1.0) * norm;
+	biquadB[6] = (1.0 - K / biquadB[1] + K * K) * norm;
+	
+	K = tan(M_PI * biquadC[0]);
+	norm = 1.0 / (1.0 + K / biquadC[1] + K * K);
+	biquadC[2] = K * K * norm;
+	biquadC[3] = 2.0 * biquadC[2];
+	biquadC[4] = biquadC[2];
+	biquadC[5] = 2.0 * (K * K - 1.0) * norm;
+	biquadC[6] = (1.0 - K / biquadC[1] + K * K) * norm;
+	
+	// there is a form for highpass
+	// but I would suggest subtracting the lowpass from dry
+	
+	while (nSampleFrames-- > 0) {
+		long double inputSample = *sourceP;
+		if (fabs(inputSample)<1.18e-37) inputSample = fpd * 1.18e-37;
+		long double drySample = *sourceP;
+		
+		inputSample = sin(inputSample);
+		//encode Console5: good cleanness
+		
+		long double tempSample = biquadA[2]*inputSample+biquadA[3]*biquadA[7]+biquadA[4]*biquadA[8]-biquadA[5]*biquadA[9]-biquadA[6]*biquadA[10];
+		biquadA[8] = biquadA[7]; biquadA[7] = inputSample; inputSample = tempSample; 
+		biquadA[10] = biquadA[9]; biquadA[9] = inputSample; //DF1
+		
+		tempSample = biquadB[2]*inputSample+biquadB[3]*biquadB[7]+biquadB[4]*biquadB[8]-biquadB[5]*biquadB[9]-biquadB[6]*biquadB[10];
+		biquadB[8] = biquadB[7]; biquadB[7] = inputSample; inputSample = tempSample; 
+		biquadB[10] = biquadB[9]; biquadB[9] = inputSample; //DF1
+		
+		tempSample = biquadC[2]*inputSample+biquadC[3]*biquadC[7]+biquadC[4]*biquadC[8]-biquadC[5]*biquadC[9]-biquadC[6]*biquadC[10];
+		biquadC[8] = biquadC[7]; biquadC[7] = inputSample; inputSample = tempSample; 
+		biquadC[10] = biquadC[9]; biquadC[9] = inputSample; //DF1
+		
+		if (inputSample > 1.0) inputSample = 1.0;
+		if (inputSample < -1.0) inputSample = -1.0;
+		//without this, you can get a NaN condition where it spits out DC offset at full blast!
+		inputSample = asin(inputSample);
+		//amplitude aspect
+		
+		if (bypass) inputSample = drySample;
+		else inputSample = (inputSample * low) + ((drySample - inputSample)*high);
+		
+		//begin 32 bit floating point dither
+		int expon; frexpf((float)inputSample, &expon);
+		fpd ^= fpd << 13; fpd ^= fpd >> 17; fpd ^= fpd << 5;
+		inputSample += ((double(fpd)-uint32_t(0x7fffffff)) * 5.5e-36l * pow(2,expon+62));
+		//end 32 bit floating point dither
+		
+		*destP = inputSample;
+		
+		sourceP += inNumChannels; destP += inNumChannels;
+	}
+}
---
+++ plugins/MacVST/Isolator/source/IsolatorProc.cpp
@@ -0,0 +1,248 @@
+/* ========================================
+ *  Isolator - Isolator.h
+ *  Copyright (c) 2016 airwindows, All rights reserved
+ * ======================================== */
+
+#ifndef __Isolator_H
+#include "Isolator.h"
+#endif
+
+void Isolator::processReplacing(float **inputs, float **outputs, VstInt32 sampleFrames) 
+{
+    float* in1  =  inputs[0];
+    float* in2  =  inputs[1];
+    float* out1 = outputs[0];
+    float* out2 = outputs[1];
+
+	double overallscale = 1.0;
+	overallscale /= 44100.0;
+	overallscale *= getSampleRate();
+
+ 	bool bypass = (A == 1.0);
+	double high = B;
+	double low = C; //this gives us shelving, and
+	//the ability to use the isolator as a highpass
+	if (high > 0.0) bypass = false;
+	if (low < 1.0) bypass = false;
+	
+	biquadA[0] = pow(A,(2.0*sqrt(overallscale)))*0.4999;
+	if (biquadA[0] < 0.001) biquadA[0] = 0.001;
+	biquadC[0] = biquadB[0] = biquadA[0];
+	
+    biquadA[1] = 0.5;
+	biquadB[1] = 0.618033988749894848204586;
+	biquadC[1] = 1.618033988749894848204586;
+	
+	double K = tan(M_PI * biquadA[0]); //lowpass
+	double norm = 1.0 / (1.0 + K / biquadA[1] + K * K);
+	biquadA[2] = K * K * norm;
+	biquadA[3] = 2.0 * biquadA[2];
+	biquadA[4] = biquadA[2];
+	biquadA[5] = 2.0 * (K * K - 1.0) * norm;
+	biquadA[6] = (1.0 - K / biquadA[1] + K * K) * norm;
+	
+	K = tan(M_PI * biquadA[0]);
+	norm = 1.0 / (1.0 + K / biquadB[1] + K * K);
+	biquadB[2] = K * K * norm;
+	biquadB[3] = 2.0 * biquadB[2];
+	biquadB[4] = biquadB[2];
+	biquadB[5] = 2.0 * (K * K - 1.0) * norm;
+	biquadB[6] = (1.0 - K / biquadB[1] + K * K) * norm;
+	
+	K = tan(M_PI * biquadC[0]);
+	norm = 1.0 / (1.0 + K / biquadC[1] + K * K);
+	biquadC[2] = K * K * norm;
+	biquadC[3] = 2.0 * biquadC[2];
+	biquadC[4] = biquadC[2];
+	biquadC[5] = 2.0 * (K * K - 1.0) * norm;
+	biquadC[6] = (1.0 - K / biquadC[1] + K * K) * norm;
+	
+	// there is a form for highpass
+	// but I would suggest subtracting the lowpass from dry
+		
+    while (--sampleFrames >= 0)
+    {
+		long double inputSampleL = *in1;
+		long double inputSampleR = *in2;
+		if (fabs(inputSampleL)<1.18e-37) inputSampleL = fpd * 1.18e-37;
+		if (fabs(inputSampleR)<1.18e-37) inputSampleR = fpd * 1.18e-37;
+		long double drySampleL = *in1;
+		long double drySampleR = *in2;
+		
+		inputSampleL = sin(inputSampleL);
+		inputSampleR = sin(inputSampleR);
+		//encode Console5: good cleanness
+		
+		long double outSampleL = biquadA[2]*inputSampleL+biquadA[3]*biquadA[7]+biquadA[4]*biquadA[8]-biquadA[5]*biquadA[9]-biquadA[6]*biquadA[10];
+		biquadA[8] = biquadA[7]; biquadA[7] = inputSampleL; inputSampleL = outSampleL; biquadA[10] = biquadA[9]; biquadA[9] = inputSampleL; //DF1 left
+		
+		outSampleL = biquadB[2]*inputSampleL+biquadB[3]*biquadB[7]+biquadB[4]*biquadB[8]-biquadB[5]*biquadB[9]-biquadB[6]*biquadB[10];
+		biquadB[8] = biquadB[7]; biquadB[7] = inputSampleL; inputSampleL = outSampleL; biquadB[10] = biquadB[9]; biquadB[9] = inputSampleL; //DF1 left
+		
+		outSampleL = biquadC[2]*inputSampleL+biquadC[3]*biquadC[7]+biquadC[4]*biquadC[8]-biquadC[5]*biquadC[9]-biquadC[6]*biquadC[10];
+		biquadC[8] = biquadC[7]; biquadC[7] = inputSampleL; inputSampleL = outSampleL; biquadC[10] = biquadC[9]; biquadC[9] = inputSampleL; //DF1 left
+		
+		long double outSampleR = biquadA[2]*inputSampleR+biquadA[3]*biquadA[11]+biquadA[4]*biquadA[12]-biquadA[5]*biquadA[13]-biquadA[6]*biquadA[14];
+		biquadA[12] = biquadA[11]; biquadA[11] = inputSampleR; inputSampleR = outSampleR; biquadA[14] = biquadA[13]; biquadA[13] = inputSampleR; //DF1 right
+		
+		outSampleR = biquadB[2]*inputSampleR+biquadB[3]*biquadB[11]+biquadB[4]*biquadB[12]-biquadB[5]*biquadB[13]-biquadB[6]*biquadB[14];
+		biquadB[12] = biquadB[11]; biquadB[11] = inputSampleR; inputSampleR = outSampleR; biquadB[14] = biquadB[13]; biquadB[13] = inputSampleR; //DF1 right
+		
+		outSampleR = biquadC[2]*inputSampleR+biquadC[3]*biquadC[11]+biquadC[4]*biquadC[12]-biquadC[5]*biquadC[13]-biquadC[6]*biquadC[14];
+		biquadC[12] = biquadC[11]; biquadC[11] = inputSampleR; inputSampleR = outSampleR; biquadC[14] = biquadC[13]; biquadC[13] = inputSampleR; //DF1 right
+		
+		if (inputSampleL > 1.0) inputSampleL = 1.0;
+		if (inputSampleL < -1.0) inputSampleL = -1.0;
+		if (inputSampleR > 1.0) inputSampleR = 1.0;
+		if (inputSampleR < -1.0) inputSampleR = -1.0;
+		
+		inputSampleL = asin(inputSampleL);
+		inputSampleR = asin(inputSampleR);
+		//amplitude aspect
+		
+		if (bypass) {
+			inputSampleL = drySampleL;
+			inputSampleR = drySampleR;
+		} else {
+			inputSampleL = (inputSampleL * low) + ((drySampleL - inputSampleL)*high);
+			inputSampleR = (inputSampleR * low) + ((drySampleR - inputSampleR)*high);
+		}
+		
+		//begin 32 bit stereo floating point dither
+		int expon; frexpf((float)inputSampleL, &expon);
+		fpd ^= fpd << 13; fpd ^= fpd >> 17; fpd ^= fpd << 5;
+		inputSampleL += ((double(fpd)-uint32_t(0x7fffffff)) * 5.5e-36l * pow(2,expon+62));
+		frexpf((float)inputSampleR, &expon);
+		fpd ^= fpd << 13; fpd ^= fpd >> 17; fpd ^= fpd << 5;
+		inputSampleR += ((double(fpd)-uint32_t(0x7fffffff)) * 5.5e-36l * pow(2,expon+62));
+		//end 32 bit stereo floating point dither
+		
+		*out1 = inputSampleL;
+		*out2 = inputSampleR;
+
+		*in1++;
+		*in2++;
+		*out1++;
+		*out2++;
+    }
+}
+
+void Isolator::processDoubleReplacing(double **inputs, double **outputs, VstInt32 sampleFrames) 
+{
+    double* in1  =  inputs[0];
+    double* in2  =  inputs[1];
+    double* out1 = outputs[0];
+    double* out2 = outputs[1];
+
+	double overallscale = 1.0;
+	overallscale /= 44100.0;
+	overallscale *= getSampleRate();
+	
+ 	bool bypass = (A == 1.0);
+	double high = B;
+	double low = C; //this gives us shelving, and
+	//the ability to use the isolator as a highpass
+	if (high > 0.0) bypass = false;
+	if (low < 1.0) bypass = false;
+	
+	biquadA[0] = pow(A,(2.0*sqrt(overallscale)))*0.4999;
+	if (biquadA[0] < 0.001) biquadA[0] = 0.001;
+	biquadC[0] = biquadB[0] = biquadA[0];
+	
+    biquadA[1] = 0.5;
+	biquadB[1] = 0.618033988749894848204586;
+	biquadC[1] = 1.618033988749894848204586;
+	
+	double K = tan(M_PI * biquadA[0]); //lowpass
+	double norm = 1.0 / (1.0 + K / biquadA[1] + K * K);
+	biquadA[2] = K * K * norm;
+	biquadA[3] = 2.0 * biquadA[2];
+	biquadA[4] = biquadA[2];
+	biquadA[5] = 2.0 * (K * K - 1.0) * norm;
+	biquadA[6] = (1.0 - K / biquadA[1] + K * K) * norm;
+	
+	K = tan(M_PI * biquadA[0]);
+	norm = 1.0 / (1.0 + K / biquadB[1] + K * K);
+	biquadB[2] = K * K * norm;
+	biquadB[3] = 2.0 * biquadB[2];
+	biquadB[4] = biquadB[2];
+	biquadB[5] = 2.0 * (K * K - 1.0) * norm;
+	biquadB[6] = (1.0 - K / biquadB[1] + K * K) * norm;
+	
+	K = tan(M_PI * biquadC[0]);
+	norm = 1.0 / (1.0 + K / biquadC[1] + K * K);
+	biquadC[2] = K * K * norm;
+	biquadC[3] = 2.0 * biquadC[2];
+	biquadC[4] = biquadC[2];
+	biquadC[5] = 2.0 * (K * K - 1.0) * norm;
+	biquadC[6] = (1.0 - K / biquadC[1] + K * K) * norm;
+	
+	// there is a form for highpass
+	// but I would suggest subtracting the lowpass from dry
+	
+    while (--sampleFrames >= 0)
+    {
+		long double inputSampleL = *in1;
+		long double inputSampleR = *in2;
+		if (fabs(inputSampleL)<1.18e-43) inputSampleL = fpd * 1.18e-43;
+		if (fabs(inputSampleR)<1.18e-43) inputSampleR = fpd * 1.18e-43;
+		long double drySampleL = *in1;
+		long double drySampleR = *in2;
+		
+		inputSampleL = sin(inputSampleL);
+		inputSampleR = sin(inputSampleR);
+		//encode Console5: good cleanness
+		
+		long double outSampleL = biquadA[2]*inputSampleL+biquadA[3]*biquadA[7]+biquadA[4]*biquadA[8]-biquadA[5]*biquadA[9]-biquadA[6]*biquadA[10];
+		biquadA[8] = biquadA[7]; biquadA[7] = inputSampleL; inputSampleL = outSampleL; biquadA[10] = biquadA[9]; biquadA[9] = inputSampleL; //DF1 left
+		
+		outSampleL = biquadB[2]*inputSampleL+biquadB[3]*biquadB[7]+biquadB[4]*biquadB[8]-biquadB[5]*biquadB[9]-biquadB[6]*biquadB[10];
+		biquadB[8] = biquadB[7]; biquadB[7] = inputSampleL; inputSampleL = outSampleL; biquadB[10] = biquadB[9]; biquadB[9] = inputSampleL; //DF1 left
+		
+		outSampleL = biquadC[2]*inputSampleL+biquadC[3]*biquadC[7]+biquadC[4]*biquadC[8]-biquadC[5]*biquadC[9]-biquadC[6]*biquadC[10];
+		biquadC[8] = biquadC[7]; biquadC[7] = inputSampleL; inputSampleL = outSampleL; biquadC[10] = biquadC[9]; biquadC[9] = inputSampleL; //DF1 left
+		
+		long double outSampleR = biquadA[2]*inputSampleR+biquadA[3]*biquadA[11]+biquadA[4]*biquadA[12]-biquadA[5]*biquadA[13]-biquadA[6]*biquadA[14];
+		biquadA[12] = biquadA[11]; biquadA[11] = inputSampleR; inputSampleR = outSampleR; biquadA[14] = biquadA[13]; biquadA[13] = inputSampleR; //DF1 right
+		
+		outSampleR = biquadB[2]*inputSampleR+biquadB[3]*biquadB[11]+biquadB[4]*biquadB[12]-biquadB[5]*biquadB[13]-biquadB[6]*biquadB[14];
+		biquadB[12] = biquadB[11]; biquadB[11] = inputSampleR; inputSampleR = outSampleR; biquadB[14] = biquadB[13]; biquadB[13] = inputSampleR; //DF1 right
+		
+		outSampleR = biquadC[2]*inputSampleR+biquadC[3]*biquadC[11]+biquadC[4]*biquadC[12]-biquadC[5]*biquadC[13]-biquadC[6]*biquadC[14];
+		biquadC[12] = biquadC[11]; biquadC[11] = inputSampleR; inputSampleR = outSampleR; biquadC[14] = biquadC[13]; biquadC[13] = inputSampleR; //DF1 right
+		
+		if (inputSampleL > 1.0) inputSampleL = 1.0;
+		if (inputSampleL < -1.0) inputSampleL = -1.0;
+		if (inputSampleR > 1.0) inputSampleR = 1.0;
+		if (inputSampleR < -1.0) inputSampleR = -1.0;
+		
+		inputSampleL = asin(inputSampleL);
+		inputSampleR = asin(inputSampleR);
+		//amplitude aspect
+		
+		if (bypass) {
+			inputSampleL = drySampleL;
+			inputSampleR = drySampleR;
+		} else {
+			inputSampleL = (inputSampleL * low) + ((drySampleL - inputSampleL)*high);
+			inputSampleR = (inputSampleR * low) + ((drySampleR - inputSampleR)*high);
+		}
+		
+		//begin 64 bit stereo floating point dither
+		int expon; frexp((double)inputSampleL, &expon);
+		fpd ^= fpd << 13; fpd ^= fpd >> 17; fpd ^= fpd << 5;
+		inputSampleL += ((double(fpd)-uint32_t(0x7fffffff)) * 1.1e-44l * pow(2,expon+62));
+		frexp((double)inputSampleR, &expon);
+		fpd ^= fpd << 13; fpd ^= fpd >> 17; fpd ^= fpd << 5;
+		inputSampleR += ((double(fpd)-uint32_t(0x7fffffff)) * 1.1e-44l * pow(2,expon+62));
+		//end 64 bit stereo floating point dither
+		
+		*out1 = inputSampleL;
+		*out2 = inputSampleR;
+
+		*in1++;
+		*in2++;
+		*out1++;
+		*out2++;
+    }
+}
