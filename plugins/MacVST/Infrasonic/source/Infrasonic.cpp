2a06dc45b2b19fcce91e9abfeaa7124791868498
Chris Johnson <jinx6568@sover.net>
2020-11-29 20:55:32 -0500

    Infrasonic

---
+++ plugins/MacVST/Infrasonic/source/Infrasonic.cpp
@@ -0,0 +1,95 @@
+/* ========================================
+ *  Infrasonic - Infrasonic.h
+ *  Copyright (c) 2016 airwindows, All rights reserved
+ * ======================================== */
+
+#ifndef __Infrasonic_H
+#include "Infrasonic.h"
+#endif
+
+AudioEffect* createEffectInstance(audioMasterCallback audioMaster) {return new Infrasonic(audioMaster);}
+
+Infrasonic::Infrasonic(audioMasterCallback audioMaster) :
+    AudioEffectX(audioMaster, kNumPrograms, kNumParameters)
+{
+	for (int x = 0; x < 15; x++) {biquadA[x] = 0.0; biquadB[x] = 0.0; biquadC[x] = 0.0; biquadD[x] = 0.0; biquadE[x] = 0.0;}
+	fpd = 17;
+	//this is reset: values being initialized only once. Startup values, whatever they are.
+	
+    _canDo.insert("plugAsChannelInsert"); // plug-in can be used as a channel insert effect.
+    _canDo.insert("plugAsSend"); // plug-in can be used as a send effect.
+    _canDo.insert("x2in2out"); 
+    setNumInputs(kNumInputs);
+    setNumOutputs(kNumOutputs);
+    setUniqueID(kUniqueId);
+    canProcessReplacing();     // supports output replacing
+    canDoubleReplacing();      // supports double precision processing
+	programsAreChunks(true);
+    vst_strncpy (_programName, "Default", kVstMaxProgNameLen); // default program name
+}
+
+Infrasonic::~Infrasonic() {}
+VstInt32 Infrasonic::getVendorVersion () {return 1000;}
+void Infrasonic::setProgramName(char *name) {vst_strncpy (_programName, name, kVstMaxProgNameLen);}
+void Infrasonic::getProgramName(char *name) {vst_strncpy (name, _programName, kVstMaxProgNameLen);}
+//airwindows likes to ignore this stuff. Make your own programs, and make a different plugin rather than
+//trying to do versioning and preventing people from using older versions. Maybe they like the old one!
+
+VstInt32 Infrasonic::getChunk (void** data, bool isPreset)
+{
+	float *chunkData = (float *)calloc(kNumParameters, sizeof(float));
+	*data = chunkData;
+	return kNumParameters * sizeof(float);
+}
+
+VstInt32 Infrasonic::setChunk (void* data, VstInt32 byteSize, bool isPreset)
+{	
+	return 0;
+}
+
+void Infrasonic::setParameter(VstInt32 index, float value) {
+    switch (index) {
+        default: throw; // unknown parameter, shouldn't happen!
+    }
+}
+
+float Infrasonic::getParameter(VstInt32 index) {
+    switch (index) {
+        default: break; // unknown parameter, shouldn't happen!
+    } return 0.0; //we only need to update the relevant name, this is simple to manage
+}
+
+void Infrasonic::getParameterName(VstInt32 index, char *text) {
+    switch (index) {
+        default: break; // unknown parameter, shouldn't happen!
+    } //this is our labels for displaying in the VST host
+}
+
+void Infrasonic::getParameterDisplay(VstInt32 index, char *text) {
+    switch (index) {
+        default: break; // unknown parameter, shouldn't happen!
+	} //this displays the values and handles 'popups' where it's discrete choices
+}
+
+void Infrasonic::getParameterLabel(VstInt32 index, char *text) {
+    switch (index) {
+		default: break; // unknown parameter, shouldn't happen!
+    }
+}
+
+VstInt32 Infrasonic::canDo(char *text) 
+{ return (_canDo.find(text) == _canDo.end()) ? -1: 1; } // 1 = yes, -1 = no, 0 = don't know
+
+bool Infrasonic::getEffectName(char* name) {
+    vst_strncpy(name, "Infrasonic", kVstMaxProductStrLen); return true;
+}
+
+VstPlugCategory Infrasonic::getPlugCategory() {return kPlugCategEffect;}
+
+bool Infrasonic::getProductString(char* text) {
+  	vst_strncpy (text, "airwindows Infrasonic", kVstMaxProductStrLen); return true;
+}
+
+bool Infrasonic::getVendorString(char* text) {
+  	vst_strncpy (text, "airwindows", kVstMaxVendorStrLen); return true;
+}
