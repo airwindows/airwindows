6ed6016b79e43b3c3183d95ed6f4a2de890c284e
Chris Johnson <jinx6568@sover.net>
2020-12-13 19:04:25 -0500

    Console7Cascade

---
+++ plugins/MacAU/Console7Cascade/Console7Cascade.cpp
@@ -0,0 +1,308 @@
+/*
+*	File:		Console7Cascade.cpp
+*	
+*	Version:	1.0
+* 
+*	Created:	12/10/20
+*	
+*	Copyright:  Copyright © 2020 Airwindows, All Rights Reserved
+* 
+*	Disclaimer:	IMPORTANT:  This Apple software is supplied to you by Apple Computer, Inc. ("Apple") in 
+*				consideration of your agreement to the following terms, and your use, installation, modification 
+*				or redistribution of this Apple software constitutes acceptance of these terms.  If you do 
+*				not agree with these terms, please do not use, install, modify or redistribute this Apple 
+*				software.
+*
+*				In consideration of your agreement to abide by the following terms, and subject to these terms, 
+*				Apple grants you a personal, non-exclusive license, under Apple's copyrights in this 
+*				original Apple software (the "Apple Software"), to use, reproduce, modify and redistribute the 
+*				Apple Software, with or without modifications, in source and/or binary forms; provided that if you 
+*				redistribute the Apple Software in its entirety and without modifications, you must retain this 
+*				notice and the following text and disclaimers in all such redistributions of the Apple Software. 
+*				Neither the name, trademarks, service marks or logos of Apple Computer, Inc. may be used to 
+*				endorse or promote products derived from the Apple Software without specific prior written 
+*				permission from Apple.  Except as expressly stated in this notice, no other rights or 
+*				licenses, express or implied, are granted by Apple herein, including but not limited to any 
+*				patent rights that may be infringed by your derivative works or by other works in which the 
+*				Apple Software may be incorporated.
+*
+*				The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO WARRANTIES, EXPRESS OR 
+*				IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY 
+*				AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND OPERATION ALONE 
+*				OR IN COMBINATION WITH YOUR PRODUCTS.
+*
+*				IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL 
+*				DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
+*				OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, 
+*				REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER 
+*				UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR OTHERWISE, EVEN 
+*				IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+/*=============================================================================
+	Console7Cascade.cpp
+	
+=============================================================================*/
+#include "Console7Cascade.h"
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+COMPONENT_ENTRY(Console7Cascade)
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Console7Cascade::Console7Cascade
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Console7Cascade::Console7Cascade(AudioUnit component)
+	: AUEffectBase(component)
+{
+	CreateElements();
+	Globals()->UseIndexedParameters(kNumberOfParameters);
+	SetParameter(kParam_One, kDefaultValue_ParamOne );
+         
+#if AU_DEBUG_DISPATCHER
+	mDebugDispatcher = new AUDebugDispatcher (this);
+#endif
+	
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Console7Cascade::GetParameterValueStrings
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Console7Cascade::GetParameterValueStrings(AudioUnitScope		inScope,
+                                                                AudioUnitParameterID	inParameterID,
+                                                                CFArrayRef *		outStrings)
+{
+        
+    return kAudioUnitErr_InvalidProperty;
+}
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Console7Cascade::GetParameterInfo
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Console7Cascade::GetParameterInfo(AudioUnitScope		inScope,
+                                                        AudioUnitParameterID	inParameterID,
+                                                        AudioUnitParameterInfo	&outParameterInfo )
+{
+	ComponentResult result = noErr;
+
+	outParameterInfo.flags = 	kAudioUnitParameterFlag_IsWritable
+						|		kAudioUnitParameterFlag_IsReadable;
+    
+    if (inScope == kAudioUnitScope_Global) {
+        switch(inParameterID)
+        {
+           case kParam_One:
+                AUBase::FillInParameterName (outParameterInfo, kParameterOneName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = 0.0;
+                outParameterInfo.maxValue = 1.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamOne;
+                break;
+           default:
+                result = kAudioUnitErr_InvalidParameter;
+                break;
+            }
+	} else {
+        result = kAudioUnitErr_InvalidParameter;
+    }
+    
+
+
+	return result;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Console7Cascade::GetPropertyInfo
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Console7Cascade::GetPropertyInfo (AudioUnitPropertyID	inID,
+                                                        AudioUnitScope		inScope,
+                                                        AudioUnitElement	inElement,
+                                                        UInt32 &		outDataSize,
+                                                        Boolean &		outWritable)
+{
+	return AUEffectBase::GetPropertyInfo (inID, inScope, inElement, outDataSize, outWritable);
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Console7Cascade::GetProperty
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Console7Cascade::GetProperty(	AudioUnitPropertyID inID,
+                                                        AudioUnitScope 		inScope,
+                                                        AudioUnitElement 	inElement,
+                                                        void *			outData )
+{
+	return AUEffectBase::GetProperty (inID, inScope, inElement, outData);
+}
+
+//	Console7Cascade::Initialize
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult Console7Cascade::Initialize()
+{
+    ComponentResult result = AUEffectBase::Initialize();
+    if (result == noErr)
+        Reset(kAudioUnitScope_Global, 0);
+    return result;
+}
+
+#pragma mark ____Console7CascadeEffectKernel
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Console7Cascade::Console7CascadeKernel::Reset()
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void		Console7Cascade::Console7CascadeKernel::Reset()
+{
+	gainchase = -1.0;
+	chasespeed = 64.0;
+	for (int x = 0; x < 11; x++) {biquadA[x] = 0.0;biquadB[x] = 0.0;biquadC[x] = 0.0;biquadD[x] = 0.0;biquadE[x] = 0.0;}
+	fpd = 1.0; while (fpd < 16386) fpd = rand()*UINT32_MAX;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Console7Cascade::Console7CascadeKernel::Process
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void		Console7Cascade::Console7CascadeKernel::Process(	const Float32 	*inSourceP,
+                                                    Float32		 	*inDestP,
+                                                    UInt32 			inFramesToProcess,
+                                                    UInt32			inNumChannels, 
+                                                    bool			&ioSilence )
+{
+	UInt32 nSampleFrames = inFramesToProcess;
+	const Float32 *sourceP = inSourceP;
+	Float32 *destP = inDestP;
+	
+	long double inputgain = 1.0-(pow(1.0-GetParameter( kParam_One ),2));
+	//this happens to give us a boost factor where the track continues to get louder even
+	//as it saturates and loses a bit of peak energy. Console7Channel channels go to 12! (.272,etc)
+	//Neutral gain through the whole system with a full scale sine ia 0.772 on the gain knob
+	if (gainchase != inputgain) chasespeed *= 2.0;
+	if (chasespeed > inFramesToProcess) chasespeed = inFramesToProcess;
+	if (gainchase < 0.0) gainchase = inputgain;
+	
+	biquadE[0] = biquadD[0] = biquadC[0] = biquadB[0] = biquadA[0] = 20000.0 / GetSampleRate();
+	biquadA[1] = 3.19622661; //tenth order Butterworth out of five biquads
+	biquadB[1] = 1.10134463; //the sharpness of the initial filter will produce more
+	biquadC[1] = 0.70710678; //edge on initial stages of clipping
+	biquadD[1] = 0.56116312;
+    biquadE[1] = 0.50623256;
+	
+	double K = tan(M_PI * biquadA[0]); //lowpass
+	double norm = 1.0 / (1.0 + K / biquadA[1] + K * K);
+	biquadA[2] = K * K * norm;
+	biquadA[3] = 2.0 * biquadA[2];
+	biquadA[4] = biquadA[2];
+	biquadA[5] = 2.0 * (K * K - 1.0) * norm;
+	biquadA[6] = (1.0 - K / biquadA[1] + K * K) * norm;
+	
+	K = tan(M_PI * biquadA[0]);
+	norm = 1.0 / (1.0 + K / biquadB[1] + K * K);
+	biquadB[2] = K * K * norm;
+	biquadB[3] = 2.0 * biquadB[2];
+	biquadB[4] = biquadB[2];
+	biquadB[5] = 2.0 * (K * K - 1.0) * norm;
+	biquadB[6] = (1.0 - K / biquadB[1] + K * K) * norm;
+	
+	K = tan(M_PI * biquadC[0]);
+	norm = 1.0 / (1.0 + K / biquadC[1] + K * K);
+	biquadC[2] = K * K * norm;
+	biquadC[3] = 2.0 * biquadC[2];
+	biquadC[4] = biquadC[2];
+	biquadC[5] = 2.0 * (K * K - 1.0) * norm;
+	biquadC[6] = (1.0 - K / biquadC[1] + K * K) * norm;
+	
+	K = tan(M_PI * biquadD[0]);
+	norm = 1.0 / (1.0 + K / biquadD[1] + K * K);
+	biquadD[2] = K * K * norm;
+	biquadD[3] = 2.0 * biquadD[2];
+	biquadD[4] = biquadD[2];
+	biquadD[5] = 2.0 * (K * K - 1.0) * norm;
+	biquadD[6] = (1.0 - K / biquadD[1] + K * K) * norm;
+	
+	K = tan(M_PI * biquadE[0]);
+	norm = 1.0 / (1.0 + K / biquadE[1] + K * K);
+	biquadE[2] = K * K * norm;
+	biquadE[3] = 2.0 * biquadE[2];
+	biquadE[4] = biquadE[2];
+	biquadE[5] = 2.0 * (K * K - 1.0) * norm;
+	biquadE[6] = (1.0 - K / biquadE[1] + K * K) * norm;
+	
+	while (nSampleFrames-- > 0) {
+		long double inputSample = *sourceP;
+		if (fabs(inputSample)<1.18e-37) inputSample = fpd * 1.18e-37;
+		
+		long double tempSample = biquadA[2]*inputSample+biquadA[3]*biquadA[7]+biquadA[4]*biquadA[8]-biquadA[5]*biquadA[9]-biquadA[6]*biquadA[10];
+		biquadA[8] = biquadA[7]; biquadA[7] = inputSample; inputSample = tempSample; 
+		biquadA[10] = biquadA[9]; biquadA[9] = inputSample; //DF1
+		
+		chasespeed *= 0.9999; chasespeed -= 0.01; if (chasespeed < 64.0) chasespeed = 64.0;
+		//we have our chase speed compensated for recent fader activity
+		gainchase = (((gainchase*chasespeed)+inputgain)/(chasespeed+1.0));
+		//gainchase is chasing the target, as a simple multiply gain factor
+		Float64 cascade = gainchase + (gainchase*0.62);
+		
+		if (1.0 != cascade) inputSample *= cascade;
+		if (inputSample > 1.097) inputSample = 1.097;
+		if (inputSample < -1.097) inputSample = -1.097;
+		inputSample = ((sin(inputSample*fabs(inputSample))/((inputSample == 0.0)?1:fabs(inputSample)))*0.8)+(sin(inputSample)*0.2);
+		//Console7Channel distortion stage, with a simpler form of the gain boost: no extra accentuation, because it's repeated 5x
+		
+		tempSample = biquadB[2]*inputSample+biquadB[3]*biquadB[7]+biquadB[4]*biquadB[8]-biquadB[5]*biquadB[9]-biquadB[6]*biquadB[10];
+		biquadB[8] = biquadB[7]; biquadB[7] = inputSample; inputSample = tempSample; 
+		biquadB[10] = biquadB[9]; biquadB[9] = inputSample; //DF1
+		
+		if (1.0 != cascade) inputSample *= cascade;
+		if (inputSample > 1.097) inputSample = 1.097;
+		if (inputSample < -1.097) inputSample = -1.097;
+		inputSample = ((sin(inputSample*fabs(inputSample))/((inputSample == 0.0)?1:fabs(inputSample)))*0.8)+(sin(inputSample)*0.2);
+		//Console7Channel distortion stage, with a simpler form of the gain boost: no extra accentuation, because it's repeated 5x
+		
+		tempSample = biquadC[2]*inputSample+biquadC[3]*biquadC[7]+biquadC[4]*biquadC[8]-biquadC[5]*biquadC[9]-biquadC[6]*biquadC[10];
+		biquadC[8] = biquadC[7]; biquadC[7] = inputSample; inputSample = tempSample; 
+		biquadC[10] = biquadC[9]; biquadC[9] = inputSample; //DF1
+		
+		if (1.0 != cascade) inputSample *= cascade;
+		if (inputSample > 1.097) inputSample = 1.097;
+		if (inputSample < -1.097) inputSample = -1.097;
+		inputSample = ((sin(inputSample*fabs(inputSample))/((inputSample == 0.0)?1:fabs(inputSample)))*0.8)+(sin(inputSample)*0.2);
+		//Console7Channel distortion stage, with a simpler form of the gain boost: no extra accentuation, because it's repeated 5x
+		
+		tempSample = biquadD[2]*inputSample+biquadD[3]*biquadD[7]+biquadD[4]*biquadD[8]-biquadD[5]*biquadD[9]-biquadD[6]*biquadD[10];
+		biquadD[8] = biquadD[7]; biquadD[7] = inputSample; inputSample = tempSample; 
+		biquadD[10] = biquadD[9]; biquadD[9] = inputSample; //DF1
+		
+		if (1.0 != cascade) inputSample *= cascade;
+		if (inputSample > 1.097) inputSample = 1.097;
+		if (inputSample < -1.097) inputSample = -1.097;
+		inputSample = ((sin(inputSample*fabs(inputSample))/((inputSample == 0.0)?1:fabs(inputSample)))*0.8)+(sin(inputSample)*0.2);
+		//Console7Channel distortion stage, with a simpler form of the gain boost: no extra accentuation, because it's repeated 5x
+		
+		tempSample = biquadE[2]*inputSample+biquadE[3]*biquadE[7]+biquadE[4]*biquadE[8]-biquadE[5]*biquadE[9]-biquadE[6]*biquadE[10];
+		biquadE[8] = biquadE[7]; biquadE[7] = inputSample; inputSample = tempSample; 
+		biquadE[10] = biquadE[9]; biquadE[9] = inputSample; //DF1
+		
+		if (1.0 != cascade) inputSample *= cascade;
+		if (inputSample > 1.097) inputSample = 1.097;
+		if (inputSample < -1.097) inputSample = -1.097;
+		inputSample = ((sin(inputSample*fabs(inputSample))/((inputSample == 0.0)?1:fabs(inputSample)))*0.8)+(sin(inputSample)*0.2);
+		//Console7Channel distortion stage, with a simpler form of the gain boost: no extra accentuation, because it's repeated 5x
+				
+		if (cascade > 1.0) inputSample /= cascade;
+		//we re-amplify after the distortion relative to how much we cut back previously.				
+		
+		//begin 32 bit floating point dither
+		int expon; frexpf((float)inputSample, &expon);
+		fpd ^= fpd << 13; fpd ^= fpd >> 17; fpd ^= fpd << 5;
+		inputSample += ((double(fpd)-uint32_t(0x7fffffff)) * 5.5e-36l * pow(2,expon+62));
+		//end 32 bit floating point dither
+		
+		*destP = inputSample;
+		
+		sourceP += inNumChannels; destP += inNumChannels;
+	}
+}
