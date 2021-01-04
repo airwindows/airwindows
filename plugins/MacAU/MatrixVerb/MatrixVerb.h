57dea44c96ec17445b06d3c45d05fd9af42528bc
Chris Johnson <jinx6568@sover.net>
2021-01-03 19:58:52 -0500

    MatrixVerb

---
+++ plugins/MacAU/MatrixVerb/MatrixVerb.h
@@ -0,0 +1,193 @@
+/*
+*	File:		MatrixVerb.h
+*	
+*	Version:	1.0
+* 
+*	Created:	10/26/20
+*	
+*	Copyright:  Copyright © 2020 Airwindows, All Rights Reserved
+* 
+*	Disclaimer:	IMPORTANT:  This Apple software is supplied to you by Apple Computer, Inc. ("Apple") in 
+*				consideration of your agreement to the following terms, and your use, installation, modification 
+*				or redistribution of this Apple software constitutes acceptance of these terms.  If you do 
+*				not agree with these terms, please do not use, install, modify or redistribute this Apple 
+*				software.
+*
+*				In consideration of your agreement to abide by the following terms, and subject to these terms, 
+*				Apple grants you a personal, non-exclusive license, under Apple's copyrights in this 
+*				original Apple software (the "Apple Software"), to use, reproduce, modify and redistribute the 
+*				Apple Software, with or without modifications, in source and/or binary forms; provided that if you 
+*				redistribute the Apple Software in its entirety and without modifications, you must retain this 
+*				notice and the following text and disclaimers in all such redistributions of the Apple Software. 
+*				Neither the name, trademarks, service marks or logos of Apple Computer, Inc. may be used to 
+*				endorse or promote products derived from the Apple Software without specific prior written 
+*				permission from Apple.  Except as expressly stated in this notice, no other rights or 
+*				licenses, express or implied, are granted by Apple herein, including but not limited to any 
+*				patent rights that may be infringed by your derivative works or by other works in which the 
+*				Apple Software may be incorporated.
+*
+*				The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO WARRANTIES, EXPRESS OR 
+*				IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY 
+*				AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND OPERATION ALONE 
+*				OR IN COMBINATION WITH YOUR PRODUCTS.
+*
+*				IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL 
+*				DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
+*				OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, 
+*				REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER 
+*				UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR OTHERWISE, EVEN 
+*				IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+#include "AUEffectBase.h"
+#include "MatrixVerbVersion.h"
+
+#if AU_DEBUG_DISPATCHER
+	#include "AUDebugDispatcher.h"
+#endif
+
+
+#ifndef __MatrixVerb_h__
+#define __MatrixVerb_h__
+
+
+#pragma mark ____MatrixVerb Parameters
+
+// parameters
+static const float kDefaultValue_ParamOne = 1.0;
+static const float kDefaultValue_ParamTwo = 0.0;
+static const float kDefaultValue_ParamThree = 0.0;
+static const float kDefaultValue_ParamFour = 0.0;
+static const float kDefaultValue_ParamFive = 0.5;
+static const float kDefaultValue_ParamSix = 0.5;
+static const float kDefaultValue_ParamSeven = 1.0;
+
+static CFStringRef kParameterOneName = CFSTR("Filter");
+static CFStringRef kParameterTwoName = CFSTR("Damping");
+static CFStringRef kParameterThreeName = CFSTR("Speed");
+static CFStringRef kParameterFourName = CFSTR("Vibrato");
+static CFStringRef kParameterFiveName = CFSTR("RmSize");
+static CFStringRef kParameterSixName = CFSTR("Flavor");
+static CFStringRef kParameterSevenName = CFSTR("Dry/Wet");
+
+enum {
+	kParam_One =0,
+	kParam_Two =1,
+	kParam_Three =2,
+	kParam_Four =3,
+	kParam_Five =4,
+	kParam_Six =5,
+	kParam_Seven =6,
+	//Add your parameters here...
+	kNumberOfParameters=7
+};
+
+#pragma mark ____MatrixVerb
+class MatrixVerb : public AUEffectBase
+{
+public:
+	MatrixVerb(AudioUnit component);
+#if AU_DEBUG_DISPATCHER
+	virtual ~MatrixVerb () { delete mDebugDispatcher; }
+#endif
+	
+	virtual AUKernelBase *		NewKernel() { return new MatrixVerbKernel(this); }
+	
+	virtual	ComponentResult		GetParameterValueStrings(AudioUnitScope			inScope,
+														 AudioUnitParameterID		inParameterID,
+														 CFArrayRef *			outStrings);
+    
+	virtual	ComponentResult		GetParameterInfo(AudioUnitScope			inScope,
+												 AudioUnitParameterID	inParameterID,
+												 AudioUnitParameterInfo	&outParameterInfo);
+    
+	virtual ComponentResult		GetPropertyInfo(AudioUnitPropertyID		inID,
+												AudioUnitScope			inScope,
+												AudioUnitElement		inElement,
+												UInt32 &			outDataSize,
+												Boolean	&			outWritable );
+	
+	virtual ComponentResult		GetProperty(AudioUnitPropertyID inID,
+											AudioUnitScope 		inScope,
+											AudioUnitElement 		inElement,
+											void *			outData);
+	
+	virtual ComponentResult    Initialize();
+	virtual bool				SupportsTail () { return true; }
+    virtual Float64				GetTailTime() {return (1.0/GetSampleRate())*0.0;} //in SECONDS! gsr * a number = in samples
+    virtual Float64				GetLatency() {return (1.0/GetSampleRate())*0.0;}	// in SECONDS! gsr * a number = in samples
+	
+	/*! @method Version */
+	virtual ComponentResult		Version() { return kMatrixVerbVersion; }
+	
+    
+	
+protected:
+		class MatrixVerbKernel : public AUKernelBase		// most of the real work happens here
+	{
+public:
+		MatrixVerbKernel(AUEffectBase *inAudioUnit )
+		: AUKernelBase(inAudioUnit)
+	{
+	}
+		
+		// *Required* overides for the process method for this effect
+		// processes one channel of interleaved samples
+        virtual void 		Process(	const Float32 	*inSourceP,
+										Float32		 	*inDestP,
+										UInt32 			inFramesToProcess,
+										UInt32			inNumChannels,
+										bool			&ioSilence);
+		
+        virtual void		Reset();
+		
+		private:
+		long double biquadA[11];
+		long double biquadB[11];
+		long double biquadC[11];
+		
+		Float64 aA[8111];
+		Float64 aB[7511];
+		Float64 aC[7311];
+		Float64 aD[6911];		
+		Float64 aE[6311];
+		Float64 aF[6111];
+		Float64 aG[5511];
+		Float64 aH[4911];
+		Float64 aI[4511];
+		Float64 aJ[4311];
+		Float64 aK[3911];
+		Float64 aL[3311];
+		Float64 aM[3111];
+		
+		int countA, delayA;
+		int countB, delayB;
+		int countC, delayC;
+		int countD, delayD;
+		int countE, delayE;
+		int countF, delayF;
+		int countG, delayG;
+		int countH, delayH;
+		int countI, delayI;
+		int countJ, delayJ;
+		int countK, delayK;
+		int countL, delayL;
+		int countM, delayM;
+		
+		Float64 feedbackA, vibA, depthA;
+		Float64 feedbackB, vibB, depthB;
+		Float64 feedbackC, vibC, depthC;
+		Float64 feedbackD, vibD, depthD;
+		Float64 feedbackE, vibE, depthE;
+		Float64 feedbackF, vibF, depthF;
+		Float64 feedbackG, vibG, depthG;
+		Float64 feedbackH, vibH, depthH;
+		
+		uint32_t fpd;
+	};
+};
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#endif
