88454a8389d1d5c85b3a60e605074ea1faf030fb
Chris Johnson <jinx6568@sover.net>
2021-01-17 20:45:17 -0500

    Channel9

---
+++ plugins/MacAU/Channel9/Channel9.cpp
@@ -0,0 +1,335 @@
+/*
+*	File:		Channel9.cpp
+*	
+*	Version:	1.0
+* 
+*	Created:	1/4/21
+*	
+*	Copyright:  Copyright © 2021 Airwindows, All Rights Reserved
+* 
+*	Disclaimer:	IMPORTANT:  This Apple software is supplied to you by Apple Computer, Inc. ("Apple") in 
+*				consideration of your agreement to the following terms, and your use, installation, modification 
+*				or redistribution of this Apple software constitutes acceptance of these terms.  If you do 
+*				not agree with these terms, please do not use, install, modify or redistribute this Apple 
+*				software.
+*
+*				In consideration of your agreement to abide by the following terms, and subject to these terms, 
+*				Apple grants you a personal, non-exclusive license, under Apple's copyrights in this 
+*				original Apple software (the "Apple Software"), to use, reproduce, modify and redistribute the 
+*				Apple Software, with or without modifications, in source and/or binary forms; provided that if you 
+*				redistribute the Apple Software in its entirety and without modifications, you must retain this 
+*				notice and the following text and disclaimers in all such redistributions of the Apple Software. 
+*				Neither the name, trademarks, service marks or logos of Apple Computer, Inc. may be used to 
+*				endorse or promote products derived from the Apple Software without specific prior written 
+*				permission from Apple.  Except as expressly stated in this notice, no other rights or 
+*				licenses, express or implied, are granted by Apple herein, including but not limited to any 
+*				patent rights that may be infringed by your derivative works or by other works in which the 
+*				Apple Software may be incorporated.
+*
+*				The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO WARRANTIES, EXPRESS OR 
+*				IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY 
+*				AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND OPERATION ALONE 
+*				OR IN COMBINATION WITH YOUR PRODUCTS.
+*
+*				IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL 
+*				DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
+*				OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, 
+*				REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER 
+*				UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR OTHERWISE, EVEN 
+*				IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+/*=============================================================================
+	Channel9.cpp
+	
+=============================================================================*/
+#include "Channel9.h"
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+COMPONENT_ENTRY(Channel9)
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Channel9::Channel9
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Channel9::Channel9(AudioUnit component)
+	: AUEffectBase(component)
+{
+	CreateElements();
+	Globals()->UseIndexedParameters(kNumberOfParameters);
+	SetParameter(kParam_One, kDefaultValue_ParamOne );
+	SetParameter(kParam_Two, kDefaultValue_ParamTwo );
+	SetParameter(kParam_Three, kDefaultValue_ParamThree );
+         
+#if AU_DEBUG_DISPATCHER
+	mDebugDispatcher = new AUDebugDispatcher (this);
+#endif
+	
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Channel9::GetParameterValueStrings
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Channel9::GetParameterValueStrings(AudioUnitScope		inScope,
+                                                                AudioUnitParameterID	inParameterID,
+                                                                CFArrayRef *		outStrings)
+{
+    if ((inScope == kAudioUnitScope_Global) && (inParameterID == kParam_One)) //ID must be actual name of parameter identifier, not number
+	{
+		if (outStrings == NULL) return noErr;
+		CFStringRef strings [] =
+		{
+			kMenuItem_Neve,
+			kMenuItem_API,
+			kMenuItem_SSL,
+			kMenuItem_Teac,
+			kMenuItem_Mackie,
+		};
+		*outStrings = CFArrayCreate (
+									 NULL,
+									 (const void **) strings,
+									 (sizeof (strings) / sizeof (strings [0])),
+									 NULL
+									 );
+		return noErr;
+	}
+    return kAudioUnitErr_InvalidProperty;
+}
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Channel9::GetParameterInfo
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Channel9::GetParameterInfo(AudioUnitScope		inScope,
+                                                        AudioUnitParameterID	inParameterID,
+                                                        AudioUnitParameterInfo	&outParameterInfo )
+{
+	ComponentResult result = noErr;
+
+	outParameterInfo.flags = 	kAudioUnitParameterFlag_IsWritable
+						|		kAudioUnitParameterFlag_IsReadable;
+    
+    if (inScope == kAudioUnitScope_Global) {
+        switch(inParameterID)
+        {
+            case kParam_One:
+                AUBase::FillInParameterName (outParameterInfo, kParameterOneName, false);
+				outParameterInfo.unit = kAudioUnitParameterUnit_Indexed;
+                outParameterInfo.minValue = kNeve;
+                outParameterInfo.maxValue = kMackie;
+                outParameterInfo.defaultValue = kDefaultValue_ParamOne;
+                break;
+            case kParam_Two:
+                AUBase::FillInParameterName (outParameterInfo, kParameterTwoName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Percent;
+                outParameterInfo.minValue = 0.0;
+                outParameterInfo.maxValue = 200.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamTwo;
+                break;
+            case kParam_Three:
+                AUBase::FillInParameterName (outParameterInfo, kParameterThreeName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = 0.0;
+                outParameterInfo.maxValue = 1.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamThree;
+                break;
+           default:
+                result = kAudioUnitErr_InvalidParameter;
+                break;
+            }
+	} else {
+        result = kAudioUnitErr_InvalidParameter;
+    }
+    
+
+
+	return result;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Channel9::GetPropertyInfo
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Channel9::GetPropertyInfo (AudioUnitPropertyID	inID,
+                                                        AudioUnitScope		inScope,
+                                                        AudioUnitElement	inElement,
+                                                        UInt32 &		outDataSize,
+                                                        Boolean &		outWritable)
+{
+	return AUEffectBase::GetPropertyInfo (inID, inScope, inElement, outDataSize, outWritable);
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Channel9::GetProperty
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Channel9::GetProperty(	AudioUnitPropertyID inID,
+                                                        AudioUnitScope 		inScope,
+                                                        AudioUnitElement 	inElement,
+                                                        void *			outData )
+{
+	return AUEffectBase::GetProperty (inID, inScope, inElement, outData);
+}
+
+//	Channel9::Initialize
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult Channel9::Initialize()
+{
+    ComponentResult result = AUEffectBase::Initialize();
+    if (result == noErr)
+        Reset(kAudioUnitScope_Global, 0);
+    return result;
+}
+
+#pragma mark ____Channel9EffectKernel
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Channel9::Channel9Kernel::Reset()
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void		Channel9::Channel9Kernel::Reset()
+{
+	fpd = 1.0; while (fpd < 16386) fpd = rand()*UINT32_MAX;
+	iirSampleA = iirSampleB = 0.0;
+	flip = false;
+	lastSampleA = lastSampleB = lastSampleC = 0.0;
+	for (int x = 0; x < 11; x++) {biquadA[x] = 0.0;biquadB[x] = 0.0;}
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Channel9::Channel9Kernel::Process
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void		Channel9::Channel9Kernel::Process(	const Float32 	*inSourceP,
+                                                    Float32		 	*inDestP,
+                                                    UInt32 			inFramesToProcess,
+                                                    UInt32			inNumChannels, 
+                                                    bool			&ioSilence )
+{
+	UInt32 nSampleFrames = inFramesToProcess;
+	const Float32 *sourceP = inSourceP;
+	Float32 *destP = inDestP;
+	Float64 overallscale = 1.0;
+	overallscale /= 44100.0;
+	overallscale *= GetSampleRate();
+	int console = (int) GetParameter( kParam_One );
+	Float64 density = GetParameter( kParam_Two )/100.0; //0-2
+	Float64 phattity = density - 1.0;
+	if (density > 1.0) density = 1.0; //max out at full wet for Spiral aspect
+	if (phattity < 0.0) phattity = 0.0; //
+	Float64 nonLin = 5.0-density; //number is smaller for more intense, larger for more subtle
+	Float64 output = GetParameter( kParam_Three );
+	Float64 iirAmount = 0.005832;
+	Float64 threshold = 0.33362176;
+	Float64 cutoff = 28811.0;
+
+	switch (console)
+	{
+		case 1: iirAmount = 0.005832; threshold = 0.33362176; cutoff = 28811.0; break; //Neve
+		case 2: iirAmount = 0.004096; threshold = 0.59969536; cutoff = 27216.0; break; //API
+		case 3: iirAmount = 0.004913; threshold = 0.84934656; cutoff = 23011.0; break; //SSL
+		case 4: iirAmount = 0.009216; threshold = 0.149; cutoff = 18544.0; break; //Teac
+		case 5: iirAmount = 0.011449; threshold = 0.092; cutoff = 19748.0; break; //Mackie
+	}
+	iirAmount /= overallscale; //we've learned not to try and adjust threshold for sample rate
+	
+	biquadB[0] = biquadA[0] = cutoff / GetSampleRate();
+    biquadA[1] = 1.618033988749894848204586;
+	biquadB[1] = 0.618033988749894848204586;
+	
+	double K = tan(M_PI * biquadA[0]); //lowpass
+	double norm = 1.0 / (1.0 + K / biquadA[1] + K * K);
+	biquadA[2] = K * K * norm;
+	biquadA[3] = 2.0 * biquadA[2];
+	biquadA[4] = biquadA[2];
+	biquadA[5] = 2.0 * (K * K - 1.0) * norm;
+	biquadA[6] = (1.0 - K / biquadA[1] + K * K) * norm;
+	
+	K = tan(M_PI * biquadA[0]);
+	norm = 1.0 / (1.0 + K / biquadB[1] + K * K);
+	biquadB[2] = K * K * norm;
+	biquadB[3] = 2.0 * biquadB[2];
+	biquadB[4] = biquadB[2];
+	biquadB[5] = 2.0 * (K * K - 1.0) * norm;
+	biquadB[6] = (1.0 - K / biquadB[1] + K * K) * norm;
+	
+	
+	while (nSampleFrames-- > 0) {
+		long double inputSample = *sourceP;
+		if (fabs(inputSample)<1.18e-37) inputSample = fpd * 1.18e-37;
+		
+		if (biquadA[0] < 0.49999) {
+			long double tempSample = biquadA[2]*inputSample+biquadA[3]*biquadA[7]+biquadA[4]*biquadA[8]-biquadA[5]*biquadA[9]-biquadA[6]*biquadA[10];
+			biquadA[8] = biquadA[7]; biquadA[7] = inputSample; inputSample = tempSample; 
+			biquadA[10] = biquadA[9]; biquadA[9] = inputSample; //DF1
+		}
+				
+		Float64 dielectricScale = fabs(2.0-((inputSample+nonLin)/nonLin));
+		
+		if (flip)
+		{
+			iirSampleA = (iirSampleA * (1.0 - (iirAmount * dielectricScale))) + (inputSample * iirAmount * dielectricScale);
+			inputSample = inputSample - iirSampleA;
+		}
+		else
+		{
+			iirSampleB = (iirSampleB * (1.0 - (iirAmount * dielectricScale))) + (inputSample * iirAmount * dielectricScale);
+			inputSample = inputSample - iirSampleB;
+		}
+		//highpass section including capacitor modeling nonlinearity
+		long double drySample = inputSample;
+		
+		if (inputSample > 1.0) inputSample = 1.0;
+		if (inputSample < -1.0) inputSample = -1.0;
+		
+		long double phatSample = sin(inputSample * 1.57079633);
+		inputSample *= 1.2533141373155;
+		//clip to 1.2533141373155 to reach maximum output, or 1.57079633 for pure sine 'phat' version
+		long double distSample = sin(inputSample * fabs(inputSample)) / ((fabs(inputSample) == 0.0) ?1:fabs(inputSample));
+		
+		inputSample = distSample; //purest form is full Spiral
+		if (density < 1.0) inputSample = (drySample*(1-density))+(distSample*density); //fade Spiral aspect
+		if (phattity > 0.0) inputSample = (inputSample*(1-phattity))+(phatSample*phattity); //apply original Density on top
+		
+		Float64 clamp = (lastSampleB - lastSampleC) * 0.381966011250105;
+		clamp -= (lastSampleA - lastSampleB) * 0.6180339887498948482045;
+		clamp += inputSample - lastSampleA; //regular slew clamping added
+		
+		lastSampleC = lastSampleB;
+		lastSampleB = lastSampleA;
+		lastSampleA = inputSample; //now our output relates off lastSampleB
+		
+		if (clamp > threshold)
+			inputSample = lastSampleB + threshold;
+		if (-clamp > threshold)
+			inputSample = lastSampleB - threshold;
+		//slew section		
+		lastSampleA = (lastSampleA*0.381966011250105)+(inputSample*0.6180339887498948482045); //split the difference between raw and smoothed for buffer
+		
+		flip = !flip;
+		
+		if (output < 1.0)
+		{
+			inputSample *= output;
+		}
+		
+		if (biquadB[0] < 0.49999) {
+			long double tempSample = biquadB[2]*inputSample+biquadB[3]*biquadB[7]+biquadB[4]*biquadB[8]-biquadB[5]*biquadB[9]-biquadB[6]*biquadB[10];
+			biquadB[8] = biquadB[7]; biquadB[7] = inputSample; inputSample = tempSample; 
+			biquadB[10] = biquadB[9]; biquadB[9] = inputSample; //DF1
+		}
+		
+		//begin 32 bit floating point dither
+		int expon; frexpf((float)inputSample, &expon);
+		fpd ^= fpd << 13; fpd ^= fpd >> 17; fpd ^= fpd << 5;
+		inputSample += ((double(fpd)-uint32_t(0x7fffffff)) * 5.5e-36l * pow(2,expon+62));
+		//end 32 bit floating point dither
+		
+		*destP = inputSample;
+		
+		sourceP += inNumChannels; destP += inNumChannels;
+	}
+}
+
---
+++ plugins/MacVST/Channel9/source/Channel9.cpp
@@ -0,0 +1,163 @@
+/* ========================================
+ *  Channel9 - Channel9.h
+ *  Copyright (c) 2016 airwindows, All rights reserved
+ * ======================================== */
+
+#ifndef __Channel9_H
+#include "Channel9.h"
+#endif
+
+AudioEffect* createEffectInstance(audioMasterCallback audioMaster) {return new Channel9(audioMaster);}
+
+Channel9::Channel9(audioMasterCallback audioMaster) :
+    AudioEffectX(audioMaster, kNumPrograms, kNumParameters)
+{
+	A = 0.0;
+	B = 0.0;
+	C = 1.0;
+	for (int x = 0; x < 15; x++) {biquadA[x] = 0.0; biquadB[x] = 0.0;}
+	fpdL = 1.0; while (fpdL < 16386) fpdL = rand()*UINT32_MAX;
+	fpdR = 1.0; while (fpdR < 16386) fpdR = rand()*UINT32_MAX;
+	iirSampleLA = 0.0;
+	iirSampleRA = 0.0;
+	iirSampleLB = 0.0;
+	iirSampleRB = 0.0;
+	lastSampleAL = lastSampleBL = lastSampleCL = 0.0;
+	lastSampleAR = lastSampleBR = lastSampleCR = 0.0;
+	flip = false;
+	iirAmount = 0.005832;
+	threshold = 0.33362176; //instantiating with Neve values
+	cutoff = 28811.0;
+	//this is reset: values being initialized only once. Startup values, whatever they are.
+	
+    _canDo.insert("plugAsChannelInsert"); // plug-in can be used as a channel insert effect.
+    _canDo.insert("plugAsSend"); // plug-in can be used as a send effect.
+    _canDo.insert("x2in2out"); 
+    setNumInputs(kNumInputs);
+    setNumOutputs(kNumOutputs);
+    setUniqueID(kUniqueId);
+    canProcessReplacing();     // supports output replacing
+    canDoubleReplacing();      // supports double precision processing
+	programsAreChunks(true);
+    vst_strncpy (_programName, "Default", kVstMaxProgNameLen); // default program name
+}
+
+Channel9::~Channel9() {}
+VstInt32 Channel9::getVendorVersion () {return 1000;}
+void Channel9::setProgramName(char *name) {vst_strncpy (_programName, name, kVstMaxProgNameLen);}
+void Channel9::getProgramName(char *name) {vst_strncpy (name, _programName, kVstMaxProgNameLen);}
+//airwindows likes to ignore this stuff. Make your own programs, and make a different plugin rather than
+//trying to do versioning and preventing people from using older versions. Maybe they like the old one!
+
+static float pinParameter(float data)
+{
+	if (data < 0.0f) return 0.0f;
+	if (data > 1.0f) return 1.0f;
+	return data;
+}
+
+VstInt32 Channel9::getChunk (void** data, bool isPreset)
+{
+	float *chunkData = (float *)calloc(kNumParameters, sizeof(float));
+	chunkData[0] = A;
+	chunkData[1] = B;
+	chunkData[2] = C;
+	/* Note: The way this is set up, it will break if you manage to save settings on an Intel
+	 machine and load them on a PPC Mac. However, it's fine if you stick to the machine you 
+	 started with. */
+	
+	*data = chunkData;
+	return kNumParameters * sizeof(float);
+}
+
+VstInt32 Channel9::setChunk (void* data, VstInt32 byteSize, bool isPreset)
+{	
+	float *chunkData = (float *)data;
+	A = pinParameter(chunkData[0]);
+	B = pinParameter(chunkData[1]);
+	C = pinParameter(chunkData[2]);
+	/* We're ignoring byteSize as we found it to be a filthy liar */
+	
+	/* calculate any other fields you need here - you could copy in 
+	 code from setParameter() here. */
+	return 0;
+}
+
+void Channel9::setParameter(VstInt32 index, float value) {
+    switch (index) {
+        case kParamA: A = value; break;
+        case kParamB: B = value; break;
+        case kParamC: C = value; break;
+        default: throw; // unknown parameter, shouldn't happen!
+    }
+	switch((VstInt32)( A * 4.999 ))
+	{  
+		case 0: iirAmount = 0.005832; threshold = 0.33362176; cutoff = 28811.0; break; //Neve
+		case 1: iirAmount = 0.004096; threshold = 0.59969536; cutoff = 27216.0; break; //API
+		case 2: iirAmount = 0.004913; threshold = 0.84934656; cutoff = 23011.0; break; //SSL
+		case 3: iirAmount = 0.009216; threshold = 0.149; cutoff = 18544.0; break; //Teac
+		case 4: iirAmount = 0.011449; threshold = 0.092; cutoff = 19748.0; break; //Mackie
+		default: break; //should not happen
+	}
+}
+
+float Channel9::getParameter(VstInt32 index) {
+    switch (index) {
+        case kParamA: return A; break;
+        case kParamB: return B; break;
+        case kParamC: return C; break;
+        default: break; // unknown parameter, shouldn't happen!
+    } return 0.0; //we only need to update the relevant name, this is simple to manage
+}
+
+void Channel9::getParameterName(VstInt32 index, char *text) {
+    switch (index) {
+        case kParamA: vst_strncpy (text, "Console Type", kVstMaxParamStrLen); break;
+		case kParamB: vst_strncpy (text, "Drive", kVstMaxParamStrLen); break;
+		case kParamC: vst_strncpy (text, "Output", kVstMaxParamStrLen); break;
+        default: break; // unknown parameter, shouldn't happen!
+    } //this is our labels for displaying in the VST host
+}
+
+void Channel9::getParameterDisplay(VstInt32 index, char *text) {
+    switch (index) {
+        case kParamA: switch((VstInt32)( A * 4.999 )) //0 to almost edge of # of params
+		{	case 0: vst_strncpy (text, "Neve", kVstMaxParamStrLen); break;
+			case 1: vst_strncpy (text, "API", kVstMaxParamStrLen); break;
+			case 2: vst_strncpy (text, "SSL", kVstMaxParamStrLen); break;
+			case 3: vst_strncpy (text, "Teac", kVstMaxParamStrLen); break;
+			case 4: vst_strncpy (text, "Mackie", kVstMaxParamStrLen); break;
+			default: break; // unknown parameter, shouldn't happen!
+		} break; //completed consoletype 'popup' parameter, exit
+		case kParamB: int2string ((VstInt32)(B*200), text, kVstMaxParamStrLen); break;
+		case kParamC: float2string (C, text, kVstMaxParamStrLen); break;
+        default: break; // unknown parameter, shouldn't happen!
+	} //this displays the values and handles 'popups' where it's discrete choices
+}
+
+void Channel9::getParameterLabel(VstInt32 index, char *text) {
+    switch (index) {
+        case kParamA: vst_strncpy (text, "", kVstMaxParamStrLen); break;
+        case kParamB: vst_strncpy (text, "%", kVstMaxParamStrLen); break; //the percent
+        case kParamC: vst_strncpy (text, "", kVstMaxParamStrLen); break; //the percent
+		default: break; // unknown parameter, shouldn't happen!
+    }
+}
+
+
+VstInt32 Channel9::canDo(char *text) 
+{ return (_canDo.find(text) == _canDo.end()) ? -1: 1; } // 1 = yes, -1 = no, 0 = don't know
+
+bool Channel9::getEffectName(char* name) {
+    vst_strncpy(name, "Channel9", kVstMaxProductStrLen); return true;
+}
+
+VstPlugCategory Channel9::getPlugCategory() {return kPlugCategEffect;}
+
+bool Channel9::getProductString(char* text) {
+  	vst_strncpy (text, "airwindows Channel9", kVstMaxProductStrLen); return true;
+}
+
+bool Channel9::getVendorString(char* text) {
+  	vst_strncpy (text, "airwindows", kVstMaxVendorStrLen); return true;
+}
