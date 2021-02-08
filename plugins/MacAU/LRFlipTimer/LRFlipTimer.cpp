f885ba303eaab68a8de079400c37c9cbf82b37c9
Chris Johnson <jinx6568@sover.net>
2021-02-07 20:04:16 -0500

    LRFlipTimer

---
+++ plugins/MacAU/LRFlipTimer/LRFlipTimer.cpp
@@ -0,0 +1,258 @@
+/*
+*	File:		LRFlipTimer.cpp
+*	
+*	Version:	1.0
+* 
+*	Created:	1/29/21
+*	
+*	Copyright:  Copyright © 2021 Airwindows, All Rights Reserved
+* 
+*	Disclaimer:	IMPORTANT:  This Apple software is supplied to you by Apple Computer, Inc. ("Apple") in 
+*				consideration of your agreement to the following terms, and your use, installation, modification 
+*				or redistribution of this Apple software constitutes acceptance of these terms.  If you do 
+*				not agree with these terms, please do not use, install, modify or redistribute this Apple 
+*				software.
+*
+*				In consideration of your agreement to abide by the following terms, and subject to these terms, 
+*				Apple grants you a personal, non-exclusive license, under Apple's copyrights in this 
+*				original Apple software (the "Apple Software"), to use, reproduce, modify and redistribute the 
+*				Apple Software, with or without modifications, in source and/or binary forms; provided that if you 
+*				redistribute the Apple Software in its entirety and without modifications, you must retain this 
+*				notice and the following text and disclaimers in all such redistributions of the Apple Software. 
+*				Neither the name, trademarks, service marks or logos of Apple Computer, Inc. may be used to 
+*				endorse or promote products derived from the Apple Software without specific prior written 
+*				permission from Apple.  Except as expressly stated in this notice, no other rights or 
+*				licenses, express or implied, are granted by Apple herein, including but not limited to any 
+*				patent rights that may be infringed by your derivative works or by other works in which the 
+*				Apple Software may be incorporated.
+*
+*				The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO WARRANTIES, EXPRESS OR 
+*				IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY 
+*				AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND OPERATION ALONE 
+*				OR IN COMBINATION WITH YOUR PRODUCTS.
+*
+*				IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL 
+*				DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
+*				OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, 
+*				REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER 
+*				UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR OTHERWISE, EVEN 
+*				IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+/*=============================================================================
+	LRFlipTimer.cpp
+	
+=============================================================================*/
+#include "LRFlipTimer.h"
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+COMPONENT_ENTRY(LRFlipTimer)
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	LRFlipTimer::LRFlipTimer
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+LRFlipTimer::LRFlipTimer(AudioUnit component)
+	: AUEffectBase(component)
+{
+	CreateElements();
+	Globals()->UseIndexedParameters(kNumberOfParameters);
+	SetParameter(kParam_One, kDefaultValue_ParamOne );
+         
+#if AU_DEBUG_DISPATCHER
+	mDebugDispatcher = new AUDebugDispatcher (this);
+#endif
+	
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	LRFlipTimer::GetParameterValueStrings
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			LRFlipTimer::GetParameterValueStrings(AudioUnitScope		inScope,
+                                                                AudioUnitParameterID	inParameterID,
+                                                                CFArrayRef *		outStrings)
+{
+        
+    return kAudioUnitErr_InvalidProperty;
+}
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	LRFlipTimer::GetParameterInfo
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			LRFlipTimer::GetParameterInfo(AudioUnitScope		inScope,
+                                                        AudioUnitParameterID	inParameterID,
+                                                        AudioUnitParameterInfo	&outParameterInfo )
+{
+	ComponentResult result = noErr;
+
+	outParameterInfo.flags = 	kAudioUnitParameterFlag_IsWritable
+						|		kAudioUnitParameterFlag_IsReadable;
+    
+    if (inScope == kAudioUnitScope_Global) {
+        switch(inParameterID)
+        {
+           case kParam_One:
+                AUBase::FillInParameterName (outParameterInfo, kParameterOneName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = 1.0;
+                outParameterInfo.maxValue = 10.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamOne;
+                break;
+           default:
+                result = kAudioUnitErr_InvalidParameter;
+                break;
+            }
+	} else {
+        result = kAudioUnitErr_InvalidParameter;
+    }
+    
+
+
+	return result;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	LRFlipTimer::GetPropertyInfo
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			LRFlipTimer::GetPropertyInfo (AudioUnitPropertyID	inID,
+                                                        AudioUnitScope		inScope,
+                                                        AudioUnitElement	inElement,
+                                                        UInt32 &		outDataSize,
+                                                        Boolean &		outWritable)
+{
+	return AUEffectBase::GetPropertyInfo (inID, inScope, inElement, outDataSize, outWritable);
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// state that plugin supports only stereo-in/stereo-out processing
+UInt32 LRFlipTimer::SupportedNumChannels(const AUChannelInfo ** outInfo)
+{
+	if (outInfo != NULL)
+	{
+		static AUChannelInfo info;
+		info.inChannels = 2;
+		info.outChannels = 2;
+		*outInfo = &info;
+	}
+
+	return 1;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	LRFlipTimer::GetProperty
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			LRFlipTimer::GetProperty(	AudioUnitPropertyID inID,
+                                                        AudioUnitScope 		inScope,
+                                                        AudioUnitElement 	inElement,
+                                                        void *			outData )
+{
+	return AUEffectBase::GetProperty (inID, inScope, inElement, outData);
+}
+
+//	LRFlipTimer::Initialize
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult LRFlipTimer::Initialize()
+{
+    ComponentResult result = AUEffectBase::Initialize();
+    if (result == noErr)
+        Reset(kAudioUnitScope_Global, 0);
+    return result;
+}
+
+#pragma mark ____LRFlipTimerEffectKernel
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	LRFlipTimer::LRFlipTimerKernel::Reset()
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult		LRFlipTimer::Reset(AudioUnitScope inScope, AudioUnitElement inElement)
+{
+	tick = 0;
+	return noErr;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	LRFlipTimer::ProcessBufferLists
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+OSStatus		LRFlipTimer::ProcessBufferLists(AudioUnitRenderActionFlags & ioActionFlags,
+													const AudioBufferList & inBuffer,
+                                                    AudioBufferList & outBuffer,
+                                                    UInt32 			inFramesToProcess)
+{
+	Float32 * inputL = (Float32*)(inBuffer.mBuffers[0].mData);
+	Float32 * inputR = (Float32*)(inBuffer.mBuffers[1].mData);
+	Float32 * outputL = (Float32*)(outBuffer.mBuffers[0].mData);
+	Float32 * outputR = (Float32*)(outBuffer.mBuffers[1].mData);
+	UInt32 nSampleFrames = inFramesToProcess;
+	
+	uint32_t time = (uint32_t)(GetParameter( kParam_One ) * 60.0 * GetSampleRate());
+	uint32_t transition = (uint32_t)(GetSampleRate()*0.1);
+	//number of minutes * 60 seconds * samples per second
+	//transition: tenth of a second crossfade
+
+	while (nSampleFrames-- > 0) {
+		Float32 inputSampleL = *inputL;
+		Float32 inputSampleR = *inputR;
+		Float32 outputSampleL = inputSampleL;
+		Float32 outputSampleR = inputSampleR;
+		
+		tick++;
+		if (tick > time*2) tick = 0;
+		
+		if (tick < time) {
+			if (tick < transition) {
+				Float64 crossfade;
+				crossfade = tick;
+				crossfade /= transition;
+				//0 to 1 from beginning swapped to end as direct stereo out
+				if (crossfade < 0.5) {
+					outputSampleL = (inputSampleL*crossfade)+(inputSampleR*(1.0-crossfade));
+					outputSampleR = (inputSampleR*crossfade)+(inputSampleL*(1.0-crossfade));
+				} else {
+					crossfade = fabs(crossfade-1.0);
+					outputSampleL = (inputSampleR*crossfade)+(inputSampleL*(1.0-crossfade));
+					outputSampleR = (inputSampleL*crossfade)+(inputSampleR*(1.0-crossfade));
+				}
+			} else {
+				outputSampleL = inputSampleL;
+				outputSampleR = inputSampleR;
+				//direct stereo out
+			}
+		} else {
+			if ((tick - time) < transition) {
+				Float64 crossfade;
+				crossfade = tick-time;
+				crossfade /= transition;
+				//0 to 1 from beginning direct to end as swapped stereo
+				if (crossfade < 0.5) {
+					outputSampleL = (inputSampleR*crossfade)+(inputSampleL*(1.0-crossfade));
+					outputSampleR = (inputSampleL*crossfade)+(inputSampleR*(1.0-crossfade));
+				} else {
+					crossfade = fabs(crossfade-1.0);
+					outputSampleL = (inputSampleL*crossfade)+(inputSampleR*(1.0-crossfade));
+					outputSampleR = (inputSampleR*crossfade)+(inputSampleL*(1.0-crossfade));
+				}
+			} else {
+				outputSampleL = inputSampleR;
+				outputSampleR = inputSampleL;
+				//swapped stereo out
+			}
+		}
+		
+		*outputL = outputSampleL;
+		*outputR = outputSampleR;
+		
+		inputL += 1;
+		inputR += 1;
+		outputL += 1;
+		outputR += 1;
+	}
+	return noErr;
+}
+
---
+++ plugins/WinVST/LRFlipTimer/LRFlipTimerProc.cpp
@@ -0,0 +1,152 @@
+/* ========================================
+ *  LRFlipTimer - LRFlipTimer.h
+ *  Copyright (c) 2016 airwindows, All rights reserved
+ * ======================================== */
+
+#ifndef __LRFlipTimer_H
+#include "LRFlipTimer.h"
+#endif
+
+void LRFlipTimer::processReplacing(float **inputs, float **outputs, VstInt32 sampleFrames) 
+{
+    float* in1  =  inputs[0];
+    float* in2  =  inputs[1];
+    float* out1 = outputs[0];
+    float* out2 = outputs[1];
+	
+	uint32_t time = (uint32_t)(((A*9.0)+1.0) * 60.0 * getSampleRate());
+	uint32_t transition = (uint32_t)(getSampleRate()*0.1);
+	//number of minutes * 60 seconds * samples per second
+	//transition: tenth of a second crossfade
+	
+    while (--sampleFrames >= 0)
+    {
+		float inputSampleL = *in1;
+		float inputSampleR = *in2;
+		float outputSampleL = inputSampleL;
+		float outputSampleR = inputSampleR;
+		
+		tick++;
+		if (tick > time*2) tick = 0;
+		
+		if (tick < time) {
+			if (tick < transition) {
+				double crossfade;
+				crossfade = tick;
+				crossfade /= transition;
+				//0 to 1 from beginning swapped to end as direct stereo out
+				if (crossfade < 0.5) {
+					outputSampleL = (inputSampleL*crossfade)+(inputSampleR*(1.0-crossfade));
+					outputSampleR = (inputSampleR*crossfade)+(inputSampleL*(1.0-crossfade));
+				} else {
+					crossfade = fabs(crossfade-1.0);
+					outputSampleL = (inputSampleR*crossfade)+(inputSampleL*(1.0-crossfade));
+					outputSampleR = (inputSampleL*crossfade)+(inputSampleR*(1.0-crossfade));
+				}
+			} else {
+				outputSampleL = inputSampleL;
+				outputSampleR = inputSampleR;
+				//direct stereo out
+			}
+		} else {
+			if ((tick - time) < transition) {
+				double crossfade;
+				crossfade = tick-time;
+				crossfade /= transition;
+				//0 to 1 from beginning direct to end as swapped stereo
+				if (crossfade < 0.5) {
+					outputSampleL = (inputSampleR*crossfade)+(inputSampleL*(1.0-crossfade));
+					outputSampleR = (inputSampleL*crossfade)+(inputSampleR*(1.0-crossfade));
+				} else {
+					crossfade = fabs(crossfade-1.0);
+					outputSampleL = (inputSampleL*crossfade)+(inputSampleR*(1.0-crossfade));
+					outputSampleR = (inputSampleR*crossfade)+(inputSampleL*(1.0-crossfade));
+				}
+			} else {
+				outputSampleL = inputSampleR;
+				outputSampleR = inputSampleL;
+				//swapped stereo out
+			}
+		}
+		
+		*out1 = outputSampleL;
+		*out2 = outputSampleR;
+
+		*in1++;
+		*in2++;
+		*out1++;
+		*out2++;
+    }
+}
+
+void LRFlipTimer::processDoubleReplacing(double **inputs, double **outputs, VstInt32 sampleFrames) 
+{
+    double* in1  =  inputs[0];
+    double* in2  =  inputs[1];
+    double* out1 = outputs[0];
+    double* out2 = outputs[1];
+	
+	uint32_t time = (uint32_t)(((A*9.0)+1.0) * 60.0 * getSampleRate());
+	uint32_t transition = (uint32_t)(getSampleRate()*0.1);
+	//number of minutes * 60 seconds * samples per second
+	//transition: tenth of a second crossfade
+
+    while (--sampleFrames >= 0)
+    {
+		double inputSampleL = *in1;
+		double inputSampleR = *in2;
+		double outputSampleL = inputSampleL;
+		double outputSampleR = inputSampleR;
+		
+		tick++;
+		if (tick > time*2) tick = 0;
+		
+		if (tick < time) {
+			if (tick < transition) {
+				double crossfade;
+				crossfade = tick;
+				crossfade /= transition;
+				//0 to 1 from beginning swapped to end as direct stereo out
+				if (crossfade < 0.5) {
+					outputSampleL = (inputSampleL*crossfade)+(inputSampleR*(1.0-crossfade));
+					outputSampleR = (inputSampleR*crossfade)+(inputSampleL*(1.0-crossfade));
+				} else {
+					crossfade = fabs(crossfade-1.0);
+					outputSampleL = (inputSampleR*crossfade)+(inputSampleL*(1.0-crossfade));
+					outputSampleR = (inputSampleL*crossfade)+(inputSampleR*(1.0-crossfade));
+				}
+			} else {
+				outputSampleL = inputSampleL;
+				outputSampleR = inputSampleR;
+				//direct stereo out
+			}
+		} else {
+			if ((tick - time) < transition) {
+				double crossfade;
+				crossfade = tick-time;
+				crossfade /= transition;
+				//0 to 1 from beginning direct to end as swapped stereo
+				if (crossfade < 0.5) {
+					outputSampleL = (inputSampleR*crossfade)+(inputSampleL*(1.0-crossfade));
+					outputSampleR = (inputSampleL*crossfade)+(inputSampleR*(1.0-crossfade));
+				} else {
+					crossfade = fabs(crossfade-1.0);
+					outputSampleL = (inputSampleL*crossfade)+(inputSampleR*(1.0-crossfade));
+					outputSampleR = (inputSampleR*crossfade)+(inputSampleL*(1.0-crossfade));
+				}
+			} else {
+				outputSampleL = inputSampleR;
+				outputSampleR = inputSampleL;
+				//swapped stereo out
+			}
+		}
+		
+		*out1 = outputSampleL;
+		*out2 = outputSampleR;
+
+		*in1++;
+		*in2++;
+		*out1++;
+		*out2++;
+    }
+}
