f6479689854102d2d22b8e73b99296c49fcb6f3c
Christopher Johnson <christopherjohnson@Christophers-MacBook-Pro.local>
2021-05-03 05:56:50 -0400

    MackEQ

---
+++ plugins/MacSignedAU/MackEQ/MackEQ.cpp
@@ -0,0 +1,366 @@
+/*
+*	File:		MackEQ.cpp
+*	
+*	Version:	1.0
+* 
+*	Created:	4/5/21
+*	
+*	Copyright:  Copyright © 2021 Airwindows, All Rights Reserved
+* 
+*	Disclaimer:	IMPORTANT:  This Apple software is supplied to you by Apple Computer, Inc. ("Apple") in 
+*				consideration of your agreement to the following terms, and your use, installation, modification 
+*				or redistribution of this Apple software constitutes acceptance of these terms.  If you do 
+*				not agree with these terms, please do not use, install, modify or redistribute this Apple 
+*				software.
+*
+*				In consideration of your agreement to abide by the following terms, and subject to these terms, 
+*				Apple grants you a personal, non-exclusive license, under Apple's copyrights in this 
+*				original Apple software (the "Apple Software"), to use, reproduce, modify and redistribute the 
+*				Apple Software, with or without modifications, in source and/or binary forms; provided that if you 
+*				redistribute the Apple Software in its entirety and without modifications, you must retain this 
+*				notice and the following text and disclaimers in all such redistributions of the Apple Software. 
+*				Neither the name, trademarks, service marks or logos of Apple Computer, Inc. may be used to 
+*				endorse or promote products derived from the Apple Software without specific prior written 
+*				permission from Apple.  Except as expressly stated in this notice, no other rights or 
+*				licenses, express or implied, are granted by Apple herein, including but not limited to any 
+*				patent rights that may be infringed by your derivative works or by other works in which the 
+*				Apple Software may be incorporated.
+*
+*				The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO WARRANTIES, EXPRESS OR 
+*				IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY 
+*				AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND OPERATION ALONE 
+*				OR IN COMBINATION WITH YOUR PRODUCTS.
+*
+*				IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL 
+*				DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
+*				OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, 
+*				REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER 
+*				UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR OTHERWISE, EVEN 
+*				IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+/*=============================================================================
+	MackEQ.cpp
+	
+=============================================================================*/
+#include "MackEQ.h"
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+AUDIOCOMPONENT_ENTRY(AUBaseFactory, MackEQ)
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	MackEQ::MackEQ
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+MackEQ::MackEQ(AudioUnit component)
+	: AUEffectBase(component)
+{
+	CreateElements();
+	Globals()->UseIndexedParameters(kNumberOfParameters);
+	SetParameter(kParam_One, kDefaultValue_ParamOne );
+	SetParameter(kParam_Two, kDefaultValue_ParamTwo );
+	SetParameter(kParam_Three, kDefaultValue_ParamThree );
+	SetParameter(kParam_Four, kDefaultValue_ParamFour );
+	SetParameter(kParam_Five, kDefaultValue_ParamFive );
+         
+#if AU_DEBUG_DISPATCHER
+	mDebugDispatcher = new AUDebugDispatcher (this);
+#endif
+	
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	MackEQ::GetParameterValueStrings
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			MackEQ::GetParameterValueStrings(AudioUnitScope		inScope,
+                                                                AudioUnitParameterID	inParameterID,
+                                                                CFArrayRef *		outStrings)
+{
+        
+    return kAudioUnitErr_InvalidProperty;
+}
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	MackEQ::GetParameterInfo
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			MackEQ::GetParameterInfo(AudioUnitScope		inScope,
+                                                        AudioUnitParameterID	inParameterID,
+                                                        AudioUnitParameterInfo	&outParameterInfo )
+{
+	ComponentResult result = noErr;
+
+	outParameterInfo.flags = 	kAudioUnitParameterFlag_IsWritable
+						|		kAudioUnitParameterFlag_IsReadable;
+    
+    if (inScope == kAudioUnitScope_Global) {
+        switch(inParameterID)
+        {
+           case kParam_One:
+                AUBase::FillInParameterName (outParameterInfo, kParameterOneName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = 0.0;
+                outParameterInfo.maxValue = 1.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamOne;
+                break;
+            case kParam_Two:
+                AUBase::FillInParameterName (outParameterInfo, kParameterTwoName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = 0.0;
+                outParameterInfo.maxValue = 1.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamTwo;
+                break;
+            case kParam_Three:
+                AUBase::FillInParameterName (outParameterInfo, kParameterThreeName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = 0.0;
+                outParameterInfo.maxValue = 1.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamThree;
+                break;
+           case kParam_Four:
+                AUBase::FillInParameterName (outParameterInfo, kParameterFourName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = 0.0;
+                outParameterInfo.maxValue = 1.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamFour;
+                break;
+           case kParam_Five:
+                AUBase::FillInParameterName (outParameterInfo, kParameterFiveName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = 0.0;
+                outParameterInfo.maxValue = 1.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamFive;
+                break;
+           default:
+                result = kAudioUnitErr_InvalidParameter;
+                break;
+            }
+	} else {
+        result = kAudioUnitErr_InvalidParameter;
+    }
+    
+
+
+	return result;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	MackEQ::GetPropertyInfo
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			MackEQ::GetPropertyInfo (AudioUnitPropertyID	inID,
+                                                        AudioUnitScope		inScope,
+                                                        AudioUnitElement	inElement,
+                                                        UInt32 &		outDataSize,
+                                                        Boolean &		outWritable)
+{
+	return AUEffectBase::GetPropertyInfo (inID, inScope, inElement, outDataSize, outWritable);
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	MackEQ::GetProperty
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			MackEQ::GetProperty(	AudioUnitPropertyID inID,
+                                                        AudioUnitScope 		inScope,
+                                                        AudioUnitElement 	inElement,
+                                                        void *			outData )
+{
+	return AUEffectBase::GetProperty (inID, inScope, inElement, outData);
+}
+
+//	MackEQ::Initialize
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult MackEQ::Initialize()
+{
+    ComponentResult result = AUEffectBase::Initialize();
+    if (result == noErr)
+        Reset(kAudioUnitScope_Global, 0);
+    return result;
+}
+
+#pragma mark ____MackEQEffectKernel
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	MackEQ::MackEQKernel::Reset()
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void		MackEQ::MackEQKernel::Reset()
+{
+	iirSampleA = 0.0;
+	iirSampleB = 0.0;
+	iirSampleC = 0.0;
+	iirSampleD = 0.0;
+	iirSampleE = 0.0;
+	iirSampleF = 0.0;
+	for (int x = 0; x < 11; x++) {biquadA[x] = 0.0; biquadB[x] = 0.0; biquadC[x] = 0.0; biquadD[x] = 0.0;}
+	fpd = 1.0; while (fpd < 16386) fpd = rand()*UINT32_MAX;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	MackEQ::MackEQKernel::Process
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void		MackEQ::MackEQKernel::Process(	const Float32 	*inSourceP,
+                                                    Float32		 	*inDestP,
+                                                    UInt32 			inFramesToProcess,
+                                                    UInt32			inNumChannels, 
+                                                    bool			&ioSilence )
+{
+	UInt32 nSampleFrames = inFramesToProcess;
+	const Float32 *sourceP = inSourceP;
+	Float32 *destP = inDestP;
+	long double overallscale = 1.0;
+	overallscale /= 44100.0;
+	overallscale *= GetSampleRate();
+	
+	double inTrim = GetParameter( kParam_One )*10.0;
+	inTrim *= inTrim;
+	
+	double gainHigh = pow(GetParameter( kParam_Two ),2)*4.0;
+	double outHigh = sqrt(GetParameter( kParam_Two ));
+	double gainBass = pow(GetParameter( kParam_Three ),2)*4.0;
+	double outBass = sqrt(GetParameter( kParam_Three ));
+	double outPad = GetParameter( kParam_Four );
+	double wet = GetParameter( kParam_Five );
+	
+	double iirAmountA = 0.001860867/overallscale;
+	double iirAmountB = 0.000287496/overallscale;
+	double iirBassMid = 0.159/overallscale;
+	double iirMidHigh = 0.236/overallscale;
+	
+	biquadD[0] = biquadC[0] = biquadB[0] = biquadA[0] = 19160.0 / GetSampleRate();
+    biquadA[1] = 0.431684981684982;
+	biquadB[1] = 1.1582298;
+	biquadC[1] = 0.657027382751269;
+	biquadD[1] = 1.076210852946577;
+	
+	double K = tan(M_PI * biquadA[0]); //lowpass
+	double norm = 1.0 / (1.0 + K / biquadA[1] + K * K);
+	biquadA[2] = K * K * norm;
+	biquadA[3] = 2.0 * biquadA[2];
+	biquadA[4] = biquadA[2];
+	biquadA[5] = 2.0 * (K * K - 1.0) * norm;
+	biquadA[6] = (1.0 - K / biquadA[1] + K * K) * norm;
+	
+	K = tan(M_PI * biquadB[0]);
+	norm = 1.0 / (1.0 + K / biquadB[1] + K * K);
+	biquadB[2] = K * K * norm;
+	biquadB[3] = 2.0 * biquadB[2];
+	biquadB[4] = biquadB[2];
+	biquadB[5] = 2.0 * (K * K - 1.0) * norm;
+	biquadB[6] = (1.0 - K / biquadB[1] + K * K) * norm;
+	
+	K = tan(M_PI * biquadC[0]);
+	norm = 1.0 / (1.0 + K / biquadC[1] + K * K);
+	biquadC[2] = K * K * norm;
+	biquadC[3] = 2.0 * biquadC[2];
+	biquadC[4] = biquadC[2];
+	biquadC[5] = 2.0 * (K * K - 1.0) * norm;
+	biquadC[6] = (1.0 - K / biquadC[1] + K * K) * norm;
+	
+	K = tan(M_PI * biquadD[0]);
+	norm = 1.0 / (1.0 + K / biquadD[1] + K * K);
+	biquadD[2] = K * K * norm;
+	biquadD[3] = 2.0 * biquadD[2];
+	biquadD[4] = biquadD[2];
+	biquadD[5] = 2.0 * (K * K - 1.0) * norm;
+	biquadD[6] = (1.0 - K / biquadD[1] + K * K) * norm;
+		
+	while (nSampleFrames-- > 0) {
+		long double inputSample = *sourceP;
+		if (fabs(inputSample)<1.18e-37) inputSample = fpd * 1.18e-37;
+		double drySample = inputSample;
+
+		if (fabs(iirSampleA)<1.18e-37) iirSampleA = 0.0;
+		iirSampleA = (iirSampleA * (1.0 - iirAmountA)) + (inputSample * iirAmountA);
+		inputSample -= iirSampleA;
+		
+		if (inTrim != 1.0) inputSample *= inTrim;
+		
+		//begin Mackity input stage
+		long double outSample = biquadA[2]*inputSample+biquadA[3]*biquadA[7]+biquadA[4]*biquadA[8]-biquadA[5]*biquadA[9]-biquadA[6]*biquadA[10];
+		biquadA[8] = biquadA[7]; biquadA[7] = inputSample; inputSample = outSample; biquadA[10] = biquadA[9]; biquadA[9] = inputSample; //DF1		
+		
+		if (inputSample > 1.0) inputSample = 1.0;
+		if (inputSample < -1.0) inputSample = -1.0;
+		inputSample -= pow(inputSample,5)*0.1768;
+		
+		outSample = biquadB[2]*inputSample+biquadB[3]*biquadB[7]+biquadB[4]*biquadB[8]-biquadB[5]*biquadB[9]-biquadB[6]*biquadB[10];
+		biquadB[8] = biquadB[7]; biquadB[7] = inputSample; inputSample = outSample; biquadB[10] = biquadB[9]; biquadB[9] = inputSample; //DF1
+		
+		if (fabs(iirSampleB)<1.18e-37) iirSampleB = 0.0;
+		iirSampleB = (iirSampleB * (1.0 - iirAmountB)) + (inputSample * iirAmountB);
+		inputSample -= iirSampleB;
+		//end Mackity input stage
+		
+		//begin EQ section
+		if (fabs(iirSampleC)<1.18e-37) iirSampleC = 0.0;
+		iirSampleC = (iirSampleC * (1.0 - iirBassMid)) + (inputSample * iirBassMid);
+		
+		long double bassSample = iirSampleC;
+		long double midSample = inputSample - bassSample;
+
+		if (gainBass != 1.0) bassSample *= gainBass;
+		if (bassSample > 1.0) bassSample = 1.0;
+		if (bassSample < -1.0) bassSample = -1.0;
+		bassSample -= pow(bassSample,5)*0.1768;
+		
+		if (fabs(iirSampleD)<1.18e-37) iirSampleD = 0.0;
+		iirSampleD = (iirSampleD * (1.0 - iirBassMid)) + (bassSample * iirBassMid);
+		bassSample = iirSampleD;
+		//we've taken the bass sample, made the mids from it, distorted it
+		//and hit it with another pole of darkening.
+		//mid sample is still normal from undistorted bass
+		
+		if (fabs(iirSampleE)<1.18e-37) iirSampleE = 0.0;
+		iirSampleE = (iirSampleE * (1.0 - iirMidHigh)) + (midSample * iirMidHigh);
+		long double highSample = midSample - iirSampleE;
+		midSample = iirSampleE;
+		//here is where we make the high sample out of the mid, and take highs
+		//away from the mid.
+		
+		if (fabs(iirSampleF)<1.18e-37) iirSampleF = 0.0;
+		iirSampleF = (iirSampleF * (1.0 - iirMidHigh)) + (highSample * iirMidHigh);
+		highSample -= iirSampleF;
+		
+		if (gainHigh != 1.0) highSample *= gainHigh;
+		if (highSample > 1.0) highSample = 1.0;
+		if (highSample < -1.0) highSample = -1.0;
+		highSample -= pow(highSample,5)*0.1768;
+		//highpassing HighSample another stage, before distorting it
+		
+		inputSample = ((bassSample*outBass) + midSample + (highSample*outHigh))*4.0;
+		//end EQ section
+		
+		outSample = biquadC[2]*inputSample+biquadC[3]*biquadC[7]+biquadC[4]*biquadC[8]-biquadC[5]*biquadC[9]-biquadC[6]*biquadC[10];
+		biquadC[8] = biquadC[7]; biquadC[7] = inputSample; inputSample = outSample; biquadC[10] = biquadC[9]; biquadC[9] = inputSample; //DF1
+		
+		if (inputSample > 1.0) inputSample = 1.0;
+		if (inputSample < -1.0) inputSample = -1.0;
+		inputSample -= pow(inputSample,5)*0.1768;
+		
+		outSample = biquadD[2]*inputSample+biquadD[3]*biquadD[7]+biquadD[4]*biquadD[8]-biquadD[5]*biquadD[9]-biquadD[6]*biquadD[10];
+		biquadD[8] = biquadD[7]; biquadD[7] = inputSample; inputSample = outSample; biquadD[10] = biquadD[9]; biquadD[9] = inputSample; //DF1
+				
+		if (outPad != 1.0) inputSample *= outPad;
+
+		if (wet !=1.0) {
+			inputSample = (inputSample * wet) + (drySample * (1.0-wet));
+		}
+		//Dry/Wet control, defaults to the last slider
+
+		//begin 32 bit floating point dither
+		int expon; frexpf((float)inputSample, &expon);
+		fpd ^= fpd << 13; fpd ^= fpd >> 17; fpd ^= fpd << 5;
+		inputSample += ((double(fpd)-uint32_t(0x7fffffff)) * 5.5e-36l * pow(2,expon+62));
+		//end 32 bit floating point dither
+		
+		*destP = inputSample;
+		
+		sourceP += inNumChannels; destP += inNumChannels;
+	}
+}
+
