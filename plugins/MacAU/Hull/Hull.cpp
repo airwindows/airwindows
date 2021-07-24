eb454669efc84f54334c9f53edcd2c28b9001673
airwindows <jinx6568@sover.net>
2021-07-23 22:34:12 -0400

    Hull

---
+++ plugins/MacAU/Hull/Hull.cpp
@@ -0,0 +1,254 @@
+/*
+ *	File:		Hull.cpp
+ *	
+ *	Version:	1.0
+ * 
+ *	Created:	7/19/21
+ *	
+ *	Copyright:  Copyright © 2021 Airwindows, All Rights Reserved
+ * 
+ *	Disclaimer:	IMPORTANT:  This Apple software is supplied to you by Apple Computer, Inc. ("Apple") in 
+ *				consideration of your agreement to the following terms, and your use, installation, modification 
+ *				or redistribution of this Apple software constitutes acceptance of these terms.  If you do 
+ *				not agree with these terms, please do not use, install, modify or redistribute this Apple 
+ *				software.
+ *
+ *				In consideration of your agreement to abide by the following terms, and subject to these terms, 
+ *				Apple grants you a personal, non-exclusive license, under Apple's copyrights in this 
+ *				original Apple software (the "Apple Software"), to use, reproduce, modify and redistribute the 
+ *				Apple Software, with or without modifications, in source and/or binary forms; provided that if you 
+ *				redistribute the Apple Software in its entirety and without modifications, you must retain this 
+ *				notice and the following text and disclaimers in all such redistributions of the Apple Software. 
+ *				Neither the name, trademarks, service marks or logos of Apple Computer, Inc. may be used to 
+ *				endorse or promote products derived from the Apple Software without specific prior written 
+ *				permission from Apple.  Except as expressly stated in this notice, no other rights or 
+ *				licenses, express or implied, are granted by Apple herein, including but not limited to any 
+ *				patent rights that may be infringed by your derivative works or by other works in which the 
+ *				Apple Software may be incorporated.
+ *
+ *				The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO WARRANTIES, EXPRESS OR 
+ *				IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY 
+ *				AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND OPERATION ALONE 
+ *				OR IN COMBINATION WITH YOUR PRODUCTS.
+ *
+ *				IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL 
+ *				DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
+ *				OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, 
+ *				REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER 
+ *				UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR OTHERWISE, EVEN 
+ *				IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+/*=============================================================================
+ Hull.cpp
+ 
+ =============================================================================*/
+#include "Hull.h"
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+COMPONENT_ENTRY(Hull)
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Hull::Hull
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Hull::Hull(AudioUnit component)
+: AUEffectBase(component)
+{
+	CreateElements();
+	Globals()->UseIndexedParameters(kNumberOfParameters);
+	SetParameter(kParam_One, kDefaultValue_ParamOne );
+	SetParameter(kParam_Two, kDefaultValue_ParamTwo );
+	
+#if AU_DEBUG_DISPATCHER
+	mDebugDispatcher = new AUDebugDispatcher (this);
+#endif
+	
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Hull::GetParameterValueStrings
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Hull::GetParameterValueStrings(AudioUnitScope		inScope,
+													   AudioUnitParameterID	inParameterID,
+													   CFArrayRef *		outStrings)
+{
+	
+    return kAudioUnitErr_InvalidProperty;
+}
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Hull::GetParameterInfo
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Hull::GetParameterInfo(AudioUnitScope		inScope,
+											   AudioUnitParameterID	inParameterID,
+											   AudioUnitParameterInfo	&outParameterInfo )
+{
+	ComponentResult result = noErr;
+	
+	outParameterInfo.flags = 	kAudioUnitParameterFlag_IsWritable
+	|		kAudioUnitParameterFlag_IsReadable;
+    
+    if (inScope == kAudioUnitScope_Global) {
+        switch(inParameterID)
+        {
+			case kParam_One:
+                AUBase::FillInParameterName (outParameterInfo, kParameterOneName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = 0.0;
+                outParameterInfo.maxValue = 1.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamOne;
+                break;
+            case kParam_Two:
+                AUBase::FillInParameterName (outParameterInfo, kParameterTwoName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = -1.0;
+                outParameterInfo.maxValue = 1.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamTwo;
+                break;
+			default:
+                result = kAudioUnitErr_InvalidParameter;
+                break;
+		}
+	} else {
+        result = kAudioUnitErr_InvalidParameter;
+    }
+    
+	
+	
+	return result;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Hull::GetPropertyInfo
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Hull::GetPropertyInfo (AudioUnitPropertyID	inID,
+											   AudioUnitScope		inScope,
+											   AudioUnitElement	inElement,
+											   UInt32 &		outDataSize,
+											   Boolean &		outWritable)
+{
+	return AUEffectBase::GetPropertyInfo (inID, inScope, inElement, outDataSize, outWritable);
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Hull::GetProperty
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Hull::GetProperty(	AudioUnitPropertyID inID,
+										  AudioUnitScope 		inScope,
+										  AudioUnitElement 	inElement,
+										  void *			outData )
+{
+	return AUEffectBase::GetProperty (inID, inScope, inElement, outData);
+}
+
+//	Hull::Initialize
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult Hull::Initialize()
+{
+    ComponentResult result = AUEffectBase::Initialize();
+    if (result == noErr)
+        Reset(kAudioUnitScope_Global, 0);
+    return result;
+}
+
+#pragma mark ____HullEffectKernel
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Hull::HullKernel::Reset()
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void		Hull::HullKernel::Reset()
+{
+	for(int count = 0; count < 4002; count++) b[count] = 0.0;
+	for(int count = 0; count < 102; count++) c[count] = 0.0;
+	bPointer = 1; cPointer = 1;
+	
+	fpd = 1.0; while (fpd < 16386) fpd = rand()*UINT32_MAX;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Hull::HullKernel::Process
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void		Hull::HullKernel::Process(	const Float32 	*inSourceP,
+									  Float32		 	*inDestP,
+									  UInt32 			inFramesToProcess,
+									  UInt32			inNumChannels, 
+									  bool			&ioSilence )
+{
+	UInt32 nSampleFrames = inFramesToProcess;
+	const Float32 *sourceP = inSourceP;
+	Float32 *destP = inDestP;
+	long double overallscale = 1.0;
+	overallscale /= 96000.0;
+	overallscale *= GetSampleRate(); //this one's scaled to 96k for the deepest bass
+	if (overallscale > 1.0) overallscale = 1.0; //and if you go for 192k, rather than crash
+	//it just cuts out the maximum (2000) depth of averaging you can get
+	Float64 hullSetting = pow(GetParameter( kParam_One ),3)*overallscale;
+	int limitA = (hullSetting*2000.0)+1.0;
+	long double divisorA = 1.0/limitA;
+	int limitB = (hullSetting*1000.0)+1.0;
+	long double divisorB = 1.0/limitB;
+	int limitC = sqrt(hullSetting*2000.0)+1.0;
+	long double divisorC = 1.0/limitC;
+	Float64 wet = -GetParameter( kParam_Two ); //functions as dark/bright
+		
+	while (nSampleFrames-- > 0) {
+		long double inputSample = *sourceP;
+		if (fabs(inputSample)<1.18e-37) inputSample = fpd * 1.18e-37;
+		long double drySample = inputSample;
+		
+		bPointer--; if (bPointer < 0) bPointer += 2000;
+		b[bPointer] = b[bPointer+2000] = inputSample;
+
+		int x = bPointer;
+		long double longAverage = 0.0;
+		long double shortAverage = 0.0;
+		while (x < bPointer+limitB) {
+			shortAverage += b[x];
+			longAverage += b[x];
+			x++;
+		} //once shorter average is reached, continue to longer
+		while (x < bPointer+limitA) {
+			longAverage += b[x];
+			x++;
+		}
+		longAverage *= divisorA;
+		shortAverage *= divisorB;
+		
+		
+		cPointer--; if (cPointer < 0) cPointer += 50;
+		c[cPointer] = c[cPointer+50] = shortAverage+(shortAverage-longAverage);
+		
+		x = cPointer;
+		long double shortestAverage = 0.0;
+		while (x < cPointer+limitC) {
+			shortestAverage += c[x];
+			x++;
+		}
+		shortestAverage *= divisorC;
+				
+		if (wet > 0.0) {
+			inputSample = (shortestAverage * wet)+(drySample * (1.0-wet)); //dark
+		} else {
+			inputSample = ((inputSample-shortestAverage) * fabs(wet))+(drySample * (1.0+wet)); //bright
+		}		
+		
+		//begin 32 bit floating point dither
+		int expon; frexpf((float)inputSample, &expon);
+		fpd ^= fpd << 13; fpd ^= fpd >> 17; fpd ^= fpd << 5;
+		inputSample += ((double(fpd)-uint32_t(0x7fffffff)) * 5.5e-36l * pow(2,expon+62));
+		//end 32 bit floating point dither
+		
+		*destP = inputSample;
+		
+		sourceP += inNumChannels; destP += inNumChannels;
+	}
+}
+
---
+++ plugins/MacVST/Hull/source/HullProc.cpp
@@ -0,0 +1,206 @@
+/* ========================================
+ *  Hull - Hull.h
+ *  Copyright (c) 2016 airwindows, All rights reserved
+ * ======================================== */
+
+#ifndef __Hull_H
+#include "Hull.h"
+#endif
+
+void Hull::processReplacing(float **inputs, float **outputs, VstInt32 sampleFrames) 
+{
+    float* in1  =  inputs[0];
+    float* in2  =  inputs[1];
+    float* out1 = outputs[0];
+    float* out2 = outputs[1];
+
+	double overallscale = 1.0;
+	overallscale /= 96000.0;
+	overallscale *= getSampleRate();
+	if (overallscale > 1.0) overallscale = 1.0; //and if you go for 192k, rather than crash
+	//it just cuts out the maximum (2000) depth of averaging you can get
+	double hullSetting = pow(A,3)*overallscale;
+	int limitA = (hullSetting*2000.0)+1.0;
+	long double divisorA = 1.0/limitA;
+	int limitB = (hullSetting*1000.0)+1.0;
+	long double divisorB = 1.0/limitB;
+	int limitC = sqrt(hullSetting*2000.0)+1.0;
+	long double divisorC = 1.0/limitC;
+	double wet = -((B*2.0)-1.0); //functions as dark/bright
+	
+    while (--sampleFrames >= 0)
+    {
+		long double inputSampleL = *in1;
+		long double inputSampleR = *in2;
+		if (fabs(inputSampleL)<1.18e-37) inputSampleL = fpdL * 1.18e-37;
+		if (fabs(inputSampleR)<1.18e-37) inputSampleR = fpdR * 1.18e-37;
+		long double drySampleL = inputSampleL;
+		long double drySampleR = inputSampleR;
+		
+		bPointer--; if (bPointer < 0) bPointer += 2000;
+		bL[bPointer] = bL[bPointer+2000] = inputSampleL;
+		bR[bPointer] = bR[bPointer+2000] = inputSampleR;
+		
+		int x = bPointer;
+		long double longAverageL = 0.0;
+		long double longAverageR = 0.0;
+		long double shortAverageL = 0.0;
+		long double shortAverageR = 0.0;
+		while (x < bPointer+limitB) {
+			shortAverageL += bL[x];
+			shortAverageR += bR[x];
+			longAverageL += bL[x];
+			longAverageR += bR[x];
+			x++;
+		} //once shorter average is reached, continue to longer
+		while (x < bPointer+limitA) {
+			longAverageL += bL[x];
+			longAverageR += bR[x];
+			x++;
+		}
+		longAverageL *= divisorA;
+		longAverageR *= divisorA;
+		shortAverageL *= divisorB;
+		shortAverageR *= divisorB;
+		
+		
+		cPointer--; if (cPointer < 0) cPointer += 50;
+		cL[cPointer] = cL[cPointer+50] = shortAverageL+(shortAverageL-longAverageL);
+		cR[cPointer] = cR[cPointer+50] = shortAverageR+(shortAverageR-longAverageR);
+		
+		x = cPointer;
+		long double shortestAverageL = 0.0;
+		long double shortestAverageR = 0.0;
+		while (x < cPointer+limitC) {
+			shortestAverageL += cL[x];
+			shortestAverageR += cR[x];
+			x++;
+		}
+		shortestAverageL *= divisorC;
+		shortestAverageR *= divisorC;
+		
+		if (wet > 0.0) {
+			inputSampleL = (shortestAverageL * wet)+(drySampleL * (1.0-wet)); //dark
+			inputSampleR = (shortestAverageR * wet)+(drySampleR * (1.0-wet)); //dark
+		} else {
+			inputSampleL = ((inputSampleL-shortestAverageL) * fabs(wet))+(drySampleL * (1.0+wet)); //bright
+			inputSampleR = ((inputSampleR-shortestAverageR) * fabs(wet))+(drySampleR * (1.0+wet)); //bright
+		}		
+		
+		//begin 32 bit stereo floating point dither
+		int expon; frexpf((float)inputSampleL, &expon);
+		fpdL ^= fpdL << 13; fpdL ^= fpdL >> 17; fpdL ^= fpdL << 5;
+		inputSampleL += ((double(fpdL)-uint32_t(0x7fffffff)) * 5.5e-36l * pow(2,expon+62));
+		frexpf((float)inputSampleR, &expon);
+		fpdR ^= fpdR << 13; fpdR ^= fpdR >> 17; fpdR ^= fpdR << 5;
+		inputSampleR += ((double(fpdR)-uint32_t(0x7fffffff)) * 5.5e-36l * pow(2,expon+62));
+		//end 32 bit stereo floating point dither
+		
+		*out1 = inputSampleL;
+		*out2 = inputSampleR;
+
+		in1++;
+		in2++;
+		out1++;
+		out2++;
+    }
+}
+
+void Hull::processDoubleReplacing(double **inputs, double **outputs, VstInt32 sampleFrames) 
+{
+    double* in1  =  inputs[0];
+    double* in2  =  inputs[1];
+    double* out1 = outputs[0];
+    double* out2 = outputs[1];
+
+	double overallscale = 1.0;
+	overallscale /= 96000.0;
+	overallscale *= getSampleRate();
+	if (overallscale > 1.0) overallscale = 1.0; //and if you go for 192k, rather than crash
+	//it just cuts out the maximum (2000) depth of averaging you can get
+	double hullSetting = pow(A,3)*overallscale;
+	int limitA = (hullSetting*2000.0)+1.0;
+	long double divisorA = 1.0/limitA;
+	int limitB = (hullSetting*1000.0)+1.0;
+	long double divisorB = 1.0/limitB;
+	int limitC = sqrt(hullSetting*2000.0)+1.0;
+	long double divisorC = 1.0/limitC;
+	double wet = -((B*2.0)-1.0); //functions as dark/bright
+	
+    while (--sampleFrames >= 0)
+    {
+		long double inputSampleL = *in1;
+		long double inputSampleR = *in2;
+		if (fabs(inputSampleL)<1.18e-43) inputSampleL = fpdL * 1.18e-43;
+		if (fabs(inputSampleR)<1.18e-43) inputSampleR = fpdR * 1.18e-43;
+		long double drySampleL = inputSampleL;
+		long double drySampleR = inputSampleR;
+		
+		bPointer--; if (bPointer < 0) bPointer += 2000;
+		bL[bPointer] = bL[bPointer+2000] = inputSampleL;
+		bR[bPointer] = bR[bPointer+2000] = inputSampleR;
+		
+		int x = bPointer;
+		long double longAverageL = 0.0;
+		long double longAverageR = 0.0;
+		long double shortAverageL = 0.0;
+		long double shortAverageR = 0.0;
+		while (x < bPointer+limitB) {
+			shortAverageL += bL[x];
+			shortAverageR += bR[x];
+			longAverageL += bL[x];
+			longAverageR += bR[x];
+			x++;
+		} //once shorter average is reached, continue to longer
+		while (x < bPointer+limitA) {
+			longAverageL += bL[x];
+			longAverageR += bR[x];
+			x++;
+		}
+		longAverageL *= divisorA;
+		longAverageR *= divisorA;
+		shortAverageL *= divisorB;
+		shortAverageR *= divisorB;
+		
+		
+		cPointer--; if (cPointer < 0) cPointer += 50;
+		cL[cPointer] = cL[cPointer+50] = shortAverageL+(shortAverageL-longAverageL);
+		cR[cPointer] = cR[cPointer+50] = shortAverageR+(shortAverageR-longAverageR);
+		
+		x = cPointer;
+		long double shortestAverageL = 0.0;
+		long double shortestAverageR = 0.0;
+		while (x < cPointer+limitC) {
+			shortestAverageL += cL[x];
+			shortestAverageR += cR[x];
+			x++;
+		}
+		shortestAverageL *= divisorC;
+		shortestAverageR *= divisorC;
+		
+		if (wet > 0.0) {
+			inputSampleL = (shortestAverageL * wet)+(drySampleL * (1.0-wet)); //dark
+			inputSampleR = (shortestAverageR * wet)+(drySampleR * (1.0-wet)); //dark
+		} else {
+			inputSampleL = ((inputSampleL-shortestAverageL) * fabs(wet))+(drySampleL * (1.0+wet)); //bright
+			inputSampleR = ((inputSampleR-shortestAverageR) * fabs(wet))+(drySampleR * (1.0+wet)); //bright
+		}		
+		
+		//begin 64 bit stereo floating point dither
+		int expon; frexp((double)inputSampleL, &expon);
+		fpdL ^= fpdL << 13; fpdL ^= fpdL >> 17; fpdL ^= fpdL << 5;
+		inputSampleL += ((double(fpdL)-uint32_t(0x7fffffff)) * 1.1e-44l * pow(2,expon+62));
+		frexp((double)inputSampleR, &expon);
+		fpdR ^= fpdR << 13; fpdR ^= fpdR >> 17; fpdR ^= fpdR << 5;
+		inputSampleR += ((double(fpdR)-uint32_t(0x7fffffff)) * 1.1e-44l * pow(2,expon+62));
+		//end 64 bit stereo floating point dither
+		
+		*out1 = inputSampleL;
+		*out2 = inputSampleR;
+
+		in1++;
+		in2++;
+		out1++;
+		out2++;
+    }
+}
