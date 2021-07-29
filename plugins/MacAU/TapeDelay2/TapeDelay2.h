6aa15eefabcfa0b636175b6dd25f399e13dc49a4
airwindows <jinx6568@sover.net>
2021-07-29 18:15:30 -0400

    TapeDelay2

---
+++ plugins/MacAU/TapeDelay2/TapeDelay2.h
@@ -0,0 +1,159 @@
+/*
+*	File:		TapeDelay2.h
+*	
+*	Version:	1.0
+* 
+*	Created:	7/19/21
+*	
+*	Copyright:  Copyright © 2021 Airwindows, All Rights Reserved
+* 
+*	Disclaimer:	IMPORTANT:  This Apple software is supplied to you by Apple Computer, Inc. ("Apple") in 
+*				consideration of your agreement to the following terms, and your use, installation, modification 
+*				or redistribution of this Apple software constitutes acceptance of these terms.  If you do 
+*				not agree with these terms, please do not use, install, modify or redistribute this Apple 
+*				software.
+*
+*				In consideration of your agreement to abide by the following terms, and subject to these terms, 
+*				Apple grants you a personal, non-exclusive license, under Apple's copyrights in this 
+*				original Apple software (the "Apple Software"), to use, reproduce, modify and redistribute the 
+*				Apple Software, with or without modifications, in source and/or binary forms; provided that if you 
+*				redistribute the Apple Software in its entirety and without modifications, you must retain this 
+*				notice and the following text and disclaimers in all such redistributions of the Apple Software. 
+*				Neither the name, trademarks, service marks or logos of Apple Computer, Inc. may be used to 
+*				endorse or promote products derived from the Apple Software without specific prior written 
+*				permission from Apple.  Except as expressly stated in this notice, no other rights or 
+*				licenses, express or implied, are granted by Apple herein, including but not limited to any 
+*				patent rights that may be infringed by your derivative works or by other works in which the 
+*				Apple Software may be incorporated.
+*
+*				The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO WARRANTIES, EXPRESS OR 
+*				IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY 
+*				AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND OPERATION ALONE 
+*				OR IN COMBINATION WITH YOUR PRODUCTS.
+*
+*				IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL 
+*				DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
+*				OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, 
+*				REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER 
+*				UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR OTHERWISE, EVEN 
+*				IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+#include "AUEffectBase.h"
+#include "TapeDelay2Version.h"
+
+#if AU_DEBUG_DISPATCHER
+	#include "AUDebugDispatcher.h"
+#endif
+
+
+#ifndef __TapeDelay2_h__
+#define __TapeDelay2_h__
+
+
+#pragma mark ____TapeDelay2 Parameters
+
+// parameters
+static const float kDefaultValue_ParamOne = 1.0;
+static const float kDefaultValue_ParamTwo = 0.0;
+static const float kDefaultValue_ParamThree = 0.5;
+static const float kDefaultValue_ParamFour = 0.0;
+static const float kDefaultValue_ParamFive = 0.0;
+static const float kDefaultValue_ParamSix = 1.0;
+//let's assume we always have a default of 0.0, for no effect
+
+static CFStringRef kParameterOneName = CFSTR("Time");
+static CFStringRef kParameterTwoName = CFSTR("Regen");
+static CFStringRef kParameterThreeName = CFSTR("Freq");
+static CFStringRef kParameterFourName = CFSTR("Reso");
+static CFStringRef kParameterFiveName = CFSTR("Flutter");
+static CFStringRef kParameterSixName = CFSTR("Dry/Wet");
+//Alter the name if desired, but using the plugin name is a start
+
+enum {
+	kParam_One =0,
+	kParam_Two =1,
+	kParam_Three =2,
+	kParam_Four =3,
+	kParam_Five =4,
+	kParam_Six =5,
+	//Add your parameters here...
+	kNumberOfParameters=6
+};
+
+#pragma mark ____TapeDelay2
+class TapeDelay2 : public AUEffectBase
+{
+public:
+	TapeDelay2(AudioUnit component);
+#if AU_DEBUG_DISPATCHER
+	virtual ~TapeDelay2 () { delete mDebugDispatcher; }
+#endif
+	
+	virtual AUKernelBase *		NewKernel() { return new TapeDelay2Kernel(this); }
+	
+	virtual	ComponentResult		GetParameterValueStrings(AudioUnitScope			inScope,
+														 AudioUnitParameterID		inParameterID,
+														 CFArrayRef *			outStrings);
+    
+	virtual	ComponentResult		GetParameterInfo(AudioUnitScope			inScope,
+												 AudioUnitParameterID	inParameterID,
+												 AudioUnitParameterInfo	&outParameterInfo);
+    
+	virtual ComponentResult		GetPropertyInfo(AudioUnitPropertyID		inID,
+												AudioUnitScope			inScope,
+												AudioUnitElement		inElement,
+												UInt32 &			outDataSize,
+												Boolean	&			outWritable );
+	
+	virtual ComponentResult		GetProperty(AudioUnitPropertyID inID,
+											AudioUnitScope 		inScope,
+											AudioUnitElement 		inElement,
+											void *			outData);
+	
+	virtual ComponentResult    Initialize();
+	virtual bool				SupportsTail () { return true; }
+    virtual Float64				GetTailTime() {return (1.0/GetSampleRate())*0.0;} //in SECONDS! gsr * a number = in samples
+    virtual Float64				GetLatency() {return (1.0/GetSampleRate())*0.0;}	// in SECONDS! gsr * a number = in samples
+	
+	/*! @method Version */
+	virtual ComponentResult		Version() { return kTapeDelay2Version; }
+	
+    
+	
+protected:
+		class TapeDelay2Kernel : public AUKernelBase		// most of the real work happens here
+	{
+public:
+		TapeDelay2Kernel(AUEffectBase *inAudioUnit )
+		: AUKernelBase(inAudioUnit)
+	{
+	}
+		
+		// *Required* overides for the process method for this effect
+		// processes one channel of interleaved samples
+        virtual void 		Process(	const Float32 	*inSourceP,
+										Float32		 	*inDestP,
+										UInt32 			inFramesToProcess,
+										UInt32			inNumChannels,
+										bool			&ioSilence);
+		
+        virtual void		Reset();
+		
+		private: 
+		Float64 d[88211];
+		Float64 prevSample;
+		Float64 delay;
+		Float64 sweep;
+		long double regenFilter[9];
+		long double outFilter[9];
+		double lastRef[10];
+		int cycle;	
+		uint32_t fpd;
+	};
+};
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#endif
---
+++ plugins/MacVST/TapeDelay2/source/TapeDelay2.cpp
@@ -0,0 +1,169 @@
+/* ========================================
+ *  TapeDelay2 - TapeDelay2.h
+ *  Copyright (c) 2016 airwindows, All rights reserved
+ * ======================================== */
+
+#ifndef __TapeDelay2_H
+#include "TapeDelay2.h"
+#endif
+
+AudioEffect* createEffectInstance(audioMasterCallback audioMaster) {return new TapeDelay2(audioMaster);}
+
+TapeDelay2::TapeDelay2(audioMasterCallback audioMaster) :
+    AudioEffectX(audioMaster, kNumPrograms, kNumParameters)
+{
+	A = 1.0;
+	B = 0.0;
+	C = 0.5;
+	D = 0.0;
+	E = 0.0;
+	F = 1.0;
+	
+	for(int x = 0; x < 88210; x++) {dL[x] = 0.0; dR[x] = 0.0;}
+	prevSampleL = 0.0; delayL = 0.0; sweepL = 0.0;
+	prevSampleR = 0.0; delayR = 0.0; sweepR = 0.0;
+	for (int x = 0; x < 9; x++) {
+		regenFilterL[x] = 0.0; outFilterL[x] = 0.0; lastRefL[x] = 0.0;
+		regenFilterR[x] = 0.0; outFilterR[x] = 0.0; lastRefR[x] = 0.0;
+	}
+	cycle = 0;	
+	
+	fpdL = 1.0; while (fpdL < 16386) fpdL = rand()*UINT32_MAX;
+	fpdR = 1.0; while (fpdR < 16386) fpdR = rand()*UINT32_MAX;
+	//this is reset: values being initialized only once. Startup values, whatever they are.
+	
+    _canDo.insert("plugAsChannelInsert"); // plug-in can be used as a channel insert effect.
+    _canDo.insert("plugAsSend"); // plug-in can be used as a send effect.
+    _canDo.insert("x2in2out"); 
+    setNumInputs(kNumInputs);
+    setNumOutputs(kNumOutputs);
+    setUniqueID(kUniqueId);
+    canProcessReplacing();     // supports output replacing
+    canDoubleReplacing();      // supports double precision processing
+	programsAreChunks(true);
+    vst_strncpy (_programName, "Default", kVstMaxProgNameLen); // default program name
+}
+
+TapeDelay2::~TapeDelay2() {}
+VstInt32 TapeDelay2::getVendorVersion () {return 1000;}
+void TapeDelay2::setProgramName(char *name) {vst_strncpy (_programName, name, kVstMaxProgNameLen);}
+void TapeDelay2::getProgramName(char *name) {vst_strncpy (name, _programName, kVstMaxProgNameLen);}
+//airwindows likes to ignore this stuff. Make your own programs, and make a different plugin rather than
+//trying to do versioning and preventing people from using older versions. Maybe they like the old one!
+
+static float pinParameter(float data)
+{
+	if (data < 0.0f) return 0.0f;
+	if (data > 1.0f) return 1.0f;
+	return data;
+}
+
+VstInt32 TapeDelay2::getChunk (void** data, bool isPreset)
+{
+	float *chunkData = (float *)calloc(kNumParameters, sizeof(float));
+	chunkData[0] = A;
+	chunkData[1] = B;
+	chunkData[2] = C;
+	chunkData[3] = D;
+	chunkData[4] = E;
+	chunkData[5] = F;
+	/* Note: The way this is set up, it will break if you manage to save settings on an Intel
+	 machine and load them on a PPC Mac. However, it's fine if you stick to the machine you 
+	 started with. */
+	
+	*data = chunkData;
+	return kNumParameters * sizeof(float);
+}
+
+VstInt32 TapeDelay2::setChunk (void* data, VstInt32 byteSize, bool isPreset)
+{	
+	float *chunkData = (float *)data;
+	A = pinParameter(chunkData[0]);
+	B = pinParameter(chunkData[1]);
+	C = pinParameter(chunkData[2]);
+	D = pinParameter(chunkData[3]);
+	E = pinParameter(chunkData[4]);
+	F = pinParameter(chunkData[5]);
+	/* We're ignoring byteSize as we found it to be a filthy liar */
+	
+	/* calculate any other fields you need here - you could copy in 
+	 code from setParameter() here. */
+	return 0;
+}
+
+void TapeDelay2::setParameter(VstInt32 index, float value) {
+    switch (index) {
+        case kParamA: A = value; break;
+        case kParamB: B = value; break;
+        case kParamC: C = value; break;
+        case kParamD: D = value; break;
+        case kParamE: E = value; break;
+        case kParamF: F = value; break;
+        default: throw; // unknown parameter, shouldn't happen!
+    }
+}
+
+float TapeDelay2::getParameter(VstInt32 index) {
+    switch (index) {
+        case kParamA: return A; break;
+        case kParamB: return B; break;
+        case kParamC: return C; break;
+        case kParamD: return D; break;
+        case kParamE: return E; break;
+        case kParamF: return F; break;
+        default: break; // unknown parameter, shouldn't happen!
+    } return 0.0; //we only need to update the relevant name, this is simple to manage
+}
+
+void TapeDelay2::getParameterName(VstInt32 index, char *text) {
+    switch (index) {
+        case kParamA: vst_strncpy (text, "Time", kVstMaxParamStrLen); break;
+		case kParamB: vst_strncpy (text, "Regen", kVstMaxParamStrLen); break;
+		case kParamC: vst_strncpy (text, "Freq", kVstMaxParamStrLen); break;
+		case kParamD: vst_strncpy (text, "Reso", kVstMaxParamStrLen); break;
+		case kParamE: vst_strncpy (text, "Flutter", kVstMaxParamStrLen); break;
+		case kParamF: vst_strncpy (text, "Dry/Wet", kVstMaxParamStrLen); break;
+        default: break; // unknown parameter, shouldn't happen!
+    } //this is our labels for displaying in the VST host
+}
+
+void TapeDelay2::getParameterDisplay(VstInt32 index, char *text) {
+    switch (index) {
+        case kParamA: float2string (A, text, kVstMaxParamStrLen); break;
+        case kParamB: float2string (B, text, kVstMaxParamStrLen); break;
+        case kParamC: float2string (C, text, kVstMaxParamStrLen); break;
+        case kParamD: float2string (D, text, kVstMaxParamStrLen); break;
+        case kParamE: float2string (E, text, kVstMaxParamStrLen); break;
+        case kParamF: float2string (F, text, kVstMaxParamStrLen); break;
+        default: break; // unknown parameter, shouldn't happen!
+	} //this displays the values and handles 'popups' where it's discrete choices
+}
+
+void TapeDelay2::getParameterLabel(VstInt32 index, char *text) {
+    switch (index) {
+        case kParamA: vst_strncpy (text, "", kVstMaxParamStrLen); break;
+        case kParamB: vst_strncpy (text, "", kVstMaxParamStrLen); break;
+        case kParamC: vst_strncpy (text, "", kVstMaxParamStrLen); break;
+        case kParamD: vst_strncpy (text, "", kVstMaxParamStrLen); break;
+        case kParamE: vst_strncpy (text, "", kVstMaxParamStrLen); break;
+        case kParamF: vst_strncpy (text, "", kVstMaxParamStrLen); break;
+		default: break; // unknown parameter, shouldn't happen!
+    }
+}
+
+VstInt32 TapeDelay2::canDo(char *text) 
+{ return (_canDo.find(text) == _canDo.end()) ? -1: 1; } // 1 = yes, -1 = no, 0 = don't know
+
+bool TapeDelay2::getEffectName(char* name) {
+    vst_strncpy(name, "TapeDelay2", kVstMaxProductStrLen); return true;
+}
+
+VstPlugCategory TapeDelay2::getPlugCategory() {return kPlugCategEffect;}
+
+bool TapeDelay2::getProductString(char* text) {
+  	vst_strncpy (text, "airwindows TapeDelay2", kVstMaxProductStrLen); return true;
+}
+
+bool TapeDelay2::getVendorString(char* text) {
+  	vst_strncpy (text, "airwindows", kVstMaxVendorStrLen); return true;
+}
