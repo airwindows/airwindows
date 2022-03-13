fea197a674777e06e7abe7c119a9526d2f031792
airwindows <jinx6568@sover.net>
2022-03-13 18:36:23 -0400

    Texturize

---
+++ plugins/MacAU/Texturize/Texturize.cpp
@@ -0,0 +1,259 @@
+/*
+*	File:		Texturize.cpp
+*	
+*	Version:	1.0
+* 
+*	Created:	3/12/22
+*	
+*	Copyright:  Copyright © 2022 Airwindows, All Rights Reserved
+* 
+*	Disclaimer:	IMPORTANT:  This Apple software is supplied to you by Apple Computer, Inc. ("Apple") in 
+*				consideration of your agreement to the following terms, and your use, installation, modification 
+*				or redistribution of this Apple software constitutes acceptance of these terms.  If you do 
+*				not agree with these terms, please do not use, install, modify or redistribute this Apple 
+*				software.
+*
+*				In consideration of your agreement to abide by the following terms, and subject to these terms, 
+*				Apple grants you a personal, non-exclusive license, under Apple's copyrights in this 
+*				original Apple software (the "Apple Software"), to use, reproduce, modify and redistribute the 
+*				Apple Software, with or without modifications, in source and/or binary forms; provided that if you 
+*				redistribute the Apple Software in its entirety and without modifications, you must retain this 
+*				notice and the following text and disclaimers in all such redistributions of the Apple Software. 
+*				Neither the name, trademarks, service marks or logos of Apple Computer, Inc. may be used to 
+*				endorse or promote products derived from the Apple Software without specific prior written 
+*				permission from Apple.  Except as expressly stated in this notice, no other rights or 
+*				licenses, express or implied, are granted by Apple herein, including but not limited to any 
+*				patent rights that may be infringed by your derivative works or by other works in which the 
+*				Apple Software may be incorporated.
+*
+*				The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO WARRANTIES, EXPRESS OR 
+*				IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY 
+*				AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND OPERATION ALONE 
+*				OR IN COMBINATION WITH YOUR PRODUCTS.
+*
+*				IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL 
+*				DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
+*				OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, 
+*				REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER 
+*				UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR OTHERWISE, EVEN 
+*				IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+/*=============================================================================
+	Texturize.cpp
+	
+=============================================================================*/
+#include "Texturize.h"
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+COMPONENT_ENTRY(Texturize)
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Texturize::Texturize
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Texturize::Texturize(AudioUnit component)
+	: AUEffectBase(component)
+{
+	CreateElements();
+	Globals()->UseIndexedParameters(kNumberOfParameters);
+	SetParameter(kParam_One, kDefaultValue_ParamOne );
+	SetParameter(kParam_Two, kDefaultValue_ParamTwo );
+	SetParameter(kParam_Three, kDefaultValue_ParamThree );
+         
+#if AU_DEBUG_DISPATCHER
+	mDebugDispatcher = new AUDebugDispatcher (this);
+#endif
+	
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Texturize::GetParameterValueStrings
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Texturize::GetParameterValueStrings(AudioUnitScope		inScope,
+                                                                AudioUnitParameterID	inParameterID,
+                                                                CFArrayRef *		outStrings)
+{
+        
+    return kAudioUnitErr_InvalidProperty;
+}
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Texturize::GetParameterInfo
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Texturize::GetParameterInfo(AudioUnitScope		inScope,
+                                                        AudioUnitParameterID	inParameterID,
+                                                        AudioUnitParameterInfo	&outParameterInfo )
+{
+	ComponentResult result = noErr;
+
+	outParameterInfo.flags = 	kAudioUnitParameterFlag_IsWritable
+						|		kAudioUnitParameterFlag_IsReadable;
+    
+    if (inScope == kAudioUnitScope_Global) {
+        switch(inParameterID)
+        {
+           case kParam_One:
+                AUBase::FillInParameterName (outParameterInfo, kParameterOneName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = 0.0;
+                outParameterInfo.maxValue = 1.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamOne;
+                break;
+            case kParam_Two:
+                AUBase::FillInParameterName (outParameterInfo, kParameterTwoName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = 0.0;
+                outParameterInfo.maxValue = 1.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamTwo;
+                break;
+            case kParam_Three:
+                AUBase::FillInParameterName (outParameterInfo, kParameterThreeName, false);
+                outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
+                outParameterInfo.minValue = 0.0;
+                outParameterInfo.maxValue = 1.0;
+                outParameterInfo.defaultValue = kDefaultValue_ParamThree;
+                break;
+           default:
+                result = kAudioUnitErr_InvalidParameter;
+                break;
+            }
+	} else {
+        result = kAudioUnitErr_InvalidParameter;
+    }
+    
+
+
+	return result;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Texturize::GetPropertyInfo
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Texturize::GetPropertyInfo (AudioUnitPropertyID	inID,
+                                                        AudioUnitScope		inScope,
+                                                        AudioUnitElement	inElement,
+                                                        UInt32 &		outDataSize,
+                                                        Boolean &		outWritable)
+{
+	return AUEffectBase::GetPropertyInfo (inID, inScope, inElement, outDataSize, outWritable);
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Texturize::GetProperty
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult			Texturize::GetProperty(	AudioUnitPropertyID inID,
+                                                        AudioUnitScope 		inScope,
+                                                        AudioUnitElement 	inElement,
+                                                        void *			outData )
+{
+	return AUEffectBase::GetProperty (inID, inScope, inElement, outData);
+}
+
+//	Texturize::Initialize
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ComponentResult Texturize::Initialize()
+{
+    ComponentResult result = AUEffectBase::Initialize();
+    if (result == noErr)
+        Reset(kAudioUnitScope_Global, 0);
+    return result;
+}
+
+#pragma mark ____TexturizeEffectKernel
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Texturize::TexturizeKernel::Reset()
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void		Texturize::TexturizeKernel::Reset()
+{
+	polarity = false;
+	lastSample = 0.0;
+	iirSample = 0.0;
+	noiseA = 0.0;
+	noiseB = 0.0;
+	noiseC = 0.0;
+	flip = true;
+	fpd = 1.0; while (fpd < 16386) fpd = rand()*UINT32_MAX;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	Texturize::TexturizeKernel::Process
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void		Texturize::TexturizeKernel::Process(	const Float32 	*inSourceP,
+                                                    Float32		 	*inDestP,
+                                                    UInt32 			inFramesToProcess,
+                                                    UInt32			inNumChannels, 
+                                                    bool			&ioSilence )
+{
+	UInt32 nSampleFrames = inFramesToProcess;
+	const Float32 *sourceP = inSourceP;
+	Float32 *destP = inDestP;
+	long double overallscale = 1.0;
+	overallscale /= 44100.0;
+	overallscale *= GetSampleRate();
+	
+	Float64 slewAmount = ((pow(GetParameter( kParam_One ),2.0)*4.0)+0.71)/overallscale;
+	Float64 dynAmount = pow(GetParameter( kParam_Two ),2.0);
+	Float64 wet = pow(GetParameter( kParam_Three ),5);
+	
+	while (nSampleFrames-- > 0) {
+		double inputSample = *sourceP;
+		if (fabs(inputSample)<1.18e-23) inputSample = fpd * 1.18e-17;
+		double drySample = inputSample;
+
+		if (inputSample < 0) {
+			if (polarity == true) {
+				if (noiseA < 0) flip = true;
+				else flip = false;
+			}
+			polarity = false;
+		} else polarity = true;		
+		
+		if (flip) noiseA += (double(fpd)/UINT32_MAX);
+		else noiseA -= (double(fpd)/UINT32_MAX);
+		//here's the guts of the random walk		
+		flip = !flip;
+		
+		if (inputSample > 1.0) inputSample = 1.0; if (inputSample < -1.0) inputSample = -1.0;
+		if (dynAmount < 0.4999) inputSample = (inputSample*dynAmount*2.0) + (sin(inputSample)*(1.0-(dynAmount*2.0)));		
+		if (dynAmount > 0.5001) inputSample = (asin(inputSample)*((dynAmount*2.0)-1.0)) + (inputSample*(1.0-((dynAmount*2.0)-1.0)));
+		//doing this in two steps means I get to not run an extra sin/asin function per sample
+		
+		noiseB = sin(noiseA*(0.2-(dynAmount*0.125))*fabs(inputSample));
+		
+		double slew = fabs(inputSample-lastSample)*slewAmount;
+		lastSample = inputSample*(0.86-(dynAmount*0.125));
+		
+		if (slew > 1.0) slew = 1.0;
+		double iirIntensity = slew;
+		iirIntensity *= 2.472;
+		iirIntensity *= iirIntensity;
+		if (iirIntensity > 1.0) iirIntensity = 1.0;
+		
+		iirSample = (iirSample * (1.0 - iirIntensity)) + (noiseB * iirIntensity);
+		noiseB = iirSample;
+		noiseB = (noiseB * slew) + (noiseC * (1.0-slew));
+		noiseC = noiseB;
+		
+		inputSample = (noiseC * wet) + (drySample * (1.0-wet));
+		
+		//begin 32 bit floating point dither
+		int expon; frexpf((float)inputSample, &expon);
+		fpd ^= fpd << 13; fpd ^= fpd >> 17; fpd ^= fpd << 5;
+		inputSample += ((double(fpd)-uint32_t(0x7fffffff)) * 5.5e-36l * pow(2,expon+62));
+		//end 32 bit floating point dither
+		
+		*destP = inputSample;
+		
+		sourceP += inNumChannels; destP += inNumChannels;
+	}
+}
+
---
+++ plugins/MacSignedVST/Texturize/source/TexturizeProc.cpp
@@ -0,0 +1,234 @@
+/* ========================================
+ *  Texturize - Texturize.h
+ *  Copyright (c) 2016 airwindows, All rights reserved
+ * ======================================== */
+
+#ifndef __Gain_H
+#include "Texturize.h"
+#endif
+
+void Texturize::processReplacing(float **inputs, float **outputs, VstInt32 sampleFrames) 
+{
+    float* in1  =  inputs[0];
+    float* in2  =  inputs[1];
+    float* out1 = outputs[0];
+    float* out2 = outputs[1];
+
+	double overallscale = 1.0;
+	overallscale /= 44100.0;
+	overallscale *= getSampleRate();
+	double slewAmount = ((pow(A,2.0)*4.0)+0.71)/overallscale;
+	double dynAmount = pow(B,2.0);
+	double wet = pow(C,5);
+	    
+    while (--sampleFrames >= 0)
+    {
+		double inputSampleL = *in1;
+		double inputSampleR = *in2;
+		if (fabs(inputSampleL)<1.18e-23) inputSampleL = fpdL * 1.18e-17;
+		if (fabs(inputSampleR)<1.18e-23) inputSampleR = fpdR * 1.18e-17;
+		double drySampleL = inputSampleL;
+		double drySampleR = inputSampleR;
+		
+		if (inputSampleL < 0) {
+			if (polarityL == true) {
+				if (noiseAL < 0) flipL = true;
+				else flipL = false;
+			}
+			polarityL = false;
+		} else polarityL = true;		
+		
+		if (inputSampleR < 0) {
+			if (polarityR == true) {
+				if (noiseAR < 0) flipR = true;
+				else flipR = false;
+			}
+			polarityR = false;
+		} else polarityR = true;		
+		
+		if (flipL) noiseAL += (double(fpdL)/UINT32_MAX);
+		else noiseAL -= (double(fpdL)/UINT32_MAX);
+		//here's the guts of the random walk		
+		flipL = !flipL;
+
+		if (flipR) noiseAR += (double(fpdR)/UINT32_MAX);
+		else noiseAR -= (double(fpdR)/UINT32_MAX);
+		//here's the guts of the random walk		
+		flipR = !flipR;
+		
+		if (inputSampleL > 1.0) inputSampleL = 1.0; if (inputSampleL < -1.0) inputSampleL = -1.0;
+		if (inputSampleR > 1.0) inputSampleR = 1.0; if (inputSampleR < -1.0) inputSampleR = -1.0;
+
+		if (dynAmount < 0.4999) {
+			inputSampleL = (inputSampleL*dynAmount*2.0) + (sin(inputSampleL)*(1.0-(dynAmount*2.0)));		
+			inputSampleR = (inputSampleR*dynAmount*2.0) + (sin(inputSampleR)*(1.0-(dynAmount*2.0)));		
+		}
+		if (dynAmount > 0.5001) {
+			inputSampleL = (asin(inputSampleL)*((dynAmount*2.0)-1.0)) + (inputSampleL*(1.0-((dynAmount*2.0)-1.0)));
+			inputSampleR = (asin(inputSampleR)*((dynAmount*2.0)-1.0)) + (inputSampleR*(1.0-((dynAmount*2.0)-1.0)));
+		}
+		//doing this in two steps means I get to not run an extra sin/asin function per sample
+		
+		noiseBL = sin(noiseAL*(0.2-(dynAmount*0.125))*fabs(inputSampleL));
+		noiseBR = sin(noiseAR*(0.2-(dynAmount*0.125))*fabs(inputSampleR));
+		
+		double slewL = fabs(inputSampleL-lastSampleL)*slewAmount;
+		lastSampleL = inputSampleL*(0.86-(dynAmount*0.125));
+		if (slewL > 1.0) slewL = 1.0;
+		double iirIntensityL = slewL;
+		iirIntensityL *= 2.472;
+		iirIntensityL *= iirIntensityL;
+		if (iirIntensityL > 1.0) iirIntensityL = 1.0;
+		
+		double slewR = fabs(inputSampleR-lastSampleR)*slewAmount;
+		lastSampleR = inputSampleR*(0.86-(dynAmount*0.125));
+		if (slewR > 1.0) slewR = 1.0;
+		double iirIntensityR = slewR;
+		iirIntensityR *= 2.472;
+		iirIntensityR *= iirIntensityR;
+		if (iirIntensityR > 1.0) iirIntensityR = 1.0;
+		
+		iirSampleL = (iirSampleL * (1.0 - iirIntensityL)) + (noiseBL * iirIntensityL);
+		noiseBL = iirSampleL;
+		noiseBL = (noiseBL * slewL) + (noiseCL * (1.0-slewL));
+		noiseCL = noiseBL;
+		
+		iirSampleR = (iirSampleR * (1.0 - iirIntensityR)) + (noiseBR * iirIntensityR);
+		noiseBR = iirSampleR;
+		noiseBR = (noiseBR * slewR) + (noiseCR * (1.0-slewR));
+		noiseCR = noiseBR;
+		
+		inputSampleL = (noiseCL * wet) + (drySampleL * (1.0-wet));
+		inputSampleR = (noiseCR * wet) + (drySampleR * (1.0-wet));
+		
+		//begin 32 bit stereo floating point dither
+		int expon; frexpf((float)inputSampleL, &expon);
+		fpdL ^= fpdL << 13; fpdL ^= fpdL >> 17; fpdL ^= fpdL << 5;
+		inputSampleL += ((double(fpdL)-uint32_t(0x7fffffff)) * 5.5e-36l * pow(2,expon+62));
+		frexpf((float)inputSampleR, &expon);
+		fpdR ^= fpdR << 13; fpdR ^= fpdR >> 17; fpdR ^= fpdR << 5;
+		inputSampleR += ((double(fpdR)-uint32_t(0x7fffffff)) * 5.5e-36l * pow(2,expon+62));
+		//end 32 bit stereo floating point dither
+		
+		*out1 = inputSampleL;
+		*out2 = inputSampleR;
+
+		in1++;
+		in2++;
+		out1++;
+		out2++;
+    }
+}
+
+void Texturize::processDoubleReplacing(double **inputs, double **outputs, VstInt32 sampleFrames) 
+{
+    double* in1  =  inputs[0];
+    double* in2  =  inputs[1];
+    double* out1 = outputs[0];
+    double* out2 = outputs[1];
+
+	double overallscale = 1.0;
+	overallscale /= 44100.0;
+	overallscale *= getSampleRate();
+	double slewAmount = ((pow(A,2.0)*4.0)+0.71)/overallscale;
+	double dynAmount = pow(B,2.0);
+	double wet = pow(C,5);
+	
+    while (--sampleFrames >= 0)
+    {
+		double inputSampleL = *in1;
+		double inputSampleR = *in2;
+		if (fabs(inputSampleL)<1.18e-23) inputSampleL = fpdL * 1.18e-17;
+		if (fabs(inputSampleR)<1.18e-23) inputSampleR = fpdR * 1.18e-17;
+		double drySampleL = inputSampleL;
+		double drySampleR = inputSampleR;
+		
+		if (inputSampleL < 0) {
+			if (polarityL == true) {
+				if (noiseAL < 0) flipL = true;
+				else flipL = false;
+			}
+			polarityL = false;
+		} else polarityL = true;		
+		
+		if (inputSampleR < 0) {
+			if (polarityR == true) {
+				if (noiseAR < 0) flipR = true;
+				else flipR = false;
+			}
+			polarityR = false;
+		} else polarityR = true;		
+		
+		if (flipL) noiseAL += (double(fpdL)/UINT32_MAX);
+		else noiseAL -= (double(fpdL)/UINT32_MAX);
+		//here's the guts of the random walk		
+		flipL = !flipL;
+		
+		if (flipR) noiseAR += (double(fpdR)/UINT32_MAX);
+		else noiseAR -= (double(fpdR)/UINT32_MAX);
+		//here's the guts of the random walk		
+		flipR = !flipR;
+		
+		if (inputSampleL > 1.0) inputSampleL = 1.0; if (inputSampleL < -1.0) inputSampleL = -1.0;
+		if (inputSampleR > 1.0) inputSampleR = 1.0; if (inputSampleR < -1.0) inputSampleR = -1.0;
+		
+		if (dynAmount < 0.4999) {
+			inputSampleL = (inputSampleL*dynAmount*2.0) + (sin(inputSampleL)*(1.0-(dynAmount*2.0)));		
+			inputSampleR = (inputSampleR*dynAmount*2.0) + (sin(inputSampleR)*(1.0-(dynAmount*2.0)));		
+		}
+		if (dynAmount > 0.5001) {
+			inputSampleL = (asin(inputSampleL)*((dynAmount*2.0)-1.0)) + (inputSampleL*(1.0-((dynAmount*2.0)-1.0)));
+			inputSampleR = (asin(inputSampleR)*((dynAmount*2.0)-1.0)) + (inputSampleR*(1.0-((dynAmount*2.0)-1.0)));
+		}
+		//doing this in two steps means I get to not run an extra sin/asin function per sample
+		
+		noiseBL = sin(noiseAL*(0.2-(dynAmount*0.125))*fabs(inputSampleL));
+		noiseBR = sin(noiseAR*(0.2-(dynAmount*0.125))*fabs(inputSampleR));
+		
+		double slewL = fabs(inputSampleL-lastSampleL)*slewAmount;
+		lastSampleL = inputSampleL*(0.86-(dynAmount*0.125));
+		if (slewL > 1.0) slewL = 1.0;
+		double iirIntensityL = slewL;
+		iirIntensityL *= 2.472;
+		iirIntensityL *= iirIntensityL;
+		if (iirIntensityL > 1.0) iirIntensityL = 1.0;
+		
+		double slewR = fabs(inputSampleR-lastSampleR)*slewAmount;
+		lastSampleR = inputSampleR*(0.86-(dynAmount*0.125));
+		if (slewR > 1.0) slewR = 1.0;
+		double iirIntensityR = slewR;
+		iirIntensityR *= 2.472;
+		iirIntensityR *= iirIntensityR;
+		if (iirIntensityR > 1.0) iirIntensityR = 1.0;
+		
+		iirSampleL = (iirSampleL * (1.0 - iirIntensityL)) + (noiseBL * iirIntensityL);
+		noiseBL = iirSampleL;
+		noiseBL = (noiseBL * slewL) + (noiseCL * (1.0-slewL));
+		noiseCL = noiseBL;
+		
+		iirSampleR = (iirSampleR * (1.0 - iirIntensityR)) + (noiseBR * iirIntensityR);
+		noiseBR = iirSampleR;
+		noiseBR = (noiseBR * slewR) + (noiseCR * (1.0-slewR));
+		noiseCR = noiseBR;
+		
+		inputSampleL = (noiseCL * wet) + (drySampleL * (1.0-wet));
+		inputSampleR = (noiseCR * wet) + (drySampleR * (1.0-wet));
+		
+		//begin 64 bit stereo floating point dither
+		//int expon; frexp((double)inputSampleL, &expon);
+		fpdL ^= fpdL << 13; fpdL ^= fpdL >> 17; fpdL ^= fpdL << 5;
+		//inputSampleL += ((double(fpdL)-uint32_t(0x7fffffff)) * 1.1e-44l * pow(2,expon+62));
+		//frexp((double)inputSampleR, &expon);
+		fpdR ^= fpdR << 13; fpdR ^= fpdR >> 17; fpdR ^= fpdR << 5;
+		//inputSampleR += ((double(fpdR)-uint32_t(0x7fffffff)) * 1.1e-44l * pow(2,expon+62));
+		//end 64 bit stereo floating point dither
+		
+		*out1 = inputSampleL;
+		*out2 = inputSampleR;
+
+		in1++;
+		in2++;
+		out1++;
+		out2++;
+    }
+}
